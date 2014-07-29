f6b7ea954d136ae8da9c3f618a55dc59f9ecd0bb
Alexander Thiemann <mail@agrafix.net>
2014-07-30 00:26:29 +0200

    first rocksdb version

---
+++ vendored/rocksdb-haskell/src/Database/RocksDB.hs
@@ -0,0 +1,17 @@
+-- |
+-- Module      : Database.RocksDB
+-- Copyright   : (c) 2012-2013 The leveldb-haskell Authors
+--               (c) 2014 The rocksdb-haskell Authors
+-- License     : BSD3
+-- Maintainer  : mail@agrafix.net
+-- Stability   : experimental
+-- Portability : non-portable
+--
+-- RocksDB Haskell binding.
+--
+-- The API closely follows the C-API of RocksDB.
+-- For more information, see: <http://rocksdb.org>
+
+module Database.RocksDB (module R) where
+
+import Database.RocksDB.MonadResource as R
--- vendored/rocksdb-haskell/src/Database/RocksDB/Base.hs
+++ vendored/rocksdb-haskell/src/Database/RocksDB/Base.hs
@@ -1,17 +1,18 @@
 -- |
--- Module      : Database.LevelDB.Base
+-- Module      : Database.RocksDB.Base
 -- Copyright   : (c) 2012-2013 The leveldb-haskell Authors
+--               (c) 2014 The rocksdb-haskell Authors
 -- License     : BSD3
--- Maintainer  : kim.altintop@gmail.com
+-- Maintainer  : mail@agrafix.net
 -- Stability   : experimental
 -- Portability : non-portable
 --
--- LevelDB Haskell binding.
+-- RocksDB Haskell binding.
 --
--- The API closely follows the C-API of LevelDB.
--- For more information, see: <http://leveldb.googlecode.com>
+-- The API closely follows the C-API of RocksDB.
+-- For more information, see: <http://agrafix.net>
 
-module Database.LevelDB.Base
+module Database.RocksDB.Base
     ( -- * Exported Types
       DB
     , BatchOp (..)
@@ -51,10 +52,9 @@ module Database.LevelDB.Base
     , destroy
     , repair
     , approximateSize
-    , version
 
     -- * Iteration
-    , module Database.LevelDB.Iterator
+    , module Database.RocksDB.Iterator
     )
 where
 
@@ -67,10 +67,10 @@ import           Data.ByteString.Internal (ByteString (..))
 import           Foreign
 import           Foreign.C.String         (withCString)
 
-import           Database.LevelDB.C
-import           Database.LevelDB.Internal
-import           Database.LevelDB.Iterator
-import           Database.LevelDB.Types
+import           Database.RocksDB.C
+import           Database.RocksDB.Internal
+import           Database.RocksDB.Iterator
+import           Database.RocksDB.Types
 
 import qualified Data.ByteString          as BS
 import qualified Data.ByteString.Unsafe   as BU
@@ -86,7 +86,7 @@ open path opts = liftIO $ bracketOnError (mkOpts opts) freeOpts mkDB
             withCString path $ \path_ptr ->
                 liftM (`DB` opts')
                 $ throwIfErr "open"
-                $ c_leveldb_open opts_ptr path_ptr
+                $ c_rocksdb_open opts_ptr path_ptr
 
 -- | Close a database.
 --
@@ -94,7 +94,7 @@ open path opts = liftIO $ bracketOnError (mkOpts opts) freeOpts mkDB
 -- longer be used.
 close :: MonadIO m => DB -> m ()
 close (DB db_ptr opts_ptr) = liftIO $
-    c_leveldb_close db_ptr `finally` freeOpts opts_ptr
+    c_rocksdb_close db_ptr `finally` freeOpts opts_ptr
 
 
 -- | Run an action with a 'Snapshot' of the database.
@@ -107,7 +107,7 @@ withSnapshot db act = liftIO $
 -- The returned 'Snapshot' should be released with 'releaseSnapshot'.
 createSnapshot :: MonadIO m => DB -> m Snapshot
 createSnapshot (DB db_ptr _) = liftIO $
-    Snapshot <$> c_leveldb_create_snapshot db_ptr
+    Snapshot <$> c_rocksdb_create_snapshot db_ptr
 
 -- | Release a snapshot.
 --
@@ -115,38 +115,38 @@ createSnapshot (DB db_ptr _) = liftIO $
 -- longer be used.
 releaseSnapshot :: MonadIO m => DB -> Snapshot -> m ()
 releaseSnapshot (DB db_ptr _) (Snapshot snap) = liftIO $
-    c_leveldb_release_snapshot db_ptr snap
+    c_rocksdb_release_snapshot db_ptr snap
 
 -- | Get a DB property.
 getProperty :: MonadIO m => DB -> Property -> m (Maybe ByteString)
 getProperty (DB db_ptr _) p = liftIO $
     withCString (prop p) $ \prop_ptr -> do
-        val_ptr <- c_leveldb_property_value db_ptr prop_ptr
+        val_ptr <- c_rocksdb_property_value db_ptr prop_ptr
         if val_ptr == nullPtr
             then return Nothing
             else do res <- Just <$> BS.packCString val_ptr
                     free val_ptr
                     return res
     where
-        prop (NumFilesAtLevel i) = "leveldb.num-files-at-level" ++ show i
-        prop Stats    = "leveldb.stats"
-        prop SSTables = "leveldb.sstables"
+        prop (NumFilesAtLevel i) = "rocksdb.num-files-at-level" ++ show i
+        prop Stats    = "rocksdb.stats"
+        prop SSTables = "rocksdb.sstables"
 
--- | Destroy the given LevelDB database.
+-- | Destroy the given RocksDB database.
 destroy :: MonadIO m => FilePath -> Options -> m ()
 destroy path opts = liftIO $ bracket (mkOpts opts) freeOpts destroy'
     where
         destroy' (Options' opts_ptr _ _ _) =
             withCString path $ \path_ptr ->
-                throwIfErr "destroy" $ c_leveldb_destroy_db opts_ptr path_ptr
+                throwIfErr "destroy" $ c_rocksdb_destroy_db opts_ptr path_ptr
 
--- | Repair the given LevelDB database.
+-- | Repair the given RocksDB database.
 repair :: MonadIO m => FilePath -> Options -> m ()
 repair path opts = liftIO $ bracket (mkOpts opts) freeOpts repair'
     where
         repair' (Options' opts_ptr _ _ _) =
             withCString path $ \path_ptr ->
-                throwIfErr "repair" $ c_leveldb_repair_db opts_ptr path_ptr
+                throwIfErr "repair" $ c_rocksdb_repair_db opts_ptr path_ptr
 
 
 -- TODO: support [Range], like C API does
@@ -162,7 +162,7 @@ approximateSize (DB db_ptr _) (from, to) = liftIO $
     withArray [to_ptr]            $ \to_ptrs          ->
     withArray [intToCSize tlen]   $ \tlen_ptrs        ->
     allocaArray 1                 $ \size_ptrs        -> do
-        c_leveldb_approximate_sizes db_ptr 1
+        c_rocksdb_approximate_sizes db_ptr 1
                                     from_ptrs flen_ptrs
                                     to_ptrs tlen_ptrs
                                     size_ptrs
@@ -178,7 +178,7 @@ put (DB db_ptr _) opts key value = liftIO $ withCWriteOpts opts $ \opts_ptr ->
     BU.unsafeUseAsCStringLen key   $ \(key_ptr, klen) ->
     BU.unsafeUseAsCStringLen value $ \(val_ptr, vlen) ->
         throwIfErr "put"
-            $ c_leveldb_put db_ptr opts_ptr
+            $ c_rocksdb_put db_ptr opts_ptr
                             key_ptr (intToCSize klen)
                             val_ptr (intToCSize vlen)
 
@@ -188,7 +188,7 @@ get (DB db_ptr _) opts key = liftIO $ withCReadOpts opts $ \opts_ptr ->
     BU.unsafeUseAsCStringLen key $ \(key_ptr, klen) ->
     alloca                       $ \vlen_ptr -> do
         val_ptr <- throwIfErr "get" $
-            c_leveldb_get db_ptr opts_ptr key_ptr (intToCSize klen) vlen_ptr
+            c_rocksdb_get db_ptr opts_ptr key_ptr (intToCSize klen) vlen_ptr
         vlen <- peek vlen_ptr
         if val_ptr == nullPtr
             then return Nothing
@@ -202,16 +202,16 @@ delete :: MonadIO m => DB -> WriteOptions -> ByteString -> m ()
 delete (DB db_ptr _) opts key = liftIO $ withCWriteOpts opts $ \opts_ptr ->
     BU.unsafeUseAsCStringLen key $ \(key_ptr, klen) ->
         throwIfErr "delete"
-            $ c_leveldb_delete db_ptr opts_ptr key_ptr (intToCSize klen)
+            $ c_rocksdb_delete db_ptr opts_ptr key_ptr (intToCSize klen)
 
 -- | Perform a batch mutation.
 write :: MonadIO m => DB -> WriteOptions -> WriteBatch -> m ()
 write (DB db_ptr _) opts batch = liftIO $ withCWriteOpts opts $ \opts_ptr ->
-    bracket c_leveldb_writebatch_create c_leveldb_writebatch_destroy $ \batch_ptr -> do
+    bracket c_rocksdb_writebatch_create c_rocksdb_writebatch_destroy $ \batch_ptr -> do
 
     mapM_ (batchAdd batch_ptr) batch
 
-    throwIfErr "write" $ c_leveldb_write db_ptr opts_ptr batch_ptr
+    throwIfErr "write" $ c_rocksdb_write db_ptr opts_ptr batch_ptr
 
     -- ensure @ByteString@s (and respective shared @CStringLen@s) aren't GC'ed
     -- until here
@@ -221,13 +221,13 @@ write (DB db_ptr _) opts batch = liftIO $ withCWriteOpts opts $ \opts_ptr ->
         batchAdd batch_ptr (Put key val) =
             BU.unsafeUseAsCStringLen key $ \(key_ptr, klen) ->
             BU.unsafeUseAsCStringLen val $ \(val_ptr, vlen) ->
-                c_leveldb_writebatch_put batch_ptr
+                c_rocksdb_writebatch_put batch_ptr
                                          key_ptr (intToCSize klen)
                                          val_ptr (intToCSize vlen)
 
         batchAdd batch_ptr (Del key) =
             BU.unsafeUseAsCStringLen key $ \(key_ptr, klen) ->
-                c_leveldb_writebatch_delete batch_ptr key_ptr (intToCSize klen)
+                c_rocksdb_writebatch_delete batch_ptr key_ptr (intToCSize klen)
 
         touch (Put (PS p _ _) (PS p' _ _)) = do
             touchForeignPtr p
@@ -235,20 +235,11 @@ write (DB db_ptr _) opts batch = liftIO $ withCWriteOpts opts $ \opts_ptr ->
 
         touch (Del (PS p _ _)) = touchForeignPtr p
 
--- | Return the runtime version of the underlying LevelDB library as a (major,
--- minor) pair.
-version :: MonadIO m => m (Int, Int)
-version = do
-    major <- liftIO c_leveldb_major_version
-    minor <- liftIO c_leveldb_minor_version
-
-    return (cIntToInt major, cIntToInt minor)
-
 createBloomFilter :: MonadIO m => Int -> m BloomFilter
 createBloomFilter i = do
     let i' = fromInteger . toInteger $ i
-    fp_ptr <- liftIO $ c_leveldb_filterpolicy_create_bloom i'
+    fp_ptr <- liftIO $ c_rocksdb_filterpolicy_create_bloom i'
     return $ BloomFilter fp_ptr
 
 releaseBloomFilter :: MonadIO m => BloomFilter -> m ()
-releaseBloomFilter (BloomFilter fp) = liftIO $ c_leveldb_filterpolicy_destroy fp
+releaseBloomFilter (BloomFilter fp) = liftIO $ c_rocksdb_filterpolicy_destroy fp
