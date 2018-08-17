048d419f5b26d6028defc0261965a5234829225d
Lars Kuhtz <lars@kadena.io>
2018-08-17 10:05:58 -0700

    initialize cabal package

---
+++ node/ChainwebNode.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE UnicodeSyntax #-}
+
+-- |
+-- Module: ChainwebNode
+-- Copyright: Copyright © 2018 Kadena LLC.
+-- License: AllRightsReserved
+-- Maintainer: Lars Kuhtz <lars@kadena.io>
+-- Stability: experimental
+--
+-- TODO
+--
+module ChainwebNode
+( main
+) where
+
+main ∷ IO ()
+main = putStrLn "nothing yet implemented"
+
