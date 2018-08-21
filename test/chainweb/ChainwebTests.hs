ba7d383007a7b7756b66c890dfc35587186ce1b3
Lars Kuhtz <lars@kadena.io>
2018-08-21 13:53:04 -0700

    add cabal flag for development builds

---
+++ test/chainweb/ChainwebTests.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE UnicodeSyntax #-}
+
+-- |
+-- Module: ChainwebTests
+-- Copyright: Copyright © 2018 Kadena LLC.
+-- License: MIT
+-- Maintainer: Lars Kuhtz <lars@kadena.io>
+-- Stability: experimental
+--
+-- TODO
+--
+module ChainwebTests
+( main
+) where
+
+main ∷ IO ()
+main = putStrLn "nothing yet implemented"
+
