c8cf2dfc7d3195ef855ab67a505f87da45d61a14
Colin Woodbury <colin@fosskers.ca>
2018-09-13 11:46:01 -0700

    Test simple DB initialization and insertion
    - The insertion test currently fails with a `ParentMissing` exception,
      implying that the `Abitrary` instance for `BlockHeader` isn't correct.

--- test/chainweb/ChainwebTests.hs
+++ test/chainweb/ChainwebTests.hs
@@ -1,6 +1,4 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-
 -- |
 -- Module: ChainwebTests
 -- Copyright: Copyright © 2018 Kadena LLC.
@@ -10,10 +8,58 @@
 --
 -- TODO
 --
-module ChainwebTests
-( main
-) where
+
+module ChainwebTests ( main ) where
+
+-- import           Chainweb.ChainDB (persist)
+-- import           System.Path (Path, Absolute, toFilePath)
+
+import           Chainweb.BlockHeader (genesisBlockHeader)
+import qualified Chainweb.ChainDB as DB
+import           Chainweb.ChainId (testChainId)
+import           Chainweb.Graph (toChainGraph)
+import           Chainweb.Version (ChainwebVersion(..))
+import           Control.Monad (void)
+import           Data.DiGraph (singleton)
+import           Orphans ()
+import           Test.QuickCheck
+import           Test.Tasty
+import           Test.Tasty.HUnit
+
+---
 
 main :: IO ()
-main = putStrLn "nothing yet implemented"
+main = defaultMain suite
 
+suite :: TestTree
+suite = testGroup "Unit Tests"
+  [ testGroup "ChainDB"
+    [ testGroup "Basic Interaction"
+      [ testCase "Initialization + Shutdown" $ chaindb >>= DB.closeChainDb
+      , testCase "Single Insertion + Sync" insertItem
+      ]
+    , testGroup "Encoding round-trips"
+      [ testCase "Empty ChainDb" empty
+      -- , testCase "Singleton ChainDb" $ undefined
+      -- , testCase "Multiple Entries"  $ undefined
+      ]
+    ]
+  ]
+
+empty :: Assertion
+empty = do
+  db <- chaindb
+  1 @?= 1
+
+chaindb :: IO DB.ChainDb
+chaindb = DB.initChainDb . DB.Configuration $ genesisBlockHeader Test graph cid
+  where graph = toChainGraph (const cid) singleton
+        cid   = testChainId 0
+
+insertItem :: Assertion
+insertItem = do
+  db  <- chaindb
+  ss  <- DB.snapshot db
+  bh  <- DB.entry <$> generate arbitrary
+  DB.insert bh ss >>= void . DB.syncSnapshot
+  DB.closeChainDb db
