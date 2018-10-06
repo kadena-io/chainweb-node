6a5eb9bf05ced8cb54f2598b2e7e5044822b2068
Colin Woodbury <colin@fosskers.ca>
2018-10-05 18:57:15 -0700

    sync: Initial skeleton for simple `sync` logic
    - Uses some yet unimplented ideas like `Peer` to get the point across.
    
    - Actual calling to remote peers depends on the Rest API work,
      which is not yet merged.
    
    - This initial version will query a peer for /all/ BlockHeaders
      of height (CURR_MAX_HEIGHT - (2 * diameter)).
      This is because neither the current heighest block nor the current
      heaviest branch on a single chain may be members of the heaviest
      cut, which is what miners actually mine on. A single chain has
      no way to know which of its leaves are part of the true cut, and so we
      need to update all branches near the top. In the average case
      where there are no competing branches, this will only fetch a single
      path of BlockHeaders.

---
+++ src/Chainweb/ChainDB/Sync.hs
@@ -0,0 +1,79 @@
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE DataKinds #-}
+
+-- |
+-- Module: Chainweb.ChainDB.Sync
+-- Copyright: Copyright © 2018 Kadena LLC.
+-- License: MIT
+-- Maintainer: Lars Kuhtz <lars@kadena.io>
+-- Stability: experimental
+--
+-- Sync a local `ChainDb` with that of some remote peer.
+--
+
+module Chainweb.ChainDB.Sync
+  ( -- * Syncronizing a Chain
+    sync
+  ) where
+
+import           Chainweb.BlockHeader (BlockHeader(..), BlockHeight(..))
+import           Chainweb.ChainDB
+import           Control.Arrow ((&&&))
+import           Data.Foldable (toList)
+import           Data.List.NonEmpty (NonEmpty)
+import qualified Data.List.NonEmpty as NEL
+import qualified Data.Map.Strict as M
+import           Data.Maybe (fromJust)
+import           Refined hiding (NonEmpty)
+import           Streaming
+
+-- | Some abstract notion of a peer to connect to.
+-- Assumed to never yield phoney `BlockHeader`s.
+data Peer
+
+-- TODO The real version of this will be present elsewhere.
+-- | The diameter of the current chain graph.
+newtype Diameter = Diameter { diameter :: Refined (Positive && LessThan 10) Int }
+
+-- | `BlockHeader`s in a `ChainDb` that have no children.
+newtype Leaves = Leaves { unleaves :: NonEmpty BlockHeader }
+
+-- TODO How to model failure?
+-- | Given a `Peer` to connect to, fetch all `BlockHeader`s that exist
+-- in the peer's chain but not our local given `ChainDb`, and sync them.
+sync :: Diameter -> Peer -> ChainDb -> IO ()
+sync d p db = do
+  s <- snapshot db
+  let mleaves :: Maybe Leaves
+      mleaves = traverse (`getEntry` s) (toList $ branches s)
+        >>= NEL.nonEmpty
+        >>= Just . Leaves . fmap dbEntry
+  case mleaves of
+    Nothing -> undefined
+    Just leaves -> putThemIn s . headers p $ _blockHeight (lowLeaf d leaves) + 1
+
+leafMap :: Leaves -> M.Map BlockHeight BlockHeader
+leafMap = M.fromList . map (_blockHeight &&& id) . toList . unleaves
+
+-- | Given some leaves, find the lowest one such that it's at most only
+-- (diameter * 2) in height away from the highest.
+lowLeaf :: Diameter -> Leaves -> BlockHeader
+lowLeaf d l = snd . fromJust $ M.lookupGE lowH lmap  -- TODO Naughty fromJust. Use `justified-containers`?
+  where
+    lmap = leafMap l
+    lowH = BlockHeight . fromIntegral $ max (high - low) 0
+    high = fromIntegral . _blockHeight . NEL.head $ unleaves l
+    low  = 2 * unrefine (diameter d)
+
+-- TODO How to model failure of the invariants? Exceptions?
+-- | Fetch all `BlockHeader`s from a peer from a given `BlockHeight` and higher.
+--
+-- INVARIANTS:
+--   * The `BlockHeader`s are streamed in order of `BlockHeight`, lowest to highest.
+headers :: Peer -> BlockHeight -> Stream (Of BlockHeader) IO ()
+headers = undefined
+
+-- TODO How often to call `syncSnapshot`?
+-- | Add the new remote `BlockHeader`s to our local chain.
+putThemIn :: Snapshot -> Stream (Of BlockHeader) IO () -> IO ()
+putThemIn s bs = undefined
