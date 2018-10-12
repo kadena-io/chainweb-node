cc6a096a6c098b1a983f94ae176ad84fe4bd2bd8
Colin Woodbury <colin@fosskers.ca>
2018-10-12 14:56:50 -0700

    sync: Ask for `ClientEnv` instead of nebulous `Peer` type

--- src/Chainweb/ChainDB/Sync.hs
+++ src/Chainweb/ChainDB/Sync.hs
@@ -7,7 +7,7 @@
 -- Module: Chainweb.ChainDB.Sync
 -- Copyright: Copyright © 2018 Kadena LLC.
 -- License: MIT
--- Maintainer: Lars Kuhtz <lars@kadena.io>
+-- Maintainer: Colin Woodbury <colin@kadena.io>
 -- Stability: experimental
 --
 -- Sync a local `ChainDb` with that of some remote peer.
@@ -16,6 +16,9 @@
 module Chainweb.ChainDB.Sync
   ( -- * Syncronizing a Chain
     sync
+  , headers
+    -- * Temporary Export
+  , Diameter(..)
   ) where
 
 import           Chainweb.BlockHeader (BlockHeader(..), BlockHeight(..))
@@ -23,22 +26,17 @@ import           Chainweb.ChainDB
 import           Chainweb.ChainDB.RestAPI.Client (headersClient)
 import           Chainweb.RestAPI.Utils (Page(..))
 import           Control.Arrow ((&&&))
+import           Control.Monad.Catch (throwM)
 import           Control.Monad.Trans.State.Strict
 import           Data.Foldable (toList)
 import           Data.List.NonEmpty (NonEmpty)
 import qualified Data.List.NonEmpty as NEL
 import qualified Data.Map.Strict as M
 import           Data.Maybe (fromMaybe)
-import           Network.HTTP.Client (newManager)
-import           Network.HTTP.Client.TLS (tlsManagerSettings)
 import           Refined hiding (NonEmpty)
 import           Servant.Client hiding (client)
 import           Streaming
 import qualified Streaming.Prelude as SP
-
--- | Some abstract notion of a peer to connect to.
--- Assumed to never yield phoney `BlockHeader`s.
-data Peer
 
 -- TODO The real version of this will be present elsewhere.
 -- | The diameter of the current chain graph.
@@ -47,19 +45,18 @@ newtype Diameter = Diameter { diameter :: Refined (Positive && LessThan 10) Int 
 -- | `BlockHeader`s in a `ChainDb` that have no children.
 newtype Leaves = Leaves { unleaves :: NonEmpty BlockHeader }
 
--- TODO How to model failure?
--- | Given a `Peer` to connect to, fetch all `BlockHeader`s that exist
+-- | Given a peer to connect to, fetch all `BlockHeader`s that exist
 -- in the peer's chain but not our local given `ChainDb`, and sync them.
-sync :: Diameter -> Peer -> ChainDb -> IO ()
-sync d p db = do
+sync :: Diameter -> ClientEnv -> ChainDb -> IO ()
+sync d env db = do
   s <- snapshot db
   let mleaves :: Maybe Leaves
       mleaves = traverse (`getEntry` s) (toList $ branches s)
         >>= NEL.nonEmpty
         >>= Just . Leaves . fmap dbEntry
   case mleaves of
-    Nothing -> undefined
-    Just leaves -> putThemIn s . headers p $ lowLeaf d leaves
+    Nothing -> error "Local ChainDb is impossibly empty."
+    Just leaves -> putThemIn s . headers env $ lowLeaf d leaves
 
 -- | \(\mathcal{O}(n \log n)\).
 leafMap :: Leaves -> M.Map BlockHeight BlockHeader
@@ -83,20 +80,14 @@ lowLeaf d l = fromMaybe (NEL.head $ unleaves l) . fmap snd $ M.lookupGE lowH lma
     high = fromIntegral . _blockHeight . NEL.head $ unleaves l
     low  = 2 * unrefine (diameter d)
 
--- TODO How to model failure of the invariants? Exceptions?
 -- | Fetch all `BlockHeader`s from a peer from a given `BlockHeight` and higher.
 --
 -- INVARIANTS:
+--
 --   * The `BlockHeader`s are streamed in order of `BlockHeight`, lowest to highest.
 --     We assume the server will do this correctly.
-headers :: Peer -> BlockHeader -> Stream (Of BlockHeader) IO ()
-headers p h = do
-    env <- flip mkClientEnv (url p) <$> lift (newManager tlsManagerSettings)
-    headers' env h
-
--- | Manage a connection to a `HeadersApi` and automatically handle its paging.
-headers' :: ClientEnv -> BlockHeader -> Stream (Of BlockHeader) IO ()
-headers' env h = g $ client Nothing
+headers :: ClientEnv -> BlockHeader -> Stream (Of BlockHeader) IO ()
+headers env h = g $ client Nothing
   where
     height = 1 + fromIntegral (_blockHeight h)
 
@@ -104,23 +95,16 @@ headers' env h = g $ client Nothing
     client :: Maybe (Key 'Unchecked) -> ClientM (Page (Key 'Unchecked) (Entry 'Unchecked))
     client next = headersClient (_blockChainwebVersion h) (_blockChainId h) (Just 100) next (Just height) Nothing Nothing
 
+    -- | Attempt to run a servant client.
     g :: ClientM (Page (Key 'Unchecked) (Entry 'Unchecked)) -> Stream (Of BlockHeader) IO ()
-    g c = do
-        lift (runClientM c env) >>= \case
-            Left err   -> undefined  -- TODO
-            Right page -> f page
+    g c = lift (runClientM c env) >>= either (lift . throwM) f
 
+    -- | Stream every `BlockHeader` from a `Page`, automatically requesting
+    -- the next `Page` if there is one.
     f :: Page (Key 'Unchecked) (Entry 'Unchecked) -> Stream (Of BlockHeader) IO ()
     f page = do
         SP.map dbEntry . SP.each $ _pageItems page
         maybe (pure ()) (g . client . Just) $ _pageNext page
-
-url :: Peer -> BaseUrl
-url p = BaseUrl Https (ip p) 443 ""
-
--- TODO In the real version, this is just a field accessor.
-ip :: Peer -> String
-ip = undefined
 
 -- TODO How often to call `syncSnapshot`?
 -- Currently it calls it after all new `BlockHeader`s have been inserted.
