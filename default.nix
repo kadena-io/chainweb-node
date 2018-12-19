45b1176b34f3fc6b01422156c74f8fc5c8a60733
Doug Beardsley <mightybyte@gmail.com>
2018-12-19 13:00:12 -0500

    Depend on pact (#107)
    Add pact as a dependency and improve nix infrastructure in general.

--- default.nix
+++ default.nix
@@ -1,58 +1,125 @@
-# To pin to a specific version of nixpkgs, you can substitute <nixpkgs> with:
-# `(builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/<nixpkgs_commit_hash>.tar.gz")`
-{ compiler ? "ghc844"
-, owner ? "NixOS"
-, rev ? "7b54e50c085501c995e38130eb6f5e3b7a8c2523"
-, sha ? "1l3083b0fjhnx5lq89bxjvimp0ax4j8shra2z3h6xnp0bd2rjgvv"
-, pkgs ? import (builtins.fetchTarball {
-                   url = "https://github.com/${owner}/nixpkgs/archive/${rev}.tar.gz";
-                   sha256 = sha; }) { config.allowUnfree = true; }
+{ rpRef ? "f3ff81d519b226752c83eefd4df6718539c3efdc"
+, rpSha ? "1ijxfwl36b9b2j4p9j3bv8vf7qfi570m1c5fjyvyac0gy0vi5g8j"
+, system ? builtins.currentSystem
 }:
-  pkgs.haskell.packages.${compiler}.developPackage {
+
+let rp = builtins.fetchTarball {
+      url = "https://github.com/reflex-frp/reflex-platform/archive/${rpRef}.tar.gz";
+      sha256 = rpSha;
+    };
+
+in
+  (import rp {}).project ({ pkgs, ... }:
+  let gitignore = pkgs.callPackage (pkgs.fetchFromGitHub {
+        owner = "siers";
+        repo = "nix-gitignore";
+        rev = "ee230196f4ce4778bfe0e84429966a343fcc5248";
+        sha256 = "10md0iaic858fvwvc71qs50w13240fwq5rvza9kd2la3s94mzawy";
+      }) {};
+  in
+  {
     name = "chainweb";
-    root = builtins.filterSource
-      (path: type: !(builtins.elem (baseNameOf path) ["result" "dist" "dist-newstyle" ".git" ".stack-work"]))
-      ./.;
-    overrides = self: super: with pkgs.haskell.lib; {
-      # Don't run a package's test suite
-      # foo = dontCheck super.chainweb;
-      #
-      # Don't enforce package's version constraints
-      # bar = doJailbreak super.bar;
-      #
-      # To discover more functions that can be used to modify haskell
-      # packages, run "nix repl", type "pkgs = import <nixpkgs> {}", hit
-      # enter, then type "pkgs.haskell.lib.", then hit <TAB> to get a
-      # tab-completed list of functions.
-      configuration-tools = dontCheck super.configuration-tools;
+    overrides = self: super: with pkgs.haskell.lib;
+      let # Working on getting this function upstreamed into nixpkgs, but
+          # this actually gets things directly from hackage and doesn't
+          # depend on the state of nixpkgs.  Should allow us to have fewer
+          # github overrides.
+          callHackageDirect = {pkg, ver, sha256}@args:
+            let pkgver = "${pkg}-${ver}";
+            in self.callCabal2nix pkg (pkgs.fetchzip {
+                 url = "http://hackage.haskell.org/package/${pkgver}/${pkgver}.tar.gz";
+                 inherit sha256;
+               }) {};
+      in {
+        aeson = callHackageDirect {
+          pkg = "aeson";
+          ver = "1.4.2.0";
+          sha256 = "0qcczw3l596knj9s4ha07wjspd9wkva0jv4734sv3z3vdad5piqh";
+        };
+
+        chainweb = doCoverage (doHaddock super.chainweb);
+        configuration-tools = self.callHackage "configuration-tools" "0.4.0" {};
+
+        # pact-2.6.1
+        pact = addBuildDepend (self.callCabal2nix "pact" (pkgs.fetchFromGitHub {
+          owner = "kadena-io";
+          repo = "pact";
+          rev = "3316593a690444e0290dfc3605b55dc5aa7e43c9";
+          sha256 = "0h5nlwklha8kgcgxybcb5g2ldfs8x78paqr8riaja73ww7ydf496";
+        }) {}) pkgs.z3;
+
+        streaming = callHackageDirect {
+          pkg = "streaming";
+          ver = "0.2.2.0";
+          sha256 = "0yym840jnh2cma5n4c0pv3nh1hyhag1v6pks73wdikhrcajffsh3";
+        };
+
+        yet-another-logger = callHackageDirect {
+          pkg = "yet-another-logger";
+          ver = "0.3.1";
+          sha256 = "17i5km3bxlp568q9pbnbp2nvpfgnmccpfnvcci0z1f56cw95679n";
+        };
+
+        ######################################################################
+        # Dependencies from pact
+        # pact = addBuildDepend super.pact pkgs.z3;
+
+        # tests for extra were failing due to an import clash (`isWindows`)
+        extra = dontCheck super.extra;
+
+        # tests try to use ghc-pkg and cabal (https://github.com/sol/doctest/issues/213)
+        doctest = dontCheck (self.callHackage "doctest" "0.16.0" {});
+
+        # these want to use doctest, which doesn't work on ghcjs
+        bytes = dontCheck super.bytes;
+        intervals = dontCheck super.intervals;
+        bound = dontCheck super.bound;
+        trifecta = dontCheck super.trifecta;
+        lens-aeson = dontCheck super.lens-aeson;
+
+        # test suite for this is failing on ghcjs:
+        hw-hspec-hedgehog = dontCheck super.hw-hspec-hedgehog;
+
+        algebraic-graphs = dontCheck super.algebraic-graphs;
+
+        # Needed to get around a requirement on `hspec-discover`.
+        megaparsec = dontCheck super.megaparsec;
+
+        hedgehog = callHackageDirect {
+          pkg = "hedgehog";
+          ver = "0.6.1";
+          sha256 = "041dn82r732vsda9zvyf3p2zkljkdala684g106d13672n2z34km";
+        };
+
+        # hlint = self.callHackage "hlint" "2.0.14" {};
+        # hoogle = self.callHackage "hoogle" "5.0.15" {};
+
+        # specific revision needed by pact
+        sbv = pkgs.haskell.lib.dontCheck (self.callCabal2nix "sbv" (pkgs.fetchFromGitHub {
+          owner = "LeventErkok";
+          repo = "sbv";
+          rev = "3dc60340634c82f39f6c5dca2b3859d10925cfdf";
+          sha256 = "18xcxg1h19zx6gdzk3dfs87447k3xjqn40raghjz53bg5k8cdc31";
+        }) {});
+
+        # Our own custom fork
+        thyme = dontCheck (self.callCabal2nix "thyme" (pkgs.fetchFromGitHub {
+          owner = "kadena-io";
+          repo = "thyme";
+          rev = "6ee9fcb026ebdb49b810802a981d166680d867c9";
+          sha256 = "09fcf896bs6i71qhj5w6qbwllkv3gywnn5wfsdrcm0w1y6h8i88f";
+        }) {});
+        ######################################################################
+
+      };
+    packages = {
+      chainweb = gitignore.gitignoreSource [] ./.;
     };
-    source-overrides = {
-      # Use a specific hackage version (if nixpkgs knows about it)
-      # foo = "0.11.3.1";
-      configuration-tools = "0.4.0";
-      aeson = "1.4.0.0";
-
-      # streaming >=0.2.2 is on Hackage, but nix doesn't know how get it from there
-      streaming = pkgs.fetchFromGitHub {
-        owner = "haskell-streaming";
-        repo = "streaming";
-        rev = "cf4ae245a0c31432776c9ae6c11312c25a8c6c88";
-        sha256 = "1b8m1ia2qp3rh09cyacsx9cjb20hdk7vlylc166wmnpwl46g4dvx";
-      };
-
-      # Use a particular commit from github
-      refined = pkgs.fetchFromGitHub {
-        owner = "nikita-volkov";
-        repo = "refined";
-        rev = "cd3d99a6a9dff6adfc6dfceb06697ae56261ea6d";
-        sha256 = "0ihgr7ldqjx7f3nzpf7g587s0i9acix9xwm9wwp0v6rs4jcdbbm7";
-      };
-
-      yet-another-logger = pkgs.fetchFromGitHub {
-        owner = "alephcloud";
-        repo = "hs-yet-another-logger";
-        rev = "5706c1bb57e18f2793da21808868d947f74005f0";
-        sha256 = "1lkg6p8s3j48q6cq27k9sldd1f8aqd6b77rsa0vbbzsi69idqb17";
-      };
+    shellToolOverrides = ghc: super: {
+      stack = pkgs.stack;
     };
-  }
+    shells = {
+      ghc = ["chainweb"];
+    };
+
+  })
