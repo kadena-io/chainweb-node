d8b3d2e5f58a03375719bfff185f3001c42b4e42
Mark Nichols <marklnichols@gmail.com>
2019-01-07 18:16:56 -0500

    WIP

--- test/Chainweb/Test/Pact.hs
+++ test/Chainweb/Test/Pact.hs
@@ -8,8 +8,10 @@
 -- Unit test for Pact execution in Chainweb
 
 
-module Chainweb.Test.Pact
+module Chainweb.Test.Pact where
 
+import qualified Pact.Gas as P
+import qualified Pact.Types.Logger as P
 import Test.Tasty
 
 tests :: TestTree
@@ -18,15 +20,52 @@ tests = testGroup "Chainweb Pact tests"
     ]
 
 simpleExecTests :: TestTree
-simpleExecTests = testGroup "Simple Pact execution tests"
-
+simpleExecTests = do
+  testGroup "Simple Pact execution tests"
+  let loggers = P.neverLog
+  let logger = P.newLogger loggers $ P.LogName "PactService"
   pactCfg <- setupConfig "pact.yaml" -- TODO: file name/location from configuration
   let cmdConfig = toCommandConfig pactCfg
-  theStore <- initPactCheckpointStore :: IO MapPurePactCheckpointStore
-  let env = CheckpointEnv {_cpeCheckpointStore = theStore, _cpeCommandConfig = cmdConfig }
-  theState <- DB.initService cmdConfig P.neverLog
-  _ <- runRWST execSimple env theState
-  return ()
+  let gasLimit = fromMaybe 0 (P._ccGasLimit cmdConfig)
+  let gasRate = fromMaybe 0 (P._ccGasRate cmdConfig)
+  let gasEnv = P.GasEnv (fromIntegral gasLimit) 0.0 (P.constGasModel (fromIntegral gasRate))
+  (checkpointEnv, theState) <-
+   case P._ccSqlite cmdConfig of
+     Nothing -> do
+       env <- P.mkPureEnv loggers
+       liftA2 (,) (initInMemoryCheckpointEnv cmdConfig logger gasEnv) (mkPureState env cmdConfig)
+     Just sqlc -> do
+       env <- P.mkSQLiteEnv logger False sqlc loggers
+       liftA2 (,) (initSQLiteCheckpointEnv cmdConfig logger gasEnv) (mkSQLiteState env cmdConfig)
+  void $ runRWST serviceRequests checkpointEnv theState
 
 execSimple :: PactT
-execSimple = do
+execSimple = undefined
+
+----------------------------------------------------------------------------------------------------
+data TestRequest = TestRequest
+  { cmd :: String
+  , eval :: TestResponse -> Expectation
+  , displayStr :: String
+  }
+
+instance Show TestRequest where
+  show tr = "cmd: " ++ cmd tr ++ "\nDisplay string: " ++ displayStr tr
+
+data TestResponse = TestResponse
+  { resultSuccess :: Bool
+  , apiResult :: ApiResult
+  , _batchCount :: Int64
+  } deriving (Eq, Generic)
+
+instance Show TestResponse where
+  show tr = "resultSuccess: " ++ show (resultSuccess tr) ++ "\n"
+    ++ "Batch count: " ++ show (_batchCount tr) ++ "\n"
+    ++ take 100 (show (apiResult tr)) ++ "..."
+
+----------------------------------------------------------------------------------------------------
+testReq1 :: TestRequest
+testReq1 = TestRequest
+  { cmd = "exec (+ 1 1)"
+  , eval = (\tr -> checkScientific (scientific 2 0) tr)
+  , displayStr = "Executes 1 + 1 in Pact and returns 2.0" }
