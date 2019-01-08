795944d4ca248c02ff789c08c373aa37caf6f394
Emmanuel Denloye-Ito <eod8tb@virginia.edu>
2019-01-08 15:30:54 -0500

    Take out unnecessary comments

--- src/Chainweb/Pact/Backend/SQLiteCheckpointer.hs
+++ src/Chainweb/Pact/Backend/SQLiteCheckpointer.hs
@@ -23,47 +23,6 @@ import qualified Pact.Types.Logger as P
 
 initSQLiteCheckpointEnv = undefined
 
--- initSQLiteCheckpointEnv :: P.CommandConfig ->  P.Logger -> P.GasEnv -> IO CheckpointEnv'
--- initSQLiteCheckpointEnv cmdConfig logger gasEnv = do
---   theStore <- newIORef HMS.empty
---   return $
---     CheckpointEnv'
---       (CheckpointEnv
---          { _cpeCheckpointer =
---              Checkpointer
---                {_cRestore = restore, _cPrepare = prepare, _cSave = save}
---          , _cpeCommandConfig = cmdConfig
---          , _cpeCheckpointStore = theStore
---          , _cpeLogger = logger
---          , _cpeGasEnv = gasEnv
---          })
-
 restore = undefined
 prepare = undefined
 save = undefined
-
--- restore ::
---      C.BlockHeight
---   -> P.Hash
---   -> CheckpointData
---   -> IORef (HashMap (C.BlockHeight, P.Hash) FilePath)
---   -> IO ()
--- restore _height _hash _cdata _store = undefined
-
--- prepare ::
---      C.BlockHeight
---   -> P.Hash
---   -> OpMode
---   -> CheckpointData
---   -> IORef (HashMap (C.BlockHeight, P.Hash) FilePath)
---   -> IO (Either String (HashMap (C.BlockHeight, P.Hash) FilePath))
--- prepare _height _hash _opmode _cdata _store = undefined
-
--- save ::
---      C.BlockHeight
---   -> P.Hash
---   -> OpMode
---   -> CheckpointData
---   -> IORef (HashMap (C.BlockHeight, P.Hash) FilePath)
---   -> IO ()
--- save _height _hash _opmode _cdata _store = undefined
