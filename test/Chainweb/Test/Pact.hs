cc7e12cd5bad60ff7063c6ff3b5f50c6c8b32a23
Mark Nichols <marklnichols@gmail.com>
2019-01-08 19:25:10 -0500

    WIP - creating Pact commands for tests

--- test/Chainweb/Test/Pact.hs
+++ test/Chainweb/Test/Pact.hs
@@ -20,6 +20,7 @@ import Chainweb.Pact.Types
 import qualified Pact.Gas as P
 import qualified Pact.Interpreter as P
 import qualified Pact.Types.API as P
+import qualified Pact.Types.Crypto as P
 import qualified Pact.Types.Gas as P
 import qualified Pact.Types.Logger as P
 import qualified Pact.Types.Server as P
@@ -64,7 +65,63 @@ pactExecTests = do
     void $ runRWST execTests checkpointEnv theState
 
 execTests :: PactT ()
-execTests = undefined
+execTests = do
+    -- forM_ :: (Foldable t, Monad m) => t a -> (a -> m b) -> m ()
+    forM_ testPactRequests (\tr ->
+
+    -- need:
+    -- kps: list of P.KeyPair
+    -- for the kadena test:
+    --     _keys = [KeyPair (_ccSecretKey conf) (_ccPublicKey conf)]
+    --         where _ccSecretKey and _ccPublicKey come from:
+    --             YAML.decodeFileEither on client.yaml containing:
+    --                 publicKey: 201a45a367e5ebc8ca5bba94602419749f452a85b7e9144f29a99f3f906c0dbc
+    --                          importPublic :: ByteString -> Maybe PublicKey
+    --                        :: P.PublicKey
+    --                          -- now use PubKey instead of PublicKey ?
+    --                 secretKey: 53108fc90b19a24aa7724184e6b9c6c1d3247765be4535906342bd5f8138f7d2
+    --                        :: P.PrivateKey
+    --                          importPrivate :: ByteString -> Maybe PublicKey
+    -- code: Pact code as string
+    -- reqId: request id as Text i.e., (pack . show) on Int64
+    -- theData: Null -- :: <some a st. (ToJSON a)>
+        Pact.mkCommand
+            (map (\Pact.KeyPair {..} -> (Pact.Ed25519, _kpSecret, _kpPublic)) kps)
+            Nothing
+            reqId
+            theData
+
+
+
+execCmd :: P.Command ByteString -> TransactionOutput
+execCmd cmd = do
+
+{- -- (Kadena's mkExec)
+mkExec :: String -> Value -> Maybe Pact.Address -> Repl (Pact.Command T.Text)
+mkExec code mdata addy = do
+  kps <- use keys
+  rid <- use requestId >>= liftIO . (`modifyMVar` (\i -> return $ (succ i, i)))
+  return $ decodeUtf8 <$>
+    Pact.mkCommand
+    (map (\Pact.KeyPair {..} -> (Pact.ED25519,_kpSecret,_kpPublic)) kps)
+    addy
+    (T.pack $ show rid)
+    (Exec (ExecMsg (T.pack code) mdata))
+-}
+
+
+{-
+mkCommand :: ToJSON a => [(PPKScheme, PrivateKey, Base.PublicKey)] -> Maybe Address -> Text -> a
+                      -> Command ByteString
+mkCommand creds addy nonce a = mkCommand' creds $ BSL.toStrict $ A.encode (Payload a nonce addy)
+
+mkCommand' :: [(PPKScheme, PrivateKey, Base.PublicKey)] -> ByteString -> Command ByteString
+mkCommand' creds env = Command env (sig <$> creds) hsh
+  where
+    hsh = hash env
+    sig (scheme, sk, pk) = UserSig scheme (toB16Text $ exportPublic pk) (toB16Text $ exportSignature $ sign hsh sk pk)
+-}
+
 
 ----------------------------------------------------------------------------------------------------
 data TestRequest = TestRequest
@@ -104,6 +161,9 @@ parseScientific (Object o) =
 parseScientific _ = Nothing
 
 ----------------------------------------------------------------------------------------------------
+testPactRequests :: [TestRequest]
+testPactRequest = [testReq1]
+
 testReq1 :: TestRequest
 testReq1 = TestRequest
     { cmd = "exec (+ 1 1)"
