01d4796fe9ef902d1bee808803df8a75b9198367
Mark Nichols <marklnichols@gmail.com>
2019-01-09 15:51:08 -0500

    Added some Pact execution code to tests

--- test/Chainweb/Test/Pact.hs
+++ test/Chainweb/Test/Pact.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
 
 -- |
 -- Module: Chainweb.Test.Pact
@@ -17,31 +18,35 @@ import Chainweb.Pact.Backend.SQLiteCheckpointer
 import Chainweb.Pact.PactService
 import Chainweb.Pact.Types
 
+import qualified Pact.ApiReq as P (KeyPair(..))
 import qualified Pact.Gas as P
 import qualified Pact.Interpreter as P
 import qualified Pact.Types.API as P
+import qualified Pact.Types.Command as P
 import qualified Pact.Types.Crypto as P
 import qualified Pact.Types.Gas as P
 import qualified Pact.Types.Logger as P
+import qualified Pact.Types.RPC as P
 import qualified Pact.Types.Server as P
 
 import Control.Applicative
 import Control.Monad.Trans.RWS.Lazy
+import Control.Monad.Zip
 import Data.Aeson (Value(..))
+import Data.ByteString (ByteString)
 import Data.Functor
 import qualified Data.HashMap.Strict as HM
 import Data.Int
 import Data.Maybe
 import Data.Scientific
+import Data.Text (Text)
+import qualified Data.Text as T
 import GHC.Generics
 import Test.Tasty
 import Test.Tasty.HUnit
 
 tests :: TestTree
 tests = testCase "Pact unit tests" pactExecTests
-    -- [ testGroup "Simple Pact Execution"
-        -- [testCase "simpleExec" simpleExec]
-    -- ]
 
 pactExecTests :: IO ()
 pactExecTests = do
@@ -66,91 +71,40 @@ pactExecTests = do
 
 execTests :: PactT ()
 execTests = do
-    -- forM_ :: (Foldable t, Monad m) => t a -> (a -> m b) -> m ()
-    forM_ testPactRequests (\tr ->
+    let reqIds = [1000, 1001 ..] :: [Int64]
+    let textIds = fmap (T.pack . show) reqIds
+    let cmdStrs = fmap _trCmd testPactRequests
+    let cmds = zipWith (mkPactCommand testKeyPairs Null) textIds cmdStrs
+    _results <- execCommands cmds
+    return ()
 
-    -- need:
-    -- kps: list of P.KeyPair
-    -- for the kadena test:
-    --     _keys = [KeyPair (_ccSecretKey conf) (_ccPublicKey conf)]
-    --         where _ccSecretKey and _ccPublicKey come from:
-    --             YAML.decodeFileEither on client.yaml containing:
-    --                 publicKey: 201a45a367e5ebc8ca5bba94602419749f452a85b7e9144f29a99f3f906c0dbc
-    --                          importPublic :: ByteString -> Maybe PublicKey
-    --                        :: P.PublicKey
-    --                          -- now use PubKey instead of PublicKey ?
-    --                 secretKey: 53108fc90b19a24aa7724184e6b9c6c1d3247765be4535906342bd5f8138f7d2
-    --                        :: P.PrivateKey
-    --                          importPrivate :: ByteString -> Maybe PublicKey
-    -- code: Pact code as string
-    -- reqId: request id as Text i.e., (pack . show) on Int64
-    -- theData: Null -- :: <some a st. (ToJSON a)>
-        Pact.mkCommand
-            (map (\Pact.KeyPair {..} -> (Pact.Ed25519, _kpSecret, _kpPublic)) kps)
-            Nothing
-            reqId
-            theData
+mkPactCommand :: [P.KeyPair] -> Value -> Text -> String -> P.Command ByteString
+mkPactCommand keyPair theData reqId theCode =
+    P.mkCommand
+      (map (\P.KeyPair {..} -> (P.ED25519, _kpSecret, _kpPublic)) keyPair)
+      Nothing
+      reqId -- nonce
+      (P.Exec (P.ExecMsg (T.pack theCode) theData))
 
+testKeyPairs :: [P.KeyPair]
+testKeyPairs =
+    let mPair = mzip (P.importPrivate testPrivateBs) (P.importPublic testPublicBs)
+        mKeyPair = fmap (\(sec, pub) -> P.KeyPair {_kpSecret = sec, _kpPublic = pub} ) mPair
+    in maybeToList mKeyPair
 
+testPrivateBs :: ByteString
+testPrivateBs = "53108fc90b19a24aa7724184e6b9c6c1d3247765be4535906342bd5f8138f7d2"
 
-execCmd :: P.Command ByteString -> TransactionOutput
-execCmd cmd = do
+testPublicBs :: ByteString
+testPublicBs = "201a45a367e5ebc8ca5bba94602419749f452a85b7e9144f29a99f3f906c0dbc"
 
-{- -- (Kadena's mkExec)
-mkExec :: String -> Value -> Maybe Pact.Address -> Repl (Pact.Command T.Text)
-mkExec code mdata addy = do
-  kps <- use keys
-  rid <- use requestId >>= liftIO . (`modifyMVar` (\i -> return $ (succ i, i)))
-  return $ decodeUtf8 <$>
-    Pact.mkCommand
-    (map (\Pact.KeyPair {..} -> (Pact.ED25519,_kpSecret,_kpPublic)) kps)
-    addy
-    (T.pack $ show rid)
-    (Exec (ExecMsg (T.pack code) mdata))
--}
+execCommands :: [P.Command ByteString] -> PactT [TransactionOutput]
+execCommands _cmds = undefined
 
-
-{-
-mkCommand :: ToJSON a => [(PPKScheme, PrivateKey, Base.PublicKey)] -> Maybe Address -> Text -> a
-                      -> Command ByteString
-mkCommand creds addy nonce a = mkCommand' creds $ BSL.toStrict $ A.encode (Payload a nonce addy)
-
-mkCommand' :: [(PPKScheme, PrivateKey, Base.PublicKey)] -> ByteString -> Command ByteString
-mkCommand' creds env = Command env (sig <$> creds) hsh
-  where
-    hsh = hash env
-    sig (scheme, sk, pk) = UserSig scheme (toB16Text $ exportPublic pk) (toB16Text $ exportSignature $ sign hsh sk pk)
--}
-
-
-----------------------------------------------------------------------------------------------------
-data TestRequest = TestRequest
-    { cmd :: String
-    , eval :: TestResponse -> Assertion
-    , displayStr :: String
-    }
-
-instance Show TestRequest where
-    show tr = "cmd: " ++ cmd tr ++ "\nDisplay string: " ++ displayStr tr
-
-data TestResponse = TestResponse
-    { resultSuccess :: Bool
-    , apiResult :: P.ApiResult
-    , _batchCount :: Int64
-    } deriving (Eq, Generic)
-
-instance Show TestResponse where
-    show tr = "resultSuccess: " ++ show (resultSuccess tr) ++ "\n"
-        ++ "Batch count: " ++ show (_batchCount tr) ++ "\n"
-        ++ take 100 (show (apiResult tr)) ++ "..."
-
-----------------------------------------------------------------------------------------------------
 checkScientific :: Scientific -> TestResponse -> Assertion
 checkScientific sci tr = do
-  --resultSuccess tr `shouldBe` True
-  --parseScientific (P._arResult $ apiResult tr) `shouldBe` Just sci
-  resultSuccess tr @? "resultSuccess was not set to True"
-  parseScientific (P._arResult $ apiResult tr) @?= Just sci
+  _trResultSuccess tr @? "resultSuccess was not set to True"
+  parseScientific (P._arResult $ _trApiResult tr) @?= Just sci
 
 parseScientific :: Value -> Maybe Scientific
 parseScientific (Object o) =
@@ -161,11 +115,32 @@ parseScientific (Object o) =
 parseScientific _ = Nothing
 
 ----------------------------------------------------------------------------------------------------
+data TestRequest = TestRequest
+    { _trCmd :: String
+    , _trEval :: TestResponse -> Assertion
+    , _trDisplayStr :: String
+    }
+
+instance Show TestRequest where
+    show tr = "cmd: " ++ _trCmd tr ++ "\nDisplay string: " ++ _trDisplayStr tr
+
+data TestResponse = TestResponse
+    { _trResultSuccess :: Bool
+    , _trApiResult :: P.ApiResult
+    , _trBatchCount :: Int64
+    } deriving (Eq, Generic)
+
+instance Show TestResponse where
+    show tr = "resultSuccess: " ++ show (_trResultSuccess tr) ++ "\n"
+        ++ "Batch count: " ++ show (_trBatchCount tr) ++ "\n"
+        ++ take 100 (show (_trApiResult tr)) ++ "..."
+
+----------------------------------------------------------------------------------------------------
 testPactRequests :: [TestRequest]
-testPactRequest = [testReq1]
+testPactRequests = [testReq1]
 
 testReq1 :: TestRequest
 testReq1 = TestRequest
-    { cmd = "exec (+ 1 1)"
-    , eval = (\tr -> checkScientific (scientific 2 0) tr)
-    , displayStr = "Executes 1 + 1 in Pact and returns 2.0" }
+    { _trCmd = "exec (+ 1 1)"
+    , _trEval = (\tr -> checkScientific (scientific 2 0) tr)
+    , _trDisplayStr = "Executes 1 + 1 in Pact and returns 2.0" }
