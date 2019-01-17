eb552f3b24872cfc4ba77d1d147301718bba0d2c
Emily Pillmore <emilypi@cohomolo.gy>
2019-01-17 16:38:32 -0500

    add back 'dontCheck' override to wai-middleware metrics in default.nix (#224)

--- default.nix
+++ default.nix
@@ -117,6 +117,8 @@ in
           sha256 = "0yym840jnh2cma5n4c0pv3nh1hyhag1v6pks73wdikhrcajffsh3";
         };
 
+        wai-middleware-metrics = dontCheck super.wai-middleware-metrics;
+
         yet-another-logger = callHackageDirect {
           pkg = "yet-another-logger";
           ver = "0.3.1";
