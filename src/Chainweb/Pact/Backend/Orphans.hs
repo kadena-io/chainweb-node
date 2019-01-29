0ba937db1d5bc1b187271b6e78b23ae6c80fde06
Emmanuel Denloye-Ito <eod8tb@virginia.edu>
2019-01-28 19:00:35 -0500

    almost done filling out on-disk checkpointer. Added Orphans.hs

---
+++ src/Chainweb/Pact/Backend/Orphans.hs
@@ -0,0 +1,9 @@
+-- |
+-- Module: Chainweb.Pact.Backend.Orphans
+-- Copyright: Copyright © 2018 Kadena LLC.
+-- License: See LICENSE file
+-- Maintainer: Emmanuel Denloye-Ito <emmanuel@kadena.io>
+-- Stability: experimental
+-- Pact SQLite checkpoint module for Chainweb
+
+module Chainweb.Pact.Backend.Orphans where
--- src/Chainweb/Pact/Backend/SQLiteCheckpointer.hs
+++ src/Chainweb/Pact/Backend/SQLiteCheckpointer.hs
@@ -1,5 +1,11 @@
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeApplications #-}
 
 -- |
 -- Module: Chainweb.Pact.Backend.SQLiteCheckpointer
@@ -10,21 +16,40 @@
 -- Pact SQLite checkpoint module for Chainweb
 module Chainweb.Pact.Backend.SQLiteCheckpointer where
 
+import qualified Data.Aeson as A
 import Data.HashMap.Strict (HashMap)
 import qualified Data.HashMap.Strict as HMS
+-- import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as M
+import Data.String
+-- import Data.Time.Clock.POSIX (getPOSIXTime)
 
 import Control.Concurrent.MVar
+import Control.Exception
+import Control.Lens
+import Control.Monad.Catch
 
--- import System.Directory
-
+import qualified Pact.Persist as P
+import qualified Pact.Persist.SQLite as P
 import qualified Pact.PersistPactDb as P
 import qualified Pact.Types.Logger as P
+import qualified Pact.Types.Persistence as P
 import qualified Pact.Types.Runtime as P
 import qualified Pact.Types.Server as P
 
 -- internal modules
 import Chainweb.BlockHeader
 import Chainweb.Pact.Backend.Types
+
+
+data DataToFill = DataToFill
+  { _dtfTxRecord :: M.Map P.TxTable [P.TxLog A.Value]
+  , _dtfTxId :: Maybe P.TxId
+  , _dtfSQLiteConfig :: P.SQLiteConfig
+  , _dtfCommandState :: P.CommandState
+  }
+
+makeLenses ''DataToFill
 
 initSQLiteCheckpointEnv :: P.CommandConfig -> P.Logger -> P.GasEnv -> IO CheckpointEnv
 initSQLiteCheckpointEnv cmdConfig logger gasEnv = do
@@ -43,7 +68,27 @@ initSQLiteCheckpointEnv cmdConfig logger gasEnv = do
 
 type Store = HashMap (BlockHeight, BlockPayloadHash) DataToFill
 
-data DataToFill = DataToFill
+changeSQLFilePath ::
+       FilePath
+    -> (FilePath -> FilePath -> FilePath)
+    -> P.SQLiteConfig
+    -> P.SQLiteConfig
+changeSQLFilePath fp f (P.SQLiteConfig dbFile pragmas) =
+    P.SQLiteConfig (f fp dbFile) pragmas
+
+reinitDbEnv :: P.Loggers -> P.Persister P.SQLite -> DataToFill -> IO PactDbState
+reinitDbEnv loggers funrec (DataToFill {..}) = do
+  _db <- P.initSQLite _dtfSQLiteConfig loggers
+  return (PactDbState (EnvPersist' (PactDbEnvPersist undefined (P.DbEnv {..}))) _dtfCommandState)
+  where
+    _persist = funrec
+    _logger = P.newLogger loggers (fromString "<to fill with something meaningful>")
+    _txRecord = _dtfTxRecord
+    _txId = _dtfTxId
+
+data SQLiteCheckpointException = RestoreNotFoundException deriving Show
+
+instance Exception SQLiteCheckpointException
 
 -- This should open a connection with the assumption that there is not
 --  any connection open. There should be tests that assert this
@@ -51,33 +96,30 @@ data DataToFill = DataToFill
 restore' :: MVar Store -> BlockHeight -> BlockPayloadHash -> IO PactDbState
 restore' lock height hash = do
     withMVarMasked lock $ \store -> do
+
       case HMS.lookup (height, hash) store of
+
         Just cdata -> do
-          let dbstate = tostate cdata
+
+          -- Open a database connection.
+          dbstate <- reinitDbEnv P.neverLog P.persister cdata
           case _pdbsDbEnv dbstate of
             EnvPersist' (PactDbEnvPersist {..}) ->
               case _pdepEnv of
                 P.DbEnv {..} -> openDb _db
           return dbstate
 
-        -- This is just a placeholder for right now (the Nothing clause)
-        Nothing ->
-          fail
-            "InMemoryCheckpointer.restore: There is no checkpoint that can be restored."
-  where
-    tostate = undefined
+        Nothing -> throwM RestoreNotFoundException
 
 -- Prepare/Save should change the field 'dbFile' (the filename of the
 -- current database) of SQLiteConfig so that the retrieval of the
 -- database (referenced by the aforementioned filename) is possible in
 -- a 'restore'.
 
-prepareForValidBlock ::
-       MVar Store -> BlockHeight -> BlockPayloadHash -> IO (Either String PactDbState)
+-- prepareForValidBlock :: MVar Store -> BlockHeight -> BlockPayloadHash -> IO (Either String PactDbState)
 prepareForValidBlock = undefined
 
-prepareForNewBlock ::
-       MVar Store -> BlockHeight -> BlockPayloadHash -> IO (Either String PactDbState)
+-- prepareForNewBlock :: MVar Store -> BlockHeight -> BlockPayloadHash -> IO (Either String PactDbState)
 prepareForNewBlock = undefined
 
 -- This should close the database connection currently open upon
@@ -85,14 +127,13 @@ prepareForNewBlock = undefined
 -- throw an error) before departure from this function. There should
 -- be tests that assert this essential aspect of the 'save' semantics.
 save' :: MVar Store -> BlockHeight -> BlockPayloadHash -> PactDbState -> IO ()
-save' lock height hash PactDbState {..} = do
-
+save' lock height hash PactDbState {..}
     -- Saving off checkpoint.
-    let datatofill = DataToFill
-    modifyMVarMasked_ lock (return . HMS.insert (height, hash) datatofill)
-
+ = do
+  let datatofill = DataToFill undefined undefined undefined undefined
+  modifyMVarMasked_ lock (return . HMS.insert (height, hash) datatofill)
     -- Closing database connection.
-    case _pdbsDbEnv of
-      EnvPersist' (PactDbEnvPersist {..}) ->
-        case _pdepEnv of
-          P.DbEnv {..} -> closeDb _db
+  case _pdbsDbEnv of
+    EnvPersist' (PactDbEnvPersist {..}) ->
+      case _pdepEnv of
+        P.DbEnv {..} -> closeDb _db
