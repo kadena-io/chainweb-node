37f8b9f2b7747fc24557eeb13c7e68ab98952e2c
Gregory Collins <greg@gregorycollins.net>
2019-01-30 09:11:13 -0500

    [block-payloads] generalize FixedThreadPool functions to traversable

--- src/Control/Concurrent/FixedThreadPool.hs
+++ src/Control/Concurrent/FixedThreadPool.hs
@@ -25,7 +25,6 @@ import Control.Concurrent.STM.TBMChan (TBMChan)
 import qualified Control.Concurrent.STM.TBMChan as TBMChan
 import Control.Exception
 import Control.Monad (forever, void, when)
-import Data.Foldable
 import Data.IORef
 import Data.Maybe (isJust)
 import Data.Vector (Vector)
@@ -45,6 +44,7 @@ data ThreadPool = ThreadPool {
   , _joins :: Vector (IO ())
   , _chan :: Chan
   }
+
 
 ------------------------------------------------------------------------------
 newThreadPool :: Int -> IO ThreadPool
@@ -66,6 +66,7 @@ newThreadPool n = do
           Nothing -> throwIO ThreadKilled
           (Just act) -> performAction act
 
+
 -- | Runs an IO action asynchronously. Returns an 'Action' you can wait on with
 -- 'waitAction'.
 runAction :: ThreadPool -> IO () -> IO Action
@@ -75,8 +76,10 @@ runAction (ThreadPool _ _ chan) action = do
     atomically $ TBMChan.writeTBMChan chan actData
     return actData
 
+
 waitAction :: Action -> IO (Maybe SomeException)
 waitAction (Action _ _ wait) = wait
+
 
 -- | Run a group of actions and wait for them all to finish
 data Group = Group {
@@ -85,8 +88,10 @@ data Group = Group {
   , _groupDone :: MVar (Maybe SomeException)
   }
 
+
 newGroup :: Int -> IO Group
 newGroup k = Group <$> newMVar k <*> newIORef Nothing <*> newEmptyMVar
+
 
 doneGroup :: Group -> Maybe SomeException -> IO ()
 doneGroup (Group sema gerr done) mberr = mask_ $ do
@@ -98,43 +103,48 @@ doneGroup (Group sema gerr done) mberr = mask_ $ do
     dec !k = let !k' = k - 1
              in return (k', k' == 0)
 
+
 waitGroup :: Group -> IO (Maybe SomeException)
 waitGroup (Group _ _ done) = readMVar done
+
 
 -- | Runs a group of IO actions and waits for them to finish. If any of the IO
 -- actions throws an exception, 'runActionGroup' will return it. If more than
 -- one action throws an exception, 'runActionGroup' will arbitrarily return one
 -- of the exceptions. The 'runActionGroup' function will wait until all of the
 -- actions have run, even if an exception is thrown.
-runActionGroup :: Foldable t
+runActionGroup :: Traversable t
                => ThreadPool
                -> t (IO ())
                -> IO (Maybe SomeException)
-runActionGroup (ThreadPool _ _ chan) actions0 =
+runActionGroup (ThreadPool _ _ chan) actions =
     if len == 0 then return Nothing else go
   where
-    actions = toList actions0
     len = length actions
 
     go = do
         g <- newGroup len
-        let actData = map (toAction g) actions
+        let actData = fmap (toAction g) actions
         mapM_ (atomically . TBMChan.writeTBMChan chan) actData
         waitGroup g
 
     toAction g a = Action a (doneGroup g) (waitGroup g)
 
+
 stopThreadPool :: ThreadPool -> IO ()
 stopThreadPool (ThreadPool _ _ chan) =
     atomically $ TBMChan.closeTBMChan chan
+
 
 killThreadPool :: ThreadPool -> IO ()
 killThreadPool tp@(ThreadPool threads _ _) = do
     stopThreadPool tp
     V.mapM_ killThread threads
 
+
 waitThreadPool :: ThreadPool -> IO ()
 waitThreadPool (ThreadPool _ waits _) = V.sequence_ waits
+
 
 withThreadPool :: Int -> (ThreadPool -> IO a) -> IO a
 withThreadPool k userFunc = bracket create destroy userFunc
@@ -142,24 +152,31 @@ withThreadPool k userFunc = bracket create destroy userFunc
     create = newThreadPool k
     destroy tp = stopThreadPool tp >> waitThreadPool tp
 
-mapAction :: ThreadPool
+
+mapAction :: Traversable t
+          => ThreadPool
           -> (a -> IO b)
-          -> [a]
-          -> IO [Either SomeException b]
-mapAction tp userFunc xs0 = do
-    mvs <- mapM (const newEmptyMVar) xs
-    let vals = xs `zip` mvs
-    let actions = map toAction vals
+          -> t a
+          -> IO (t (Either SomeException b))
+mapAction tp userFunc xs = do
+    vals <- mapM zipMV xs
+    let mvs = fmap snd vals
+    let actions = fmap toAction vals
     mapM_ (runAction tp) actions
     mapM takeMVar mvs
+
   where
-    xs = toList xs0
+    zipMV x = do
+        mv <- newEmptyMVar
+        return $! (x, mv)
     toAction (x, mv) = try (userFunc x) >>= putMVar mv
 
-mapAction_ :: ThreadPool
-          -> (a -> IO b)
-          -> [a]
-          -> IO ()
+
+mapAction_ :: Traversable t
+           => ThreadPool
+           -> (a -> IO b)
+           -> t a
+           -> IO ()
 mapAction_ tp userFunc xs0 = do
     e <- sequence_ <$> mapAction tp (void . userFunc) xs0
     either throwIO return e
