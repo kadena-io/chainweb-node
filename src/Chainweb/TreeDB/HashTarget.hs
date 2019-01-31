aa91956132b67ad9c349bda8c67d1fa3256918ef
Colin Woodbury <colin@fosskers.ca>
2019-01-31 15:28:53 -0800

    [difficulty] Use a Stream

--- src/Chainweb/TreeDB/HashTarget.hs
+++ src/Chainweb/TreeDB/HashTarget.hs
@@ -1,5 +1,7 @@
-{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE MultiWayIf #-}
+{-# LANGUAGE TypeFamilies #-}
 
 -- |
 -- Module: Chainweb.TreeDB.HashTarget
@@ -12,11 +14,16 @@ module Chainweb.TreeDB.HashTarget
   ( hashTargetFromHistory
   ) where
 
+import Data.Function ((&))
 import Data.Int (Int64)
-import Data.List.NonEmpty (NonEmpty)
 import qualified Data.List.NonEmpty as NEL
+import Data.Semigroup (Max(..), Min(..))
+import qualified Data.HashSet as HS
 
-import Numeric.AffineSpace (Diff, diff)
+import Numeric.Additive (invert)
+import Numeric.AffineSpace (Diff, add, diff)
+
+import qualified Streaming.Prelude as P
 
 -- internal modules
 
@@ -45,28 +52,35 @@ A `BlockHashNat` is a newtype around a `Word256`, which comes from the `data-dwo
 -- in a window which goes back the given amount of time from its parent.
 --
 hashTargetFromHistory
-    :: Monad m
-    => TreeDb db
+    :: TreeDb db
     => DbEntry db ~ BlockHeader  -- TODO Use `IsBlockHeader` once Git Store PR is merged.
     => db
     -> DbEntry db
     -> TimeSpan Int64
-    -> m HashTarget
-hashTargetFromHistory db bh dt = do
-    es <- lookupTimeSpanFrom db bh dt
-    let !targetDeltas = zipWith (\x y -> (_blockTarget x, timeDelta x y)) (NEL.toList es) $ NEL.tail es
-        !start = _blockCreationTime $ NEL.head es
+    -> IO HashTarget
+hashTargetFromHistory db bh ts = do
+    es <- branchEntries db Nothing Nothing minr maxr lower upper
+          & P.takeWhile (\h -> _blockCreationTime h > time)
+          & P.toList_
+          & fmap NEL.fromList
+
+    let deltas :: [(HashTarget, TimeSpan Int64)]
+        !deltas = zipWith (\x y -> (_blockTarget x, timeDelta x y)) (NEL.toList es) $ NEL.tail es
+
+    let !start = _blockCreationTime $ NEL.head es
         !end = _blockCreationTime bh
-    pure $! calculateTarget (diff end start) targetDeltas
+
+    -- calculateTarget :: Integral a => TimeSpan a -> [(HashTarget, TimeSpan a)] -> HashTarget
+    -- TODO Why is passing the original @ts :: TimeSpan Int64@ not sufficient?
+    pure $! calculateTarget (diff end start) deltas
   where
     timeDelta :: BlockHeader -> BlockHeader -> Diff (Time Int64)
     timeDelta x y = diff (_blockCreationTime y) (_blockCreationTime x)
 
-lookupTimeSpanFrom
-    :: TreeDb db
-    => DbEntry db ~ BlockHeader
-    => db
-    -> DbEntry db
-    -> TimeSpan Int64
-    -> m (NonEmpty (DbEntry db))
-lookupTimeSpanFrom = undefined
+    time :: Time Int64
+    time = invert ts `add` _blockCreationTime bh
+
+    minr = Just . MinRank $ Min 0
+    maxr = Just . MaxRank . Max . fromIntegral $ _blockHeight bh
+    lower = HS.empty
+    upper = HS.singleton . UpperBound $ key bh
