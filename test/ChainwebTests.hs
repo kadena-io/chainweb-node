165dc461498a5e46a309e705ed717a3f38d09b6e
Lars Kuhtz <lars@kadena.io>
2019-02-13 23:03:35 -0800

    run all tests in the same process

--- test/ChainwebTests.hs
+++ test/ChainwebTests.hs
@@ -13,6 +13,8 @@
 
 module Main ( main ) where
 
+import System.LogLevel
+
 import Test.Tasty
 import Test.Tasty.QuickCheck
 
@@ -25,6 +27,8 @@ import qualified Chainweb.Test.DiGraph
 import qualified Chainweb.Test.Mempool.InMem
 import qualified Chainweb.Test.Mempool.Socket
 import qualified Chainweb.Test.Mempool.Websocket
+import qualified Chainweb.Test.MultiNode
+import qualified Chainweb.Test.Pact
 import qualified Chainweb.Test.RestAPI
 import qualified Chainweb.Test.Roundtrips
 import qualified Chainweb.Test.Store.CAS.FS
@@ -33,6 +37,7 @@ import qualified Chainweb.Test.TreeDB.Persistence
 import qualified Chainweb.Test.TreeDB.RemoteDB
 import qualified Chainweb.Test.TreeDB.Sync
 import qualified Chainweb.Utils.Paging (properties)
+import Chainweb.Version
 
 import qualified Data.DiGraph (properties)
 import qualified Data.Word.Encoding (properties)
@@ -41,34 +46,38 @@ import qualified Network.X509.SelfSigned.Test
 
 import qualified P2P.Node.PeerDB (properties)
 
----
-
 main :: IO ()
 main = defaultMain suite
 
 suite :: TestTree
-suite = testGroup "Unit Tests"
-    [ testGroup "BlockHeaderDb"
-        [ Chainweb.Test.BlockHeaderDB.tests
-        , Chainweb.Test.TreeDB.RemoteDB.tests
-        , Chainweb.Test.TreeDB.Persistence.tests
-        , Chainweb.Test.TreeDB.Sync.tests
+suite = testGroup "ChainwebTests"
+    [ testGroup "Unit Tests"
+        [ testGroup "BlockHeaderDb"
+            [ Chainweb.Test.BlockHeaderDB.tests
+            , Chainweb.Test.TreeDB.RemoteDB.tests
+            , Chainweb.Test.TreeDB.Persistence.tests
+            , Chainweb.Test.TreeDB.Sync.tests
+            ]
+        , Chainweb.Test.Store.CAS.FS.tests
+        , Chainweb.Test.Store.Git.tests
+        , Chainweb.Test.Roundtrips.tests
+        , Chainweb.Test.RestAPI.tests
+        , Chainweb.Test.DiGraph.tests
+        , Chainweb.Test.Mempool.InMem.tests
+        , Chainweb.Test.Mempool.Socket.tests
+        , Chainweb.Test.Mempool.Websocket.tests
+        , testProperties "Chainweb.BlockHeaderDb.RestAPI.Server" Chainweb.Utils.Paging.properties
+        , testProperties "Chainweb.HostAddress" Chainweb.HostAddress.properties
+        , testProperties "P2P.Node.PeerDB" P2P.Node.PeerDB.properties
+        , testProperties "Data.DiGraph" Data.DiGraph.properties
+        , testGroup "Network.X05.SelfSigned.Test"
+            [ Network.X509.SelfSigned.Test.tests
+            ]
+        , testProperties "Chainweb.Difficulty" Chainweb.Difficulty.properties
+        , testProperties "Data.Word.Encoding" Data.Word.Encoding.properties
         ]
-    , Chainweb.Test.Store.CAS.FS.tests
-    , Chainweb.Test.Store.Git.tests
-    , Chainweb.Test.Roundtrips.tests
-    , Chainweb.Test.RestAPI.tests
-    , Chainweb.Test.DiGraph.tests
-    , Chainweb.Test.Mempool.InMem.tests
-    , Chainweb.Test.Mempool.Socket.tests
-    , Chainweb.Test.Mempool.Websocket.tests
-    , testProperties "Chainweb.BlockHeaderDb.RestAPI.Server" Chainweb.Utils.Paging.properties
-    , testProperties "Chainweb.HostAddress" Chainweb.HostAddress.properties
-    , testProperties "P2P.Node.PeerDB" P2P.Node.PeerDB.properties
-    , testProperties "Data.DiGraph" Data.DiGraph.properties
-    , testGroup "Network.X05.SelfSigned.Test"
-        [ Network.X509.SelfSigned.Test.tests
-        ]
-    , testProperties "Chainweb.Difficulty" Chainweb.Difficulty.properties
-    , testProperties "Data.Word.Encoding" Data.Word.Encoding.properties
+
+    , Chainweb.Test.MultiNode.test Warn TestWithTime 10 (Chainweb.Test.MultiNode.Seconds 120) Nothing
+    , Chainweb.Test.Pact.tests
     ]
+
