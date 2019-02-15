fe3a047aea312bae5ef10c4fd0344ceef6bba482
Colin Woodbury <colin@fosskers.ca>
2019-02-15 11:03:21 -0800

    [difficulty] Full justification for `hashTarget` algorithm

--- src/Chainweb/TreeDB/HashTarget.hs
+++ src/Chainweb/TreeDB/HashTarget.hs
@@ -12,7 +12,8 @@
 -- Stability: experimental
 --
 module Chainweb.TreeDB.HashTarget
-  ( BlockRate(..)
+  ( -- * Epoch-based Adjustment
+    BlockRate(..)
   , WindowWidth(..)
   , hashTarget
   ) where
@@ -54,12 +55,123 @@ newtype BlockRate = BlockRate Natural
 
 -- | The number of blocks to be mined after a difficulty adjustment, before
 -- considering a further adjustment. Critical for the "epoch-based" adjustment
--- algorithm seen in `hashTarget.`
+-- algorithm seen in `hashTarget`.
 --
 newtype WindowWidth = WindowWidth Natural
 
 -- | A potentially new `HashTarget`, based on the rate of mining success over
 -- the previous N blocks.
+--
+-- == Epoch-based Difficulty Adjustment
+--
+-- This function represents a Bitcoin-inspired, "epoch-based" adjustment
+-- algorithm. For every N blocks (as defined by `WindowWidth`), we perform an
+-- adjustment.
+--
+-- === Terminology
+--
+-- `BlockHeader` stores a 256-bit measure of difficulty: `HashTarget`. More
+-- precisely, `HashTarget` is a derivation (seen below) of the `HashDifficulty`.
+-- `HashDifficulty` in itself is roughly a measure of the number of hashes
+-- necessary to "solve" a block. For non-POW testing scenarios that use trivial
+-- targets (i.e. `maxBound`), then difficulty is exactly the number of necessary
+-- hashes. For POW mining, this is offset. See `maxTarget`.
+--
+-- A `HashDifficulty` of 1 is considered the "easiest" difficulty, and
+-- represents a `HashTarget` of `maxTarget`. There must never be a difficulty of
+-- 0.
+--
+-- Given the same `Chainweb.Version.ChainwebVersion`, the functions
+-- `targetToDifficulty` and `difficultyToTarget` form an isomorphism between the
+-- above mentioned types.
+--
+-- === Justification
+--
+-- We define the maximum possible hash target (the "easiest" target) as follows:
+--
+-- \[
+-- \begin{align*}
+--   \text{MaxBound} &= 2^{256} - 1 \\
+--   \text{MaxTarget} &= \frac{\text{MaxBound}}{2^{\text{offset}}}
+-- \end{align*}
+-- \]
+--
+-- where /offset/ is some number of bits, 0 for trivial scenarios and some
+-- experimentally discovered \(N\) for real POW mining scenarios. For Bitcoin,
+-- \(N = 32\).
+--
+-- Given some difficulty \(D\), its corresponding `HashTarget` can be found by:
+--
+-- \[
+-- \text{Target} = \frac{\text{maxTarget}}{D}
+-- \]
+--
+-- During adjustment, we seek to solve for some new \(D\). From the above, it
+-- follows that the expected number of hashes necessary to "solve" a block
+-- becomes:
+--
+-- \[
+-- \text{Expected} = \frac{D * \text{MaxBound}}{\text{MaxTarget}}
+-- \]
+--
+-- If we expect a block to be solved every \(R\) seconds, we find our total
+-- Network Hash Rate:
+--
+-- \[
+-- \text{HashRate} = \frac{\text{Expected}}{R}
+-- \]
+--
+-- But, as a block chain is a dynamic system, the real time it took to mine some
+-- block would likely not be exactly \(R\). This implies:
+--
+-- \[
+-- \begin{align*}
+--   \frac{\text{Expected}}{R} &= \text{HashRate} = \frac{\text{Expected}'}{M} \\
+--   \frac{D * \text{MaxBound}}{R * \text{MaxTarget}} &= \text{HashRate} = \frac{D' * \text{MaxBound}}{M * \text{MaxTarget}} \\
+--   \frac{D}{R} &= \text{HashRate} = \frac{D'}{M}
+-- \end{align*}
+-- \]
+--
+-- where \(D'\) is the known difficulty from the previous block, \(M\) is the
+-- average time in seconds it took to calculate the previous \(B\) blocks. The
+-- value of \(B\) is assumed to be configurable.
+--
+-- Given this, our new \(D\) is a simple ratio:
+--
+-- \[
+-- D = \frac{D' * R}{M}
+-- \]
+--
+-- /HashRate/ will of course not stay fixed as the network grows. Luckily, the
+-- difference in \(M\) values will naturally correct for this in the calculation
+-- of a new \(D\).
+--
+-- === Precision
+--
+-- In real systems, the difference between \(M\) and \(R\) may be minute. To
+-- ensure that:
+--
+--   * differences are not lost to integer-math rounding errors
+--   * adjustment actually occurs
+--   * small, incremental adjustments are allowed to build into greater change over time
+--   * `Word256`-based overflows do not occur
+--
+-- we use the infinite-precision `Rational` type in the calculation of the new
+-- \(D\). Only when being converted to a final `HashTarget` is the non-integer
+-- precision discarded.
+--
+-- /Note/: Use of `Rational` is likely not our final solution, and complicates
+-- any cross-language spec we would write regarding adjustment algorithm
+-- expectations. For now, however, `Rational` is stable for a Haskell-only
+-- environment.
+--
+-- === Adjustment Limits
+--
+-- Spikes in /HashRate/ may occur as the mining network grows. To ensure that
+-- adjustment does not occur too quickly, we cap the total "significant bits of
+-- change" as to no more than 3 bits in either the "harder" or "easier" direction
+-- at one time. Experimentally, it has been shown than the maximum change should
+-- be greater than \(e = 2.71828\cdots\) (/source needed/).
 --
 hashTarget
     :: forall db. TreeDb db
