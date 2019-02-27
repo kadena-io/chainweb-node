96d78118c93cc0b0eba2a997326a4a441a01dd16
Colin Woodbury <colin@fosskers.ca>
2019-02-27 06:41:54 -0800

    [genesis] Refactor with `unsafeFromYamlText`

--- src/Chainweb/BlockHeader/Genesis.hs
+++ src/Chainweb/BlockHeader/Genesis.hs
@@ -26,6 +26,7 @@ module Chainweb.BlockHeader.Genesis
 
 
 import Data.Maybe (fromJust)
+import Data.Text (Text)
 import qualified Data.Text.Encoding as T
 import qualified Data.Yaml as Yaml
 
@@ -84,8 +85,11 @@ visually (as mentioned above).
 
 -}
 
+unsafeFromYamlText :: Text -> BlockHeader
+unsafeFromYamlText = _objectEncoded . fromJust . Yaml.decodeThrow . T.encodeUtf8
+
 testnet00C0 :: BlockHeader
-testnet00C0 = _objectEncoded . fromJust . Yaml.decodeThrow $ T.encodeUtf8
+testnet00C0 = unsafeFromYamlText
     [text|
 creationTime: 1551207336601038
 parent: hkY3tAJOaRSSTG5DUYBEMRjNlZr2jEyA_8d0_NJ76ow
@@ -107,7 +111,7 @@ nonce: 783
     |]
 
 testnet00C1 :: BlockHeader
-testnet00C1 = _objectEncoded . fromJust . Yaml.decodeThrow $ T.encodeUtf8
+testnet00C1 = unsafeFromYamlText
     [text|
 creationTime: 1551207336601038
 parent: gSDXx0M9qJg03BU2zi1jDGo0n8lHhcojup27cl5bVtM
@@ -129,7 +133,7 @@ nonce: 269
     |]
 
 testnet00C2 :: BlockHeader
-testnet00C2 = _objectEncoded . fromJust . Yaml.decodeThrow $ T.encodeUtf8
+testnet00C2 = unsafeFromYamlText
     [text|
 creationTime: 1551207336601038
 parent: zBD6jyT5Irr5QIcNoDw48_aN8TcPI7-HgHJBYm_ra18
@@ -151,7 +155,7 @@ nonce: 380
     |]
 
 testnet00C3 :: BlockHeader
-testnet00C3 = _objectEncoded . fromJust . Yaml.decodeThrow $ T.encodeUtf8
+testnet00C3 = unsafeFromYamlText
     [text|
 creationTime: 1551207336601038
 parent: iliOelarez9K7DNE1Je8V_TczJAgJh4dB9Pm3WgKbMQ
@@ -173,7 +177,7 @@ nonce: 113
     |]
 
 testnet00C4 :: BlockHeader
-testnet00C4 = _objectEncoded . fromJust . Yaml.decodeThrow $ T.encodeUtf8
+testnet00C4 = unsafeFromYamlText
     [text|
 creationTime: 1551207336601038
 parent: E8g1sAZD7xvIRLiqF-TmQ6YwIh1lUxmIpSzaJb9F8WM
@@ -195,7 +199,7 @@ nonce: 1205
     |]
 
 testnet00C5 :: BlockHeader
-testnet00C5 = _objectEncoded . fromJust . Yaml.decodeThrow $ T.encodeUtf8
+testnet00C5 = unsafeFromYamlText
     [text|
 creationTime: 1551207336601038
 parent: jRBryPOLRqBKjceQXsRuLp6Q9mMqrZmCW3vQ3XgDtts
@@ -217,7 +221,7 @@ nonce: 183
     |]
 
 testnet00C6 :: BlockHeader
-testnet00C6 = _objectEncoded . fromJust . Yaml.decodeThrow $ T.encodeUtf8
+testnet00C6 = unsafeFromYamlText
     [text|
 creationTime: 1551207336601038
 parent: xSXQP0riuw-DDRLz-BEdw7Vn7C8c8ICwlQK_DwhE18Q
@@ -239,7 +243,7 @@ nonce: 1966
     |]
 
 testnet00C7 :: BlockHeader
-testnet00C7 = _objectEncoded . fromJust . Yaml.decodeThrow $ T.encodeUtf8
+testnet00C7 = unsafeFromYamlText
     [text|
 creationTime: 1551207336601038
 parent: wHYmEOiBrC3l7ZaZdrr2Nr1ClvsA6WdS3Tps20HfvjY
@@ -261,7 +265,7 @@ nonce: 439
     |]
 
 testnet00C8 :: BlockHeader
-testnet00C8 = _objectEncoded . fromJust . Yaml.decodeThrow $ T.encodeUtf8
+testnet00C8 = unsafeFromYamlText
     [text|
 creationTime: 1551207336601038
 parent: -acx5PNzURsOtqhJKm08Zf9FchU7FDs64cKVqA5Vm0A
@@ -283,7 +287,7 @@ nonce: 307
     |]
 
 testnet00C9 :: BlockHeader
-testnet00C9 = _objectEncoded . fromJust . Yaml.decodeThrow $ T.encodeUtf8
+testnet00C9 = unsafeFromYamlText
     [text|
 creationTime: 1551207336601038
 parent: CY9Uo83VT4g_RJar_lLItK_MpWvl4e4yHsY1i2KXuBk
