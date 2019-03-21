5b5c37b2e869a5c2184c3db634d7361286428e1c
Colin Woodbury <colin@fosskers.ca>
2019-03-21 11:14:35 -0700

    [ea] Move where `NoImplicitPrelude` is defined [skip ci] (#34)

--- tools/ea/Ea.hs
+++ tools/ea/Ea.hs
@@ -3,6 +3,7 @@
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TypeOperators #-}
 
