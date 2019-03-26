19698e330fb6de89c662cc4e992d172a5b0fbb2f
Gregory Collins <greg@gregorycollins.net>
2019-03-26 16:46:15 -0400

    [mempool,pactEx] mark mempool txs as confirmed when we call pactValidate. (#72)

--- src/Chainweb/WebPactExecutionService.hs
+++ src/Chainweb/WebPactExecutionService.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE BangPatterns #-}
 
 module Chainweb.WebPactExecutionService
   ( WebPactExecutionService(..)
@@ -9,14 +10,20 @@ module Chainweb.WebPactExecutionService
 import Control.Concurrent.MVar
 import Control.Concurrent.STM.TQueue
 import Control.Monad.Catch
+import qualified Data.Either as Either
+import Data.Foldable
 import qualified Data.HashMap.Strict as HM
+import qualified Data.Vector as V
 
 import Chainweb.BlockHeader
-import Chainweb.Payload
 import Chainweb.ChainId
+import Chainweb.Mempool.Mempool
 import Chainweb.Pact.Service.BlockValidation
-import Chainweb.Pact.Types
 import Chainweb.Pact.Service.Types
+import Chainweb.Pact.Types
+import Chainweb.Payload
+import Chainweb.Transaction
+import Chainweb.Utils (codecDecode)
 
 data PactExecutionService = PactExecutionService
   { _pactValidateBlock :: BlockHeader -> PayloadData -> IO PayloadWithOutputs
@@ -37,13 +44,13 @@ mkWebPactExecutionService hm = WebPactExecutionService $ PactExecutionService
           Just p -> act p
           Nothing -> throwM (userError $ "PactExecutionService: Invalid chain ID in header: " ++ show h)
 
-mkPactExecutionService :: TQueue RequestMsg -> PactExecutionService
-mkPactExecutionService q = PactExecutionService
+mkPactExecutionService :: MempoolBackend ChainwebTransaction -> TQueue RequestMsg -> PactExecutionService
+mkPactExecutionService mempool q = PactExecutionService
   { _pactValidateBlock = \h pd -> do
       mv <- validateBlock h pd q
       r <- takeMVar mv
       case r of
-        Right pdo -> return pdo
+        Right pdo -> markAllConfirmed mempool pdo >> return pdo
         Left e -> throwM e
   , _pactNewBlock = \m h -> do
       mv <- newBlock m h q
@@ -52,3 +59,19 @@ mkPactExecutionService q = PactExecutionService
         Right pdo -> return pdo
         Left e -> throwM e
   }
+
+-- TODO: to support mempool transaction reintroduction we need to hook into
+-- consensus instead of just killing every tx that ever made it into a valid
+-- block
+markAllConfirmed
+    :: MempoolBackend ChainwebTransaction
+    -> PayloadWithOutputs
+    -> IO ()
+markAllConfirmed mempool payload = mempoolMarkConfirmed mempool txHashes
+  where
+    txcfg = mempoolTxConfig mempool
+    decodeTx = codecDecode $ txCodec txcfg
+    decodedTxs = Either.rights $ fmap (decodeTx . _transactionBytes . fst)
+                   $ toList $ _payloadWithOutputsTransactions payload
+    !txHashes = V.fromList $ map (txHasher txcfg) decodedTxs
+
