d3bbf4beaf1358e5c47af6da74c516ba0b015261
Doug Beardsley <mightybyte@gmail.com>
2019-03-27 03:11:39 -0400

    Fix typo of future-work.md in gitignore

--- default.nix
+++ default.nix
@@ -237,7 +237,7 @@ in
       };
     packages = {
       chainweb = gitignore.gitignoreSource
-        [".git" ".gitlab-ci.yml" "CHANGELOG.md" "README.md" "future-ork.md"] ./.;
+        [".git" ".gitlab-ci.yml" "CHANGELOG.md" "README.md" "future-work.md"] ./.;
     };
     shellToolOverrides = ghc: super: {
       stack = pkgs.stack;
