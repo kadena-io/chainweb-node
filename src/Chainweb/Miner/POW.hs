e7a63011279cd2032de3e789a897f53c78c1b225
Lars Kuhtz <lars@kadena.io>
2019-04-03 18:34:37 -0700

    cleanup and document POW miner

--- src/Chainweb/Miner/POW.hs
+++ src/Chainweb/Miner/POW.hs
@@ -24,7 +24,6 @@ module Chainweb.Miner.POW
 -- * Internal
 , mineCut
 , mine
-, mineFast
 ) where
 
 import Control.Concurrent.Async
@@ -116,13 +115,11 @@ powMiner logFun conf nid cutDb = runForever logFun "POW Miner" $ do
         -> IO ()
     go gen !i !adjustments0 = do
 
-        nonce0 <- Nonce <$> MWC.uniform gen
-
         -- Mine a new Cut
         --
         c <- _cut cutDb
         T3 newBh c' adjustments' <- do
-            let go2 !x = race (awaitNextCut cutDb x) (mineCut @cas logFun conf nid cutDb x nonce0 adjustments0) >>= \case
+            let go2 !x = race (awaitNextCut cutDb x) (mineCut @cas logFun conf nid cutDb gen x adjustments0) >>= \case
                     Left c' -> go2 c'
                     Right !r -> return r
             go2 c
@@ -169,11 +166,11 @@ mineCut
     -> MinerConfig
     -> NodeId
     -> CutDb cas
+    -> MWC.GenIO
     -> Cut
-    -> Nonce
     -> Adjustments
     -> IO (T3 BlockHeader Cut Adjustments)
-mineCut logfun conf nid cutDb !c !nonce !adjustments = do
+mineCut logfun conf nid cutDb gen !c !adjustments = do
 
     -- Randomly pick a chain to mine on.
     --
@@ -189,7 +186,7 @@ mineCut logfun conf nid cutDb !c !nonce !adjustments = do
     --
     case getAdjacentParents c p of
 
-        Nothing -> mineCut logfun conf nid cutDb c nonce adjustments
+        Nothing -> mineCut logfun conf nid cutDb gen c adjustments
             -- spin until a chain is found that isn't blocked
 
         Just adjParents -> do
@@ -203,16 +200,18 @@ mineCut logfun conf nid cutDb !c !nonce !adjustments = do
 
             -- Assemble block without Nonce and Timestamp
             --
+            creationTime <- getCurrentTimeIntegral
+            nonce <- Nonce <$> MWC.uniform gen
             let candidateHeader = newBlockHeader
                     (nodeIdFromNodeId nid cid)
                     adjParents
                     (_payloadWithOutputsPayloadHash payload)
-                    (Nonce 0) -- preliminary
+                    nonce
                     target
-                    epoche -- preliminary
+                    creationTime
                     p
 
-            newHeader <- (usePowHash v mineFast) candidateHeader nonce
+            newHeader <- (usePowHash v mine) candidateHeader nonce
 
             -- create cut with new block
             --
@@ -292,39 +291,8 @@ getAdjacentParents c p = BlockHashRecord <$> newAdjHashes
         | _blockHeight b == h + 1 = Just $! _blockParent b
         | otherwise = Nothing
 
--- | Run a mining loop. Updates creation time and nonce.
--- The result is guaranteed to have a valid nonce.
---
-mine :: BlockHeader -> Nonce -> IO BlockHeader
-mine h nonce = do
-    ct <- getCurrentTimeIntegral
-    go (0 :: Int) nonce $ injectTime ct $ runPutS $ encodeBlockHeaderWithoutHash h
-  where
-    target = _blockTarget h
-    hash = powHash (_blockChainwebVersion h)
-
-    -- TODO update bytes in in place (e.g. in a mutable vector) and feed
-    -- bytes to the hash function from the same buffer.
-    --
-    go 100000 !n !bytes = do
-        ct <- getCurrentTimeIntegral
-        go 0 n (injectTime ct bytes)
-    go !i !n !bytes = do
-        let bytes' = injectNonce n bytes
-        if checkTarget target $ hash bytes'
-            then runGet decodeBlockHeaderWithoutHash bytes'
-            else go (succ i) (succ n) bytes
-
-    injectTime t b = B.take 8 b
-        <> runPutS (encodeTime t)
-        <> B.drop 16 b
-
-    injectNonce n b
-        = runPutS (encodeNonce n)
-        <> B.drop 8 b
-
 -- -------------------------------------------------------------------------- --
--- Fast Miner
+-- Inner Mining loop
 
 usePowHash :: ChainwebVersion -> (forall a . HashAlgorithm a => Proxy a -> f) -> f
 usePowHash Test{} f = f $ Proxy @SHA512t_256
@@ -333,47 +301,61 @@ usePowHash TestWithPow{} f = f $ Proxy @SHA512t_256
 usePowHash Simulation{} f = f $ Proxy @SHA512t_256
 usePowHash Testnet00{} f = f $ Proxy @SHA512t_256
 
--- This Miner makes low-level assumptions about the chainweb protocol. It may
+-- | This Miner makes low-level assumptions about the chainweb protocol. It may
 -- break if the protocol changes.
 --
 -- TODO: Check the chainweb version to make sure this function can handle the
 -- respective version.
 --
-mineFast
+mine
     :: forall a
     . HashAlgorithm a
     => Proxy a
     -> BlockHeader
     -> Nonce
     -> IO BlockHeader
-mineFast _ h nonce = do
+mine _ h nonce = do
     !ctx <- hashMutableInit @a
     bytes <- BA.copy initialBytes $ \buf -> do
         allocaBytes (powSize :: Int) $ \pow -> do
+
+            -- inner mining loop
+            --
+            -- We do 100000 hashes before we update the creation time.
+            --
             let go 100000 !n = do
+
+                    -- update the block creation time
                     ct <- getCurrentTimeIntegral
                     injectTime ct buf
                     go 0 n
 
                 go !i !n = do
+
+                    -- Compute POW hash for the nonce
                     injectNonce n buf
                     hash ctx buf pow
                     powByteString <- mkPowHash =<< B.unsafePackCStringLen (castPtr pow, powSize)
+
+                    -- check whether the nonce meets the target
                     if checkTarget target powByteString
                         then return ()
                         else go (succ i) (succ n)
 
-            ct <- getCurrentTimeIntegral
-            injectTime ct buf
+            -- Start inner mining loop
             go (0 :: Int) nonce
 
+    -- On success: deserialize and returnb the new BlockHeader
     runGet decodeBlockHeaderWithoutHash bytes
+
   where
+
     !initialBytes = runPutS $ encodeBlockHeaderWithoutHash h
     !bufSize = B.length initialBytes
     !target = _blockTarget h
     !powSize = int $ hashDigestSize @a undefined
 
+    --  Compute POW hash
     hash :: MutableContext a -> Ptr Word8 -> Ptr Word8 -> IO ()
     hash ctx buf pow = do
         hashMutableReset ctx
