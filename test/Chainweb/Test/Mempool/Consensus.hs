4bc83fbd4f235f33afc09b6c5c5027afffc88191
Mark Nichols <marklnichols@gmail.com>
2019-04-29 17:04:33 -0400

    WIP - temp

--- test/Chainweb/Test/Mempool/Consensus.hs
+++ test/Chainweb/Test/Mempool/Consensus.hs
@@ -1,12 +1,25 @@
+module Chainweb.Test.Mempool.Consensus
+  ( tests
+  ) where
+
+import System.Random
+
 import Test.QuickCheck hiding ((.&.))
 import Test.QuickCheck.Gen (Gen, chooseAny, generate, resize)
 import Test.QuickCheck.Monadic
 import Test.Tasty
 import Test.Tasty.HUnit
 
-module Chainweb.Test.Mempool.Consensus
-  ( tests
-  ) where
-
-tests :: MempoolWithFunc -> [TestTree]
+tests :: [TestTree]
 tests = undefined
+
+genFork :: IO Int
+genFork = do
+    z <- getStdRandom (randomR (1,6))
+    return z
+
+checkProp :: IO ()
+checkProp = quickCheck prop_reverse
+
+prop_reverse :: [Int] -> Bool
+prop_reverse xs = reverse (reverse xs) == xs
