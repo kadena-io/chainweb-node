83d549e1421582caebc0d6b6cd604743b7fb7960
Emmanuel Denloye-Ito <eod8tb@virginia.edu>
2019-05-08 23:23:06 -0400

    update util.hs (from merge)

--- src/Chainweb/Pact/Utils.hs
+++ src/Chainweb/Pact/Utils.hs
@@ -12,11 +12,6 @@
 module Chainweb.Pact.Utils
     ( toEnv'
     , toEnvPersist'
-<<<<<<< HEAD
-    , closePactDb
-    , withSQLiteDb
-=======
->>>>>>> master
     ) where
 
 import Control.Concurrent.MVar
@@ -54,29 +49,3 @@ toEnvPersist' (Env' pactDbEnv) = do
           , _pdepEnv = dbEnv
           }
     return $! EnvPersist' pDbEnvPersist
-<<<<<<< HEAD
-
--- This is a band-aid solution; We're just going to close the
--- database connection here to be safe.
-closePactDb :: PactDbState -> IO ()
-closePactDb =  go . _pdbsDbEnv
-  where
-    go (EnvPersist' (PactDbEnvPersist _ dbEnv)) =
-      either fail return =<< closeDb (_db dbEnv)
-
-withSQLiteDb :: Logger -> Bool -> Maybe SQLiteConfig -> Loggers -> (PactDbEnv (DbEnv SQLite) -> IO a) -> IO a
-withSQLiteDb _ _ Nothing _ = error "withSQLitedb: No SQLite config!"
-withSQLiteDb initLog deleteOldFile (Just sqlc) loggers = bracket open close
-  where
-    open = do
-      when deleteOldFile $ do
-        dbExists <- doesFileExist (_dbFile sqlc)
-        when dbExists $ do
-          logLog initLog "INIT" "Deleting existing Pact DB File"
-          removeFile (_dbFile sqlc)
-      dbe <- initDbEnv loggers persister <$> initSQLite sqlc loggers
-      mkPactDbEnv chainwebpactdb dbe
-    -- We would want asynchronous exceptions to bubble up if any should exist.
-    close env = withMVar (pdPactDbVar env) (closeSQLite . _db)
-=======
->>>>>>> master
