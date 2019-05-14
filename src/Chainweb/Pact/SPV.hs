901ca230b320e2efb6b799443a86680e34bd84a2
Emily Pillmore <emily@kadena.io>
2019-05-14 14:51:45 -0400

    updates

--- src/Chainweb/Pact/SPV.hs
+++ src/Chainweb/Pact/SPV.hs
@@ -5,7 +5,7 @@
 {-# LANGUAGE TypeApplications #-}
 -- |
 -- Module: Chainweb.Pact.PactService
--- Copyright: Copyright © 2018 Kadena LLC.
+-- Copyright: Copyright © 2019 Kadena LLC.
 -- License: See LICENSE file
 -- Maintainers: Emily Pillmore <emily@kadena.io>
 -- Stability: experimental
@@ -42,8 +42,14 @@ import Pact.Types.Command
 import Pact.Types.Runtime
 
 
+-- the type of transaction output proofs i.e. TXOUT
+type OutputProof = TransactionOutputProof SHA512t_256
+
+-- the type of transaction input proofs i.e. TXIN
+type InputProof = TransactionProof SHA512t_256
+
 -- -------------------------------------------------------------------------- --
--- Pact SPV functionality
+-- Noop and std pact spv support
 
 noSPV :: SPVSupport
 noSPV = noSPVSupport
@@ -63,28 +69,28 @@ pactSPV dbVar = SPVSupport $ \s o -> do
 
       -- 1. Create JSON-formatted input or output proof from
       -- pact object (KV-pairs of keys and pact types as values)
-
-      t <- proofOf @OutputProof o
+      t <- mkProof @OutputProof o
 
       -- 2. Verify the output/input via the SPV api. This will
       -- produce proof that a transaction input/output occurred.
       -- If this fails, it will be returned as a failed command
       -- along with the relevant error message.
-
       (TransactionOutput u) <- verifyTransactionOutputProof cdb t
 
       -- 3. Create a Pact 'CommandSuccess'f object and return
       -- the successfully verified proof object.
-
       supportOf u
 
     txin cdb o = do
-      t <- proofOf @InputProof o
+      t <- mkProof @InputProof o
       (Transaction u) <- verifyTransactionProof cdb t
       supportOf u
 
     -- retrieve the spv object from 'CommandSuccess'
     supportOf v = Right . _tObject . _csData <$> mkSuccess v
+
+-- -------------------------------------------------------------------------- --
+-- utilities
 
 -- | Obtain an SPV proof given a Pact object
 --
@@ -92,10 +98,8 @@ pactSPV dbVar = SPVSupport $ \s o -> do
 --
 -- 'proofOf' '@(Transaction SHA512t_256)'
 --
-proofOf :: FromJSON a => Object Name -> IO a
-proofOf o =
-    let t = TObject o def
-    in print t >> (spvDecode . toJSON $ t)
+mkProof :: FromJSON a => Object Name -> IO a
+mkProof o = spvDecode . toJSON $ TObject o def
   where
     spvDecode a = case fromJSON a of
       Error s -> spvError $ "Unable to decode proof subject: " <> s
@@ -110,12 +114,6 @@ mkSuccess
     . fromStrict
   where
     err = "Unable to decode proof object"
-
-type OutputProof = TransactionOutputProof SHA512t_256
-type InputProof = TransactionProof SHA512t_256
-
--- -------------------------------------------------------------------------- --
--- Nicely formatted errors
 
 -- | Prepend "spvSupport" to any errors so we can differentiate
 --
