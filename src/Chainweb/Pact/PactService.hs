b27e4915b746f3e976b4ea3567f2be37134a5a60
Mark Nichols <marklnichols@gmail.com>
2019-05-22 14:22:28 -0400

    Added some debug log info

--- src/Chainweb/Pact/PactService.hs
+++ src/Chainweb/Pact/PactService.hs
@@ -278,6 +278,7 @@ execNewBlock memPoolAccess header miner = do
     let bHeight@(BlockHeight bh) = _blockHeight header
         bHash = _blockHash header
 
+    logInfo "execNewBlock, about to call memPoolAccess"
     newTrans <- liftIO $! memPoolAccess bHeight bHash header
 
     restoreCheckpointer $ Just (bHeight, bHash)
