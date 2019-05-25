c11a53d3118ad1283419c1378259611f0a3196de
Emily Pillmore <emily@kadena.io>
2019-05-25 12:44:55 -0400

    add retry behavior to cutdb extension

--- test/Chainweb/Test/Pact/SPV.hs
+++ test/Chainweb/Test/Pact/SPV.hs
@@ -180,7 +180,7 @@ roundtrip sid0 tid0 retries burn create = do
             -- Note: we must mine at least (diam + 1) * graph order many blocks
             -- to ensure we synchronize the cutdb across all chains
 
-            c1 <- retryMaybeN retries $ extendAwait cutDb pact1 (diam * gorder) $
+            c1 <- extendAwaitRetryN cutDb pact1 (diam * gorder) retries $
                 ((<) `on` height sid) c0
 
             -- A proof can only be constructed if the block hash of the source
@@ -202,7 +202,7 @@ roundtrip sid0 tid0 retries burn create = do
             -- block heights between any two chains can be at most
             -- `diameter(graph)` apart.
 
-            c2 <- retryMaybeN retries $ extendAwait cutDb pact1 60 $ \c ->
+            c2 <- extendAwaitRetryN cutDb pact1 60 retries $ \c ->
                 height tid c > diam + height sid c1
 
             -- execute '(coin.create-coin ...)' using the  correct chain id and block height
@@ -211,14 +211,10 @@ roundtrip sid0 tid0 retries burn create = do
             syncPact cutDb pact2
 
             -- consume the stream and mine second batch of transactions
-            void $ retryMaybeN retries $ extendAwait cutDb pact2 (diam * gorder)
+            void $ extendAwaitRetryN cutDb pact2 (diam * gorder) retries
                 $ ((<) `on` height tid) c2
 
             return (True, "test succeeded")
-  where
-      retryMaybeN :: Natural -> IO (Maybe a) -> IO a
-      retryMaybeN 0 _ = throwIO $ userError "Retries exceeded while extending cut db"
-      retryMaybeN n action = action >>= maybe (retryMaybeN (pred n) action) return
 
 -- -------------------------------------------------------------------------- --
 -- transaction generators
