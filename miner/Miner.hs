4a330bb365f4ce964ca25193643a0b277b51706f
Colin Woodbury <colin@fosskers.ca>
2019-07-08 11:20:58 -0700

    [miner] Elaborate on high-level Miner details

--- miner/Miner.hs
+++ miner/Miner.hs
@@ -18,27 +18,87 @@
 -- The fast "inner loop" of the mining process. Sits idle upon startup until
 -- work is submitted.
 --
+-- == Purpose and Expectations ==
+--
+-- This tool is a mid-level, push-based, private, multicore CPU miner for
+-- Chainweb. By this we mean:
+--
+--   * mid-level: the miner is aware of what `BlockHeader`s are, but does not
+--     necessarily know how to construct them. It has no concept of `Cut`s or
+--     the cut network used by Chainweb - it simply attempts `Nonce`s until a
+--     suitable hash is found that matches the current `HashTarget`.
+--
+--   * push-based: Work is submitted to the miner in the form of a candidate
+--     `BlockHeader`, in which the `Nonce` has been completely guessed. The
+--     miner then automatically handles cancelling previous mining attempts,
+--     hashing, cycling the `Nonce`, and caching the result. Note that we
+--     maintain no "long connections", so work submissions yield an HTTP
+--     response immediately, and the results of successful mining must be polled
+--     manually.
+--
+--   * private: Work can be submitted (and results polled) by anyone. It is
+--     therefore expected that a miner's HTTP interface is not exposed to the
+--     open internet.
+--     __It is the duty of the user (you) to ensure a correct configuration!__
+--
+--   * multicore: the miner uses 1 CPU core by default, but can use as many as
+--     you indicate. GPU support will be added soon.
+--
+-- Since this miner is "mid-level", it is up to the user to submit valid
+-- `BlockHeader`s derived from some suitable `Cut` (a full @chainweb-node@
+-- handles this automatically).
+--
+-- When mining has completed for some given work, the process will sit idle. At
+-- most, between 128 and 256 recently mined `BlockHeader`s per chain will be
+-- kept in the cache. If the miner process exits, the cache is lost.
+-- __Unpolled results thus cannot be recovered after a miner has been shutdown.__
+--
+-- == Usage ==
+--
+-- === submit/ endpoint ===
+--
+-- A POST call, expecting a single `BlockHeader` in its standard JSON format.
+-- This will cancel any current mining. If you wish to mine on multiple chains
+-- simultaneously, you can run multiple miner processes on different ports.
+--
+-- === poll/ endpoint ===
+--
+-- A GET call, given a `ChainId` and `BlockHeight` where we expect there to be a
+-- result. /May/ return a single `BlockHeader`, if mining were successful there.
+-- Failure indicates one of the following:
+--
+--   * New work cancelled the previous, thus no result was saved.
+--   * Mining may still succeed before the work stales, but just hasn't yet.
+--   * The given `ChainId` and `BlockHeight` were incorrect.
+--
 
 module Main ( main ) where
 
 import BasePrelude hiding (app, option)
+
 import Control.Concurrent.Async (async, race, wait)
 import Control.Concurrent.STM.TMVar
 import Control.Concurrent.STM.TVar (modifyTVar')
 import Control.Error.Util (note)
 import Control.Scheduler (Comp(..), replicateWork, terminateWith, withScheduler)
+
 import Crypto.Hash.Algorithms (SHA512t_256)
 import Crypto.Hash.IO
+
 import qualified Data.ByteArray as BA
-import Data.Bytes.Put
+import Data.Bytes.Put (runPutS)
 import qualified Data.ByteString as B
 import Data.HashMap.Strict (HashMap)
 import qualified Data.HashMap.Strict as HM
 import qualified Data.Text as T
 import Data.Tuple.Strict (T2(..))
+
 import Foreign.Marshal.Alloc (allocaBytes)
+
 import qualified Network.Wai.Handler.Warp as W
+
 import Options.Applicative
+
 import Servant.API
 import Servant.Server (Application, Server, serve)
 
@@ -140,7 +200,7 @@ mining e = do
     race newWork (go bh) >>= traverse_ miningSuccess
     mining e
   where
-    -- | Wait for new work to come in from a `poll` call. `readTMVar` has the
+    -- | Wait for new work to come in from a `submit` call. `readTMVar` has the
     -- effect of "waiting patiently" and will not cook the CPU.
     newWork :: IO ()
     newWork = void . atomically . readTMVar $ work e
