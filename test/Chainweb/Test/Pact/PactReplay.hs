17d945d420365d16becb31c72b0c3726ed5efb4a
Emmanuel Denloye-Ito <eod8tb@virginia.edu>
2019-07-18 19:30:45 -0400

    create scaffold for unit test

--- test/Chainweb/Test/Pact/PactReplay.hs
+++ test/Chainweb/Test/Pact/PactReplay.hs
@@ -1,3 +1,5 @@
+{-# language TypeFamilies #-}
+
 module Chainweb.Test.Pact.PactReplay (tests) where
 
 import System.Directory
@@ -7,21 +9,39 @@ import Test.Tasty
 
 -- chainweb imports
 
+import Chainweb.BlockHeader.Genesis
+import Chainweb.BlockHeaderDB.Types
+import Chainweb.ChainId
 import Chainweb.Payload.PayloadStore.InMemory
+import Chainweb.Payload.PayloadStore.Types
 import Chainweb.Test.Utils
+import Chainweb.Version
 
 
 tests :: ScheduledTest
 tests = ScheduledTest label $
     withRocksTemporaryFile $ \_sqlfile ->
-    withResource newPayloadDb killPdb $ \_iopdb ->
-    withRocksResource $ \_rocksIO ->
+    withResource newPayloadDb killPdb $ \iopdb ->
+    withRocksResource $ \rocksIO ->
+    withPopulatedDBs iopdb (createTestBlockHeaderDb rocksIO) $ \_iodbs ->
       testGroup label []
   where
-    label = "Pact replay test"
+    label = "Chainweb.Test.Pact.PactReplay"
     killPdb _ = return ()
+    v :: ChainwebVersion
+    v = undefined
+    cid :: ChainId
+    cid = undefined
+    createTestBlockHeaderDb iordb = do
+      rdb <- iordb
+      testBlockHeaderDb rdb (genesisBlockHeader v cid)
 
-
--- THIS SHOULD BE MOVED TO UTILS
+-- THESE SHOULD BE MOVED TO UTILS
 withRocksTemporaryFile :: (IO FilePath -> TestTree) -> TestTree
 withRocksTemporaryFile = withResource (fst <$> newTempFile) removeFile
+
+withPopulatedDBs :: PayloadCas cas => IO (PayloadDb cas) -> IO BlockHeaderDb -> (IO (PayloadDb cas, BlockHeaderDb) -> TestTree) -> TestTree
+withPopulatedDBs iopdb iobhdb = withResource ((,) <$> (populatepayloaddb <$> iopdb) <*> (populatebhdb <$> iobhdb)) (const $ return ())
+  where
+    populatebhdb = undefined
+    populatepayloaddb = undefined
