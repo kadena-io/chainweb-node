741163ee03feddcc4a156b2238782492e1eb9dbb
Emmanuel Denloye-Ito <eod8tb@virginia.edu>
2019-07-26 13:44:18 -0400

    adding in pact transactions
    still not quite there

--- test/Chainweb/Test/Pact/PactReplay.hs
+++ test/Chainweb/Test/Pact/PactReplay.hs
@@ -1,8 +1,9 @@
-{-# LANGUAGE BangPatterns #-}
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE BangPatterns      #-}
+{-# LANGUAGE LambdaCase        #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TypeApplications #-}
-{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeApplications  #-}
+{-# LANGUAGE TypeFamilies      #-}
+{-# LANGUAGE QuasiQuotes       #-}
 
 module Chainweb.Test.Pact.PactReplay where
 
@@ -17,22 +18,23 @@ import Control.Monad.State
 
 import Crypto.Hash.Algorithms (SHA512t_256)
 
-import Data.Bits
-import qualified Data.ByteString.Lazy as BL
+import Data.Aeson
 import Data.CAS.HashMap
 import Data.CAS.RocksDB
 -- import Data.Foldable
--- import Data.Map (Map)
--- import qualified Data.Map as M
+import Data.IORef
+import Data.Map (Map)
+import qualified Data.Map as M
 import Data.Proxy
+import Data.Text (Text)
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
 import Data.Tuple.Strict (T3(..))
--- import Data.Vector (Vector)
+import Data.Vector (Vector)
 import qualified Data.Vector as V
 import Data.Word
-import Data.Yaml
 
+import NeatInterpolation (text)
 
 import System.Directory
 import System.IO.Extra
@@ -47,14 +49,12 @@ import Chainweb.BlockHash
 import Chainweb.BlockHeader
 import Chainweb.BlockHeader.Genesis
 import Chainweb.BlockHeaderDB hiding (withBlockHeaderDb)
--- import Chainweb.BlockHeaderDB.Types
 import Chainweb.ChainId
 import Chainweb.Difficulty
 import Chainweb.Logger
 import Chainweb.Miner.POW
 import Chainweb.NodeId
 import Chainweb.Pact.Backend.Types
--- import Chainweb.Pact.Backend.Utils
 import Chainweb.Pact.PactService
 import Chainweb.Pact.Service.BlockValidation
 import Chainweb.Pact.Service.Types
@@ -65,99 +65,48 @@ import Chainweb.Payload
 import Chainweb.Payload.PayloadStore.InMemory
 import Chainweb.Payload.PayloadStore.Types
 import Chainweb.Test.Utils
--- import Chainweb.Test.Pact.Utils
--- import Chainweb.Time (getCurrentTimeIntegral)
 import Chainweb.Time
--- import Chainweb.Transaction
 import Chainweb.TreeDB
+import Chainweb.Transaction
 import Chainweb.Version
 
 testVersion :: ChainwebVersion
 testVersion = Testnet00
 
 tests :: ScheduledTest
-tests = ScheduledTest label $
-  withRocksResource $ \rocksIO ->
-  withPayloadDb $ \pdb ->
-  withBlockHeaderDb rocksIO genblock $ \bhdb ->
-  withTemporaryDir $ \dir ->
-   testGroup label
-    [withPact pdb bhdb testEmptyMemPool dir $ \reqQIO ->
-                testCase "simple-replay1" $
-                firstPlayThrough genblock cid pdb bhdb reqQIO
-    , after AllSucceed "simple-replay1" $ withPact pdb bhdb testEmptyMemPool dir $ \reqQIO ->
-        testCase "simple-replay2" $ do
-            bhdb' <- bhdb
-            maxblock <- maxEntry bhdb'
-            secondPlayThrough maxblock cid pdb bhdb reqQIO
-    ]
+tests =
+    ScheduledTest label $
+    withRocksResource $ \rocksIO ->
+      withPayloadDb $ \pdb ->
+        withBlockHeaderDb rocksIO genblock $ \bhdb ->
+          withTemporaryDir $ \dir ->
+            withTransactionsMap $ \mm ->
+            testGroup
+              label
+              [ withPact pdb bhdb testMemPoolAccess' dir $ \reqQIO ->
+                  testCase "initial-playthrough" $
+                  firstPlayThrough genblock cid pdb bhdb mm reqQIO
+              , after AllSucceed "initial-playthrough" $
+                withPact pdb bhdb testMemPoolAccess' dir $ \reqQIO ->
+                  testCase "on-restart" $ onRestart cid pdb bhdb reqQIO
+              ]
   where
     genblock = genesisBlockHeader testVersion cid
     label = "Chainweb.Test.Pact.PactReplay"
     cid = someChainId testVersion
 
-first3 :: (a,b,c) -> a
-first3 (a,_,_) = a
-second3 :: (a,b,c) -> b
-second3 (_,a,_) = a
-third3 :: (a,b,c) -> c
-third3 (_,_,a) = a
-
-
-withLastBlock :: (IO (MVar BlockHeader) -> TestTree) -> TestTree
-withLastBlock = withResource newEmptyMVar (const $ return ())
-
-secondPlayThrough
-            :: BlockHeader
-            -> ChainId
-            -> IO (PayloadDb HashMapCas)
-            -> IO (BlockHeaderDb)
-            -> IO (TQueue RequestMsg)
-            -> Assertion
-secondPlayThrough block cid pdb bhdb r =
-  void $ mineBlock block cid (Nonce $ fromIntegral $ _blockHeight block) pdb bhdb r
-
-withPayloadDb :: (IO (PayloadDb HashMapCas) -> TestTree) -> TestTree
-withPayloadDb = withResource newPayloadDb (\_ -> return ())
-
-withBlockHeaderDb
-    :: IO RocksDb
-    -> BlockHeader
-    -> (IO BlockHeaderDb -> TestTree)
-    -> TestTree
-withBlockHeaderDb iordb b = withResource start stop
-  where
-    start = do
-      rdb <- iordb
-      testBlockHeaderDb rdb b
-    stop = closeBlockHeaderDb
-
-withPact
-    :: IO (PayloadDb HashMapCas)
-    -> IO BlockHeaderDb
-    -> MemPoolAccess
-    -> IO FilePath
-    -> (IO (TQueue RequestMsg) -> TestTree)
-    -> TestTree
-withPact iopdb iobhdb mempool iodir f = withResource startPact stopPact $ f . fmap snd
-  where
-    startPact = do
-        mv <- newEmptyMVar
-        reqQ <- atomically newTQueue
-        pdb <- iopdb
-        bhdb <- iobhdb
-        dir <- iodir
-
-        a <- async $ initPactService testVersion cid logger reqQ mempool mv bhdb pdb (Just dir) Nothing False
-
-        return (a, reqQ)
-
-    stopPact (a, reqQ) = do
-        sendCloseMsg reqQ
-        cancel a
-
-    logger = genericLogger Warn T.putStrLn
-    cid = someChainId testVersion
+onRestart ::
+       ChainId
+    -> IO (PayloadDb HashMapCas)
+    -> IO (BlockHeaderDb)
+    -> IO (TQueue RequestMsg)
+    -> Assertion
+onRestart cid pdb bhdb r = do
+    bhdb' <- bhdb
+    block <- maxEntry bhdb'
+    let nonce = Nonce $ fromIntegral $ _blockHeight block
+    T3 _ b _ <- mineBlock block cid nonce pdb bhdb r
+    assertEqual "Invalid BlockHeight" 9 (_blockHeight b)
 
 testMemPoolAccess :: MemPoolAccess
 testMemPoolAccess  = MemPoolAccess
@@ -177,6 +126,27 @@ testMemPoolAccess  = MemPoolAccess
               ]
         goldenTestTransactions txs
 
+
+testMemPoolAccess' :: MemPoolAccess
+testMemPoolAccess'  = MemPoolAccess
+    { mpaGetBlock = getTestBlock
+    , mpaSetLastHeader = \_ -> return ()
+    , mpaProcessFork = \_ -> return ()
+    }
+  where
+    ksData :: Text -> Value
+    ksData idx = object [("k" <> idx) .= object [ "keys" .= ([] :: [Text]), "pred" .= String ">=" ]]
+    getTestBlock _bHeight _bHash bHeader = do
+        -- moduleStr <- readFile' $ testPactFilesDir ++ "test1.pact"
+        let Nonce nonce = _blockNonce bHeader
+            moduleStr = defModule (T.pack $ show nonce)
+            d = Just $ ksData (T.pack $ show nonce)
+        print nonce
+        -- d <- adminData
+        let txs = V.fromList [ PactTransaction moduleStr d]
+        goldenTestTransactions txs
+
+{-
 goldenBytes :: ToJSON a => Exception e => String -> Either e a -> IO BL.ByteString
 goldenBytes label (Left e) = assertFailure $ label ++ ": " ++ show e
 goldenBytes label (Right a) = return $ BL.fromStrict $ Data.Yaml.encode $ object
@@ -192,43 +162,41 @@ newBlockTest label reqIO = golden label $ do
     goldenBytes "new-block" =<< takeMVar respVar
   where
     cid = someChainId testVersion
+-}
 
 firstPlayThrough ::
             BlockHeader
             -> ChainId
             -> IO (PayloadDb HashMapCas)
             -> IO (BlockHeaderDb)
+            -> IO (MVar TransactionsMap)
             -> IO (TQueue RequestMsg)
             -> Assertion
-firstPlayThrough genesisBlock c iopdb iobhdb rr = do
-    mainlineblocks <- mineLine genesisBlock (Nonce . (xor minBound)) 7
+firstPlayThrough genesisBlock c iopdb iobhdb iotmap rr = do
+    nonceCounter <- newIORef (0 :: Word64)
+    mainlineblocks <- mineLine genesisBlock nonceCounter 7
+    -- mapM_ (\(T3 _ b _) -> print $ _blockHash b) mainlineblocks
     let T3 _ startline1 _ = mainlineblocks !! 0
     let T3 _ startline2 _ = mainlineblocks !! 1
-    -- let T3 _ end0 _ = last mainlineblocks
-    void $ mineLine startline1 (Nonce . (xor maxBound)) 4
-    void $ mineLine startline2 (Nonce . (xor 10000000)) 4
+    void $ mineLine startline1 nonceCounter 4
+    void $ mineLine startline2 nonceCounter 4
     return ()
-
-    -- assertEqual ""  [1..8] ((\(T3 _ a _) -> _blockHeight a) <$> mainlineblocks)
-    -- assertEqual ""  8 (_blockHeight end0)
-    -- assertEqual ""  6 (_blockHeight end1)
-    -- assertEqual ""  7 (_blockHeight end2)
   where
-    mineLine start withNonce len =
+    mineLine start ncounter len =
       evalStateT (runReaderT (mapM go [startHeight :: Word64 .. (startHeight + len)]) rr) start
         where
           startHeight = fromIntegral $ _blockHeight start
-          go height = do
+          go _height = do
             r <- ask
             pblock <- get
-            ret@(T3 _ newblock _) <- liftIO $ mineBlock pblock c (withNonce height) iopdb iobhdb r
+            n <- liftIO $ Nonce <$> readIORef ncounter
+            ret@(T3 pnewblock newblock _) <- liftIO $ mineBlock pblock c n iopdb iobhdb r
+            liftIO $ modifyIORef' ncounter succ
+            tmap <- liftIO iotmap
+            liftIO $ modifyMVar_ tmap $
+              return . M.insert (_blockHeight newblock, _blockHash newblock, pnewblock) undefined
             put newblock
             return ret
-
-
-assertNotLeft :: (MonadThrow m, Exception e) => Either e a -> m a
-assertNotLeft (Left l) = throwM l
-assertNotLeft (Right r) = return r
 
 mineBlock
     :: BlockHeader
@@ -279,11 +247,6 @@ mineBlock parentHeader cid nonce iopdb iobhdb r = do
                  , _payloadDataOutputsHash = _payloadWithOutputsOutputsHash d
                  }
 
-
-withTemporaryDir :: (IO FilePath -> TestTree) -> TestTree
-withTemporaryDir = withResource (fst <$> newTempDir) removeDirectoryRecursive
-
-
 -- TODO: uncomment this for MempoolAccess
 {-
 -- _testMemPoolAccess :: MemPoolAccess
@@ -333,3 +296,94 @@ testEmptyMemPool = MemPoolAccess
 -- --               , PactTransaction "(test1.transfer \"Acct1\" \"Acct2\" 1.00)" d
 -- --               ]
 -- --         goldenTestTransactions txs
+
+{-
+withLastBlock :: (IO (MVar BlockHeader) -> TestTree) -> TestTree
+withLastBlock = withResource newEmptyMVar (const $ return ())
+-}
+
+type TransactionsMap
+   = Map (BlockHeight, BlockHash, BlockHeader) (Vector ChainwebTransaction)
+
+withTransactionsMap :: (IO (MVar TransactionsMap) -> TestTree) -> TestTree
+withTransactionsMap = withResource (newMVar M.empty) (const $ return ())
+
+withTemporaryDir :: (IO FilePath -> TestTree) -> TestTree
+withTemporaryDir = withResource (fst <$> newTempDir) removeDirectoryRecursive
+
+withPayloadDb :: (IO (PayloadDb HashMapCas) -> TestTree) -> TestTree
+withPayloadDb = withResource newPayloadDb (\_ -> return ())
+
+withBlockHeaderDb
+    :: IO RocksDb
+    -> BlockHeader
+    -> (IO BlockHeaderDb -> TestTree)
+    -> TestTree
+withBlockHeaderDb iordb b = withResource start stop
+  where
+    start = do
+      rdb <- iordb
+      testBlockHeaderDb rdb b
+    stop = closeBlockHeaderDb
+
+withPact
+    :: IO (PayloadDb HashMapCas)
+    -> IO BlockHeaderDb
+    -> MemPoolAccess
+    -> IO FilePath
+    -> (IO (TQueue RequestMsg) -> TestTree)
+    -> TestTree
+withPact iopdb iobhdb mempool iodir f = withResource startPact stopPact $ f . fmap snd
+  where
+    startPact = do
+        mv <- newEmptyMVar
+        reqQ <- atomically newTQueue
+        pdb <- iopdb
+        bhdb <- iobhdb
+        dir <- iodir
+
+        a <- async $ initPactService testVersion cid logger reqQ mempool mv bhdb pdb (Just dir) Nothing False
+
+        return (a, reqQ)
+
+    stopPact (a, reqQ) = do
+        sendCloseMsg reqQ
+        cancel a
+
+    logger = genericLogger Warn T.putStrLn
+    cid = someChainId testVersion
+
+assertNotLeft :: (MonadThrow m, Exception e) => Either e a -> m a
+assertNotLeft (Left l) = throwM l
+assertNotLeft (Right r) = return r
+
+defModule :: Text -> Text
+defModule idx = [text| ;;
+
+(define-keyset 'k$idx (read-keyset 'k$idx))
+
+(module m$idx 'k$idx
+
+  (defschema sch col:integer)
+
+  (deftable tbl:{sch})
+
+  (defun insertTbl (a i)
+    (insert tbl a { 'col: i }))
+
+  (defun updateTbl (a i)
+    (update tbl a { 'col: i}))
+
+  (defun readTbl ()
+    (sort (map (at 'col)
+      (select tbl (constantly true)))))
+
+  (defpact dopact (n)
+    (step { 'name: n, 'value: 1 })
+    (step { 'name: n, 'value: 2 }))
+
+)
+(create-table tbl)
+(readTbl)
+;; (insertTbl "a" 1)
+|]
