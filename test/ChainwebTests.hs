846f36d80bda4ebe59cb3cd917225404d32711c4
Emmanuel Denloye-Ito <eod8tb@virginia.edu>
2019-08-02 18:41:29 -0400

    add comment about hanging test

--- test/ChainwebTests.hs
+++ test/ChainwebTests.hs
@@ -74,6 +74,8 @@ pactTestSuite rdb = testGroupSch "Chainweb-Pact Tests"
         , Chainweb.Test.Pact.PactInProcApi.tests
         , Chainweb.Test.Pact.RemotePactTest.tests rdb
         -- , Chainweb.Test.Pact.PactReplay.tests
+        -- Pact replay is hanging in some instances.
+        -- Temporarily disabling until a fix comes in the future.
         ]
 
 suite :: RocksDb -> [ScheduledTest]
