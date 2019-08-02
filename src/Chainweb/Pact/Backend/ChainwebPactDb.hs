703e61be2e547a8fd1cc97640de727d606736a60
Gregory Collins <greg@gregorycollins.net>
2019-08-02 16:47:16 -0400

    Revert "Fix getEndingTxId"
    It was right before?

--- src/Chainweb/Pact/Backend/ChainwebPactDb.hs
+++ src/Chainweb/Pact/Backend/ChainwebPactDb.hs
@@ -711,10 +711,13 @@ initSchema = withSavepoint DbTransaction $ do
 
 getEndingTxId :: BlockHeight -> BlockHandler SQLiteEnv TxId
 getEndingTxId bh = callDb "getEndingTxId" $ \db -> do
-    qry db "SELECT endingtxid FROM BlockHistory where blockheight = ?"
-        [SInt (fromIntegral bh)]
-        [RInt]
-      >>= fmap convertInt . expectSingleRowCol "endingtxid for block"
+    if bh == 0
+      then return 0
+      else
+        qry db "SELECT endingtxid FROM BlockHistory where blockheight = ?"
+            [SInt (fromIntegral $ pred bh)]
+            [RInt]
+          >>= fmap convertInt . expectSingleRowCol "endingtxid for block"
   where
     convertInt (SInt thing) = fromIntegral thing
     convertInt _ = error "impossible"
