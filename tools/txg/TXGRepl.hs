0e98f5a09c9aaa8252bba0cf324d13318f38e676
Emmanuel Denloye-Ito <eod8tb@virginia.edu>
2019-08-15 20:15:20 -0400

    easier implementation
    There are only two things that should concern the user
    
    1) Constructing commands to send along the wire
    
    2) Actually sending them
    
    Most users should only concern themselves with `easyCmd` and
    `easySend`. (There are advanced versions of these functions available).

--- tools/txg/TXGRepl.hs
+++ tools/txg/TXGRepl.hs
@@ -8,27 +8,28 @@
 {-# LANGUAGE OverloadedStrings          #-}
 {-# LANGUAGE ScopedTypeVariables        #-}
 {-# LANGUAGE TupleSections              #-}
-{-# language GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
 {-# OPTIONS_GHC -fno-warn-missing-signatures #-}
 
 module TXGRepl where
 
 import Control.Monad
-import Control.Monad.Catch
-import Control.Monad.Reader
+-- import Control.Monad.Catch
+-- import Control.Monad.Reader
 import Control.Monad.State
-import Control.Lens hiding ((.=))
+-- import Control.Lens hiding ((.=))
 
 import Data.Aeson
-import Data.Aeson.Types
+-- import Data.Aeson.Types
 -- import Data.ByteString (ByteString)
 import Data.Decimal
-import Data.Foldable
+-- import Data.Foldable
 import qualified Data.List.NonEmpty as NEL
 import Data.Proxy
 import qualified Data.Text as T
 import Data.Text (Text)
+-- import Data.Text.Encoding
 
 import GHC.Generics
 
@@ -47,6 +48,7 @@ import Text.Printf
 
 import Pact.ApiReq
 import Pact.Types.API
+import Pact.Types.Command
 import Pact.Types.ChainMeta
 import qualified Pact.Types.ChainId as Pact
 import Pact.Types.Crypto
@@ -61,18 +63,18 @@ import Chainweb.Pact.RestAPI
 import Chainweb.RestAPI.Utils
 #endif
 
+import TXG.Simulate.Contracts.HelloWorld
+import TXG.Simulate.Contracts.SimplePayments
+import TXG.Simulate.Contracts.CoinContract
 import TXG.Simulate.Utils
 
 -- for ghci
 
-data Builtin
-    = Coin
-    | Hello
-    | Payments
+data Builtin = Hello | Payments
     deriving (Show, Eq, Ord, Bounded, Enum, Generic)
     deriving anyclass (FromJSON, ToJSON)
 
-type Keyset = [SomeKeyPair]
+type Keyset = NEL.NonEmpty SomeKeyPair
 
 type Guard = Keyset
 
@@ -86,33 +88,37 @@ type Balance = Decimal
 
 type Amount = Decimal
 
-data CoinCode
-    = CoinCreateAccount Account Guard
-    | CoinAccountBalance Account
-    | CoinTransfer SenderName ReceiverName Amount Keyset
+-- data CoinCode
+--     = CoinCreateAccount Account Guard
+--     | CoinAccountBalance Account
+--     | CoinTransfer SenderName ReceiverName Amount
+--     | CoinTransferAndCreate SenderName ReceiverName Amount  Keyset
 
-data SimplePaymentsCode =
-    SPGetBalance Account
-    | SPPay Account Account Amount
-    | SPCreateAccount Account Balance [Keyset]
+-- data SimplePaymentsCode =
+--     SPGetBalance Account
+--     | SPPay Account Account Amount
+--     | SPCreateAccount Account Balance Keyset
 
 data CallBuiltIn'
-    = CC CoinCode
-    | SP SimplePaymentsCode
-    | HelloCode String
+    = CC CoinContractRequest
+    | SP SimplePaymentRequest (Maybe Keyset)
+    | HelloCode Text
 
 data TxContent
     = PactCode String
     | Define Builtin
     | CallBuiltin CallBuiltIn'
 
+{-
 data TxConfig = TxConfig
     { txcfgVersion :: ChainwebVersion
     , txcfgHostAddress :: HostAddress
     , txcfgChainId :: ChainId
     }
     deriving Show
+-}
 
+{-
 defaultTxConfig :: MonadThrow m => m TxConfig
 defaultTxConfig =
     TxConfig Testnet02
@@ -124,6 +130,7 @@ mkTxConfig hostaddress cid =
     TxConfig Testnet02
     <$> hostAddressFromText hostaddress
     <*> chainIdFromText cid
+-}
 
 genClientEnv :: HostAddress -> IO ClientEnv
 genClientEnv hostaddress = do
@@ -136,6 +143,7 @@ genClientEnv hostaddress = do
               ""
     pure $! mkClientEnv mgr url
 
+{-
 newtype Txg a = Txg {runTxg' :: ReaderT TxConfig IO a}
   deriving newtype
     (Functor
@@ -146,98 +154,66 @@ newtype Txg a = Txg {runTxg' :: ReaderT TxConfig IO a}
 
 runTxg :: TxConfig -> Txg a -> IO a
 runTxg = flip $ runReaderT . runTxg'
+-}
 
-type PactBatch a = (a, [Pair], Text, Keyset)
-type PactBatch' a = (a, [Pair], PublicMeta, Keyset)
+easyTxToCommand :: TxContent -> IO (Command Text)
+easyTxToCommand = txToCommand defPubMeta
 
-defaultPublicMeta :: PublicMeta
-defaultPublicMeta = PublicMeta (Pact.ChainId "0") "sender00" 100 1.0
+txToCommand :: PublicMeta -> TxContent -> IO (Command Text)
+txToCommand pubmeta = \case
+  PactCode str -> easyCmd str
+  Define Hello -> do
+    ks <- testSomeKeyPairs
+    helloWorldContractLoader pubmeta ks
+  Define Payments -> do
+    ks <- testSomeKeyPairs
+    simplePaymentsContractLoader pubmeta ks
+  CallBuiltin (CC coinReq) ->
+    createCoinContractRequest pubmeta coinReq
+  CallBuiltin (SP spReq mkeyset) ->
+    simplePayReq pubmeta spReq mkeyset
+  CallBuiltin (HelloCode helloname) ->
+    helloRequest (Name helloname)
 
-sendPactCodeDefaultSender
-    :: [(String, [Pair], PublicMeta)]
-    -> Txg (Either ClientError RequestKeys)
-sendPactCodeDefaultSender xs = do
-    kps <- toList <$> liftIO testSomeKeyPairs
-    sendPactCode' (addAdmin kps . f kps <$> xs)
+defChainwebVersion :: ChainwebVersion
+defChainwebVersion = Development
+
+defChainId :: ChainId
+defChainId = foldr const err $ chainIds defChainwebVersion
   where
-    f d (a,b,c) = (a,b,c,d)
-    -- I'm not sure of addAdmin is necessary any longer.
-    addAdmin kps = over _2 (("test-admin-keyset" .= fmap formatB16PubKey kps) :)
+    err = error "You shouldn't have a chainweb version with 0 chains"
 
-sendPactCode :: [PactBatch String] -> Txg (Either ClientError RequestKeys)
-sendPactCode [] = error "come back to this"
-sendPactCode xs = do
-    cmds <-
-      liftIO $
-        forM xs $ \(code, thepairs, sender, keyset) ->
-           mkExec code (object thepairs) (toPubMeta sender) keyset Nothing
-    TxConfig v h cid <- ask
-    cenv <- liftIO $ genClientEnv h
-    liftIO $ runClientM (send v cid (SubmitBatch $ NEL.fromList cmds)) cenv
-  where
-    toPubMeta sender =
-      defaultPublicMeta { _pmSender = sender }
+defHostAddressText :: Text
+defHostAddressText = "us2.testnet.chainweb.com:443"
 
--- This is the more general version which lets you set the gas price
--- and gas limit
-sendPactCode' :: [PactBatch' String] -> Txg (Either ClientError RequestKeys)
-sendPactCode' [] = error "come back to this"
-sendPactCode' xs = do
-    cmds <-
-      liftIO $
-        forM xs $ \(code, thepairs, pubMeta, keyset) ->
-           mkExec code (object thepairs) pubMeta keyset Nothing
-    TxConfig v h cid <- ask
-    cenv <- liftIO $ genClientEnv h
-    liftIO $ runClientM (send v cid (SubmitBatch $ NEL.fromList cmds)) cenv
+defPubMeta :: PublicMeta
+defPubMeta = PublicMeta (Pact.ChainId "0") "sender00" 100 1.0
 
-coinTransfer :: CoinCode -> Maybe (String, Keyset)
-coinTransfer (CoinTransfer sender receiver amount guard') = Just (stmt, guard')
-    where
-    stmt = printf
-        "(coin.transfer \"%s\" \"%s\" (read-keyset \"%s\") %s)"
-        sender
-        receiver
-        ("receiver-guard" :: String)
-        (show amount)
-coinTransfer _ = Nothing
+primSend
+    :: HostAddress
+    -> ChainwebVersion
+    -> ChainId
+    -> [Command Text]
+    -> IO (Either ClientError RequestKeys)
+primSend h v cid xs = do
+    cenv <- genClientEnv h
+    runClientM (send v cid (SubmitBatch (NEL.fromList xs))) cenv
 
-coinAccountBalance :: CoinCode -> Maybe String
-coinAccountBalance (CoinAccountBalance account) = Just $
-    printf "(coin.account-balance \"%s\")"
-      account
-coinAccountBalance _ = Nothing
+easySend :: [Command Text] -> IO (Either ClientError RequestKeys)
+easySend xs = do
+  hostaddress <- hostAddressFromText defHostAddressText
+  primSend hostaddress defChainwebVersion defChainId xs
 
-coinCreateAccount :: CoinCode -> Maybe (String, Keyset)
-coinCreateAccount (CoinCreateAccount account guard') =
-    Just (stmt, guard')
-  where
-    stmt = printf "(coin.create-account \"%s\" (read-keyset \"%s\"))"
-        account
-coinCreateAccount _ = Nothing
+easyCmd :: String -> IO (Command Text)
+easyCmd str = primCommand str Null defPubMeta [] Nothing
 
-handleCoin :: CoinCode -> Maybe (Either String (String, Keyset))
-handleCoin = undefined
-
-handleSP :: SimplePaymentsCode -> Maybe (Either String (String, Keyset))
-handleSP = undefined
-
-handleHello :: String -> Maybe (Either String (String, Keyset))
-handleHello = undefined
-
-txContentToString :: TxContent -> String
-txContentToString = \case
-    PactCode str -> undefined $ str
-    Define _ -> undefined
-    CallBuiltin (CC coincode) -> undefined $ coincode
-    CallBuiltin (SP spcode) -> undefined $ spcode
-    CallBuiltin (HelloCode personName) -> undefined $ personName
-
-sendTx :: [PactBatch TxContent] -> Txg (Either ClientError RequestKeys)
-sendTx = sendPactCode . map (over _1 txContentToString)
-
-sendTx' :: [PactBatch' TxContent] -> Txg (Either ClientError RequestKeys)
-sendTx' = sendPactCode' . map (over _1 txContentToString)
+primCommand
+    :: String
+    -> Value
+    -> PublicMeta
+    -> [SomeKeyPair]
+    -> Maybe String -> IO (Command Text)
+primCommand = mkExec
 
 api version chainid =
     case someChainwebVersionVal version of
@@ -270,29 +246,16 @@ listen version chainid = go
   where
     _ :<|> _ :<|> go :<|> _ = api version chainid
 
--- This is how you might send some "simple arithmetic transactions" programmatically.
-generateDefaultSimpleBatch :: Int -> IO [(String,[Pair],PublicMeta)]
-generateDefaultSimpleBatch batchsize =
-    replicateM batchsize $ getStdRandom (runState go)
+generateDefaultSimpleCommands :: Int -> IO [Command Text]
+generateDefaultSimpleCommands batchsize =
+    replicateM batchsize $ getStdRandom (runState go) >>= easyCmd
   where
     go = do
-        a <- state $ randomR (1, 100 :: Integer)
-        b <- state $ randomR (1, 100 :: Integer)
-        opIndex <- state $ randomR (0,2 :: Int)
-        return (toCode ("+-*" !! opIndex) a b,[] :: [Pair],defaultPublicMeta)
-      where
-        toCode operation operandA operandB =
-          "(" ++ [operation] ++ " " ++ show operandA ++ " " ++ show operandB ++ ")"
+      a <- state $ randomR (1, 100 :: Integer)
+      b <- state $ randomR (1, 100 :: Integer)
+      opIndex <- state $ randomR (0, 2 :: Int)
+      return $ printf "(%s %s %s)" ["+-*" !! opIndex] a b
 
-
-sendSimpleBatch
-    :: Text
-    -> Text
-    -> Int
-    -> IO (Either ClientError RequestKeys)
-sendSimpleBatch hostaddress cid batchsize = do
-    batch <- syncChainId <$> generateDefaultSimpleBatch batchsize
-    conf <- mkTxConfig hostaddress cid
-    runTxg conf (sendPactCodeDefaultSender batch)
-  where
-    syncChainId = traverse . _3 . pmChainId .~ (Pact.ChainId cid)
+sendSimpleBatch :: Int -> IO (Either ClientError RequestKeys)
+sendSimpleBatch batchsize =
+    generateDefaultSimpleCommands batchsize >>= easySend
