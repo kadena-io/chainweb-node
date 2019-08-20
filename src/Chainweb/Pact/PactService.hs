bba0d75ab1554e593264cbe4f4dc89851547626b
Emily Pillmore <emilypi@cohomolo.gy>
2019-08-19 20:59:15 -0400

    Add BlockHash and BlockHeight + PublicData to Local results (#361)
    * make coinbase result changes
    
    * replace sshow with toText
    
    * remove sshow
    
    * Update new-block-0-expected golden test

--- src/Chainweb/Pact/PactService.hs
+++ src/Chainweb/Pact/PactService.hs
@@ -387,13 +387,22 @@ execLocal cmd = do
         Nothing -> throwM NoBlockValidatedYet
         (Just !p) -> return p
 
-    !pdbst <- restoreCheckpointer $ Just (succ $ _blockHeight bh, _blockHash bh)
+    let bhe@(BlockHeight !bhe') = succ $ _blockHeight bh
+        bha@(BlockHash !bha') = _blockHash bh
+
+    !pdbst <- restoreCheckpointer $ Just (bhe, bha)
 
     case pdbst of
         PactDbEnv' pactdbenv -> do
             PactServiceEnv{..} <- ask
+
+            let pd = _psPublicData
+                  { P._pdBlockHeight = bhe'
+                  , P._pdPrevBlockHash = toText bha'
+                  }
+
             r <- liftIO $ applyLocal (_cpeLogger _psCheckpointEnv) pactdbenv
-                 _psPublicData _psSpvSupport (fmap payloadObj cmd)
+                 pd _psSpvSupport (fmap payloadObj cmd)
             discardCheckpointer
             return $! toHashCommandResult r
 
@@ -421,7 +430,7 @@ withBlockData
 withBlockData bhe action = action
     & locally (psPublicData . P.pdBlockHeight) (const bh)
     & locally (psPublicData . P.pdBlockTime) (const bt)
-    & locally (psPublicData . P.pdPrevBlockHash) (const $ sshow ph)
+    & locally (psPublicData . P.pdPrevBlockHash) (const $ toText ph)
   where
     (BlockHeight !bh) = _blockHeight bhe
     (BlockCreationTime (Time (TimeSpan (Micros !bt)))) = _blockCreationTime bhe
@@ -441,7 +450,7 @@ withParentBlockData
     -> PactServiceM cas a
 withParentBlockData phe action = action
     & locally (psPublicData . P.pdBlockHeight) (const bh)
-    & locally (psPublicData . P.pdPrevBlockHash) (const $ sshow ph)
+    & locally (psPublicData . P.pdPrevBlockHash) (const $ toText ph)
   where
     (BlockHeight !bh) = succ $ _blockHeight phe
     (BlockHash !ph) = _blockHash phe
