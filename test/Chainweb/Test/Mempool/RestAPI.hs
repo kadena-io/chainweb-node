582f618c2e3e27a775a23230e2efe9c2eb690556
Colin Woodbury <colin@fosskers.ca>
2019-09-06 11:33:19 -0700

    Minist-Evoluo (#381)
    * [miner] `working` function for submission of mining work
    
    * [miner] First pass at `publishing`
    
    * [miner] Only consider the most recent Payload
    
    * [miner] Don't mine on the same Cut more than once
    
    * [miner] Also hold onto previous block
    
    * [miner] Remove unused functions
    
    * [miner] Account for `Adjustments` pruning
    
    * [miner] Some linting
    
    * [miner] First pass at solved BlockHeader submission API
    
    * [miner] Modules for communication with the "Mining Result" endpoint
    
    Although these are incomplete.
    
    * [miner] `publishing` accepts `BlockHeader` directly
    
    Principle of Perfect Input.
    
    * [miner] `MiningResources` refers to `Ministo`
    
    At least as much as possible at the moment. This should be enough to nuke
    the old `Coordinator` module and deal with the fallout.
    
    * [miner] Ministo -> Coordinator
    
    And tentatively run `listener` on a side-thread.
    
    * [miner] Concrete datatype (MiningState) to share between threads
    
    * [miner] someMiningServer
    
    * [miner] Shared variable into `MinerResources`
    
    * [miner] `Chainweb.Chainweb` cleanup
    
    * [miner] Pipe `MinerResources` through to mining endpoints
    
    * [miner] Reinstate `remoteMining` function
    
    * [miner] Repair tests
    
    * [miner] chainweb-miner sends mining results back automatically
    
    This allowed me to remove all the old result caching logic,
    which cleans things up significantly.
    
    * [miner] Add missed dependency
    
    * hostAddressToBaseUrl
    
    * [miner] Accept coordinator URL on the command line
    
    * [miner] Make `Scheme` variable
    
    * [miner] Fix hang with in-process mining
    
    * [miner] Fix communication bugs and weird hang
    
    * [miner] Be robust to exceptions in `working`
    
    * [miner] Minor clarification [skip ci]
    
    * [miner] Remove some TODOs
    
    * fix coordinator publishing logic
    
    * use runForever to run listener of mining coordinator
    
    * [miner] Slight refactor
    
    * [miner] Reinstate a `race` to reenable preemption
    
    * Removed `undefined` left in code

--- test/Chainweb/Test/Mempool/RestAPI.hs
+++ test/Chainweb/Test/Mempool/RestAPI.hs
@@ -1,6 +1,5 @@
 module Chainweb.Test.Mempool.RestAPI (tests) where
 
-------------------------------------------------------------------------------
 import Control.Concurrent
 import Control.Concurrent.STM
 import Control.Exception
@@ -8,27 +7,33 @@ import Control.Exception
 import qualified Data.Pool as Pool
 
 import qualified Network.HTTP.Client as HTTP
-import Servant.Client (BaseUrl(..), Scheme(..), mkClientEnv)
+import Network.Wai (Application)
+
+import Servant.Client (BaseUrl(..), ClientEnv, Scheme(..), mkClientEnv)
+
 import Test.Tasty
 
-------------------------------------------------------------------------------
+-- internal modules
+
 import Chainweb.ChainId (ChainId)
+import Chainweb.Chainweb.MinerResources (MinerResources)
 import Chainweb.Graph
+import Chainweb.Logger (GenericLogger)
 import qualified Chainweb.Mempool.InMem as InMem
 import Chainweb.Mempool.InMemTypes (InMemConfig(..))
 import qualified Chainweb.Mempool.InMemTypes as InMem
 import Chainweb.Mempool.Mempool
 import qualified Chainweb.Mempool.RestAPI.Client as MClient
 import Chainweb.RestAPI
-    (ChainwebServerDbs(..), chainwebApplication, emptyChainwebServerDbs)
 import Chainweb.Test.Mempool (MempoolWithFunc(..))
 import qualified Chainweb.Test.Mempool
 import Chainweb.Test.Utils (withTestAppServer)
 import Chainweb.Utils (Codec(..))
 import Chainweb.Version
-import Data.CAS.RocksDB
-import Network.X509.SelfSigned
 
+import Data.CAS.RocksDB
+
+import Network.X509.SelfSigned
 
 ------------------------------------------------------------------------------
 tests :: TestTree
@@ -44,11 +49,11 @@ tests = withResource (newPool cfg) Pool.destroyAllResources $
     hashmeta = chainwebTestHashMeta
     hasher = chainwebTestHasher . codecEncode mockCodec
 
-data TestServer = TestServer {
-    _tsRemoteMempool :: !(MempoolBackend MockTx)
-  , _tsLocalMempool :: !(MempoolBackend MockTx)
-  , _tsServerThread :: !ThreadId
-  }
+data TestServer = TestServer
+    { _tsRemoteMempool :: !(MempoolBackend MockTx)
+    , _tsLocalMempool :: !(MempoolBackend MockTx)
+    , _tsServerThread :: !ThreadId
+    }
 
 newTestServer :: InMemConfig MockTx -> IO TestServer
 newTestServer inMemCfg = mask_ $ do
@@ -59,12 +64,10 @@ newTestServer inMemCfg = mask_ $ do
     env <- takeMVar envMv
     let remoteMp0 = MClient.toMempool version chain txcfg blocksizeLimit env
     -- allow remoteMp to call the local mempool's getBlock (for testing)
-    let remoteMp = remoteMp0 {
-                       mempoolGetBlock = \a b c -> mempoolGetBlock inmem a b c
-                     }
+    let remoteMp = remoteMp0 { mempoolGetBlock = \a b c -> mempoolGetBlock inmem a b c }
     return $! TestServer remoteMp inmem tid
-
   where
+    server :: MVar (MempoolBackend MockTx) -> MVar ClientEnv -> (IO b -> IO a) -> IO a
     server inmemMv envMv restore =
         InMem.withInMemoryMempool inMemCfg $ \inmem -> do
             putMVar inmemMv inmem
@@ -72,21 +75,35 @@ newTestServer inMemCfg = mask_ $ do
                 putMVar envMv env
                 atomically retry
 
+    version :: ChainwebVersion
     version = Test singletonChainGraph
+
+    blocksizeLimit :: GasLimit
     blocksizeLimit = InMem._inmemTxBlockSizeLimit inMemCfg
+
+    txcfg :: TransactionConfig MockTx
     txcfg = InMem._inmemTxCfg inMemCfg
+
+    host :: String
     host = "127.0.0.1"
+
+    chain :: ChainId
     chain = someChainId version
-    mkApp mp = chainwebApplication version (serverMempools [(chain, mp)])
+
+    mkApp :: MempoolBackend MockTx -> Application
+    mkApp mp = chainwebApplication version (serverMempools [(chain, mp)]) mr
+      where
+        mr :: Maybe (MinerResources GenericLogger cas)
+        mr = Nothing
+
+    mkEnv :: Int -> IO ClientEnv
     mkEnv port = do
         mgrSettings <- certificateCacheManagerSettings TlsInsecure Nothing
         mgr <- HTTP.newManager mgrSettings
         return $! mkClientEnv mgr $ BaseUrl Https host port ""
 
-
 destroyTestServer :: TestServer -> IO ()
 destroyTestServer (TestServer _ _ tid) = killThread tid
-
 
 newPool :: InMemConfig MockTx -> IO (Pool.Pool TestServer)
 newPool cfg = Pool.createPool (newTestServer cfg) destroyTestServer 1 10 20
@@ -94,13 +111,14 @@ newPool cfg = Pool.createPool (newTestServer cfg) destroyTestServer 1 10 20
 ------------------------------------------------------------------------------
 
 serverMempools
-    :: [(ChainId, MempoolBackend t)] -> ChainwebServerDbs t () RocksDbCas {- ununsed -}
+    :: [(ChainId, MempoolBackend t)]
+    -> ChainwebServerDbs t a RocksDbCas {- ununsed -}
 serverMempools mempools = emptyChainwebServerDbs
     { _chainwebServerMempools = mempools
     }
 
 withRemoteMempool
-  :: IO (Pool.Pool TestServer) -> (MempoolBackend MockTx -> IO a) -> IO a
+    :: IO (Pool.Pool TestServer) -> (MempoolBackend MockTx -> IO a) -> IO a
 withRemoteMempool poolIO userFunc = do
     pool <- poolIO
     Pool.withResource pool $ \ts -> do
