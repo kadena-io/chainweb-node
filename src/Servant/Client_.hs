e6610ddd16aba99a85b735f7feb8baf2c25d4ead
Lars Kuhtz <lars@kadena.io>
2019-10-01 18:07:34 -0700

    add Servant.Client_

---
+++ src/Servant/Client_.hs
@@ -0,0 +1,123 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies #-}
+
+-- |
+-- Module: Servant.Client_
+-- Copyright: Copyright © 2019 Kadena LLC.
+-- License: MIT
+-- Maintainer: Lars Kuhtz <lars@kadena.io>
+-- Stability: experimental
+--
+-- TODO
+--
+module Servant.Client_
+( ClientM_
+, client_
+, runClientM_
+, modifyRequest
+, modifyResponse
+) where
+
+import Control.Lens
+import Control.Monad.Base
+import Control.Monad.Catch
+import Control.Monad.Error.Class
+import Control.Monad.Reader
+import Control.Monad.Trans.Control
+
+import Data.Functor.Alt
+import Data.Proxy
+
+import GHC.Generics
+
+import Servant.Client
+import Servant.Client.Core
+
+-- -------------------------------------------------------------------------- --
+-- Run Modified Client
+
+data Mods = Mods
+    { _modReq :: !(Request -> IO Request)
+    , _modRes :: !(Response -> IO Response)
+    }
+
+makeLenses ''Mods
+
+-- | 'ClientM_' mimics the behavior of 'ClientM', but provides the functions
+-- 'modifyResponse' and 'modifyRequest' that allow to locally overwrite requests
+-- and responses.
+--
+newtype ClientM_ a = ClientM_ { unClientM_ :: ReaderT Mods ClientM a }
+  deriving
+    ( Functor, Applicative, Monad, MonadIO
+    , Generic
+    , MonadError ClientError, MonadThrow , MonadCatch
+    )
+
+instance MonadReader ClientEnv ClientM_ where
+    ask = ClientM_ $ lift ask
+    local f = ClientM_ . mapReaderT (local f) . unClientM_
+    {-# INLINE ask #-}
+    {-# INLINE local #-}
+
+instance MonadBase IO ClientM_ where
+    liftBase = ClientM_ . liftBase
+    {-# INLINE liftBase #-}
+
+instance MonadBaseControl IO ClientM_ where
+    type StM ClientM_ a = Either ClientError a
+    liftBaseWith f = ClientM_ (liftBaseWith (\g -> f (g . unClientM_)))
+    restoreM st = ClientM_ (restoreM st)
+    {-# INLINE liftBaseWith #-}
+    {-# INLINE restoreM #-}
+
+instance Alt ClientM_ where
+    a <!> b = a `catchError` \_ -> b
+    {-# INLINE (<!>) #-}
+
+instance RunClient ClientM_ where
+    runRequest req = ClientM_ $ do
+        e <- ask
+        req' <- liftIO $ _modReq e req
+        res <- lift (runRequest req')
+        liftIO $ _modRes e res
+    throwClientError = throwError
+
+    {-# INLINE runRequest #-}
+    {-# INLINE throwClientError #-}
+
+-- | Locally overwrite 'Request's made the inner computation.
+--
+modifyRequest :: (Request -> IO Request) -> ClientM_ a -> ClientM_ a
+modifyRequest f = ClientM_ . local (set modReq f) . unClientM_
+{-# INLINE modifyResponse #-}
+
+-- | Locally overwrite 'Response's received the inner computation.
+--
+modifyResponse :: (Response -> IO Response) -> ClientM_ a -> ClientM_ a
+modifyResponse f = ClientM_ . local (set modRes f) . unClientM_
+{-# INLINE modifyRequest #-}
+
+-- | Run a 'ClientM_' computation.
+--
+runClientM_ :: ClientM_ a -> ClientEnv -> IO (Either ClientError a)
+runClientM_ cm env
+    = flip runClientM env . flip runReaderT (Mods pure pure) $ unClientM_ cm
+{-# INLINE runClientM_ #-}
+
+-- | Create API client functions
+--
+client_ :: forall api . HasClient ClientM_ api => Client ClientM_ api
+client_ = clientIn (Proxy @api) (Proxy @ClientM_)
+{-# INLINE client_ #-}
+
