4c1e3e26f40d558caaf753e484f36c653575e811
Emily Pillmore <emily@kadena.io>
2019-10-23 19:34:09 -0400

    updateg

---
+++ test/Chainweb/Test/Pact/RewardsTest.hs
@@ -0,0 +1,67 @@
+{-# LANGUAGE RankNTypes #-}
+module Chainweb.Test.Pact.RewardsTest
+( tests
+) where
+
+
+import Test.Tasty
+import Test.Tasty.HUnit
+
+import Control.Lens
+
+import Data.Foldable
+import qualified Data.Vector as V
+
+import Pact.Parse
+
+import Chainweb.BlockHeader.Genesis
+import Chainweb.Graph
+import Chainweb.Miner.Pact
+import Chainweb.Pact.Backend.Types
+import Chainweb.Pact.PactService
+import Chainweb.Payload.PayloadStore.InMemory (newPayloadDb)
+import Chainweb.Test.Pact.Utils
+import Chainweb.Test.Utils
+import Chainweb.Version
+
+
+v :: ChainwebVersion
+v = FastTimedCPM petersonChainGraph
+
+cid :: ChainId
+cid = head . toList $ chainIds v
+
+
+tests :: ScheduledTest
+tests = ScheduledTest "Chainweb.Test.Pact.RewardsTest" $
+    testGroup "Miner Rewards Unit Tests"
+      [ rewardsTest
+      ]
+    where
+      bhdbIO rocksIO = do
+        rdb <- rocksIO
+        testBlockHeaderDb rdb $ genesisBlockHeader v cid
+
+      killPdb _ = return ()
+
+rewardsTest :: TestTree
+rewardsTest = testCaseSteps "rewards" $ \step -> do
+    step "block heights below initial threshold"
+    ParsedDecimal a <- k $ minerReward 0
+    assertEqual "initial miner reward is 2.304523" 2.304523 a
+
+    step "block heights at threshold"
+    ParsedDecimal b <- k $ minerReward 87600
+    assertEqual "max threshold miner reward is 2.304523" 2.304523 b
+
+    step "block heights exceeding thresholds change"
+    ParsedDecimal c <- k $ minerReward 87601
+    assertEqual "max threshold miner reward is 2.297878" 2.297878 c
+
+    step "value of minimal blockheight in state changes"
+    bh <- k $ minerReward 87601 >>= \_ -> do
+      w <- use $ psMinerRewards . minerRewardHeights
+      return $ V.head w
+    assertEqual "block height reaches new threshold, minimum height changes" bh 175200
+
+    return ()
