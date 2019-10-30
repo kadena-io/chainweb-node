26c5ffb06b165845fcf2da6feefecfd807b9005b
Colin Woodbury <colin@fosskers.ca>
2019-10-30 16:24:15 -0700

    Guard early against bad POW hashes (#613)
    * [miner] Guard early against bad POW hashes
    
    * [miner] Show bad nonce too
    
    * [miner] Clarifications

--- src/Chainweb/Miner/Coordinator.hs
+++ src/Chainweb/Miner/Coordinator.hs
@@ -35,8 +35,9 @@ import Data.Aeson (ToJSON)
 import Data.Bool (bool)
 
 import Control.DeepSeq (NFData)
-import Control.Error.Util ((!?), (??))
+import Control.Error.Util (hoistEither, (!?), (??))
 import Control.Lens (iforM, set, to, (^.), (^?!))
+import Control.Monad (unless)
 import Control.Monad.Trans.Class (lift)
 import Control.Monad.Trans.Except (runExceptT)
 
@@ -60,6 +61,7 @@ import System.LogLevel (LogLevel(..))
 
 import Chainweb.BlockHash (BlockHash, BlockHashRecord(..))
 import Chainweb.BlockHeader
+import Chainweb.BlockHeader.Validation (prop_block_pow)
 import Chainweb.Cut
 import Chainweb.Cut.CutHashes
 import Chainweb.CutDB
@@ -177,13 +179,31 @@ publish lf ms cdb bh = do
 -- mining client), attempts to reassociate it with the current best `Cut`, and
 -- publishes the result to the `Cut` network.
 --
+-- There are a number of "fail fast" conditions which will kill the candidate
+-- `BlockHeader` before it enters the Cut pipeline.
+--
 publish' :: LogFunction -> MiningState -> CutDb cas -> BlockHeader -> IO ()
 publish' lf (MiningState ms) cdb bh = do
     c <- _cut cdb
     let !phash = _blockPayloadHash bh
     res <- runExceptT $ do
+        -- Fail Early: If a `BlockHeader` comes in that isn't associated with any
+        -- Payload we know about, reject it.
+        --
         T3 m p pl <- M.lookup phash ms ?? "BlockHeader given with no associated Payload"
+
         let !miner = m ^. minerId . _Unwrapped
+            !nonce = _blockNonce bh
+
+        -- Fail Early: If a `BlockHeader`'s injected Nonce (and thus its POW
+        -- Hash) is trivially incorrect, reject it.
+        --
+        unless (prop_block_pow bh) . hoistEither .
+            Left $ "Invalid POW hash given for nonce: " <> sshow nonce
+
+        -- Fail Early: If the `BlockHeader` is already stale and can't be
+        -- appended to the best `Cut` we know about, reject it.
+        --
         c' <- tryMonotonicCutExtension c bh !? ("Newly mined block for outdated cut: " <> miner)
         lift $ do
             -- Publish the new Cut into the CutDb (add to queue).
