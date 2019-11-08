d7e2969d56c778e54bfac546e8e9712e06b6ba1d
Mark Nichols <marklnichols@gmail.com>
2019-11-08 18:27:31 -0500

    WIP

--- test/Chainweb/Test/Pact/ForkTest.hs
+++ test/Chainweb/Test/Pact/ForkTest.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE DeriveAnyClass #-}
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE TypeFamilies #-}
 
@@ -7,58 +8,170 @@ module Chainweb.Test.Pact.ForkTest
   ( tests
   ) where
 
+import Control.Lens
+import Control.Monad
 import Control.Monad.IO.Class
 
+import Data.Aeson (Value)
+import Data.CAS.RocksDB
+import Data.String.Conv (toS)
 import Data.HashMap.Strict (HashMap)
 import qualified Data.HashMap.Strict as HM
 import Data.HashSet (HashSet)
 import Data.IORef
+import Data.Text (Text)
+import qualified Data.Text as T
+import Data.Vector (Vector)
+import qualified Data.Vector as V
+
+import System.Directory
+import System.IO.Extra
+import System.IO.Temp
+import System.LogLevel
 
 import Test.QuickCheck hiding ((.&.))
 import Test.QuickCheck.Monadic
 import Test.Tasty
 import Test.Tasty.QuickCheck
 
+import Pact.Parse
+import Pact.Types.ChainMeta
+import Pact.Types.Command
+
 -- internal modules
 import Chainweb.BlockHeader
-import Chainweb.BlockHeaderDB
+import Chainweb.BlockHeaderDB.Types
 import Chainweb.Mempool.Mempool
+import Chainweb.Miner.Pact
+import Chainweb.Pact.Backend.Types
+import Chainweb.Pact.Service.BlockValidation
 import Chainweb.Pact.Service.PactQueue
 import Chainweb.Test.ForkGen
+import Chainweb.Test.Pact.Utils
 import Chainweb.Test.Utils
+import Chainweb.Time
+import Chainweb.Transaction
+import Chainweb.Utils hiding (withTempDir)
+import Chainweb.Version
 
 tests :: BlockHeaderDb -> BlockHeader -> ScheduledTest
 tests db h0 = testGroupSch "pact-fork-quickcheck-tests"
-    [ testProperty "fork-prop-1-tbd" (prop_forkProp1 db h0) ]
+    [ testProperty "prop-forkValidates" (prop_forkValidates db h0) ]
+
+testVersion :: ChainwebVersion
+testVersion = Development
 
 -- | Property: Fork requiring checkpointer rewind validates properly
 prop_forkValidates
     :: BlockHeaderDb
     -> BlockHeader
     -> Property
-prop_forkValidates db genBlock = monadicIO $ do
-    mapRef <- liftIO $ newIORef (HM.empty :: HashMap BlockHeader (HashSet TransactionHash))
-    fi <- genFork db mapRef genBlock
-    let blockList = blocksFromFork fi
-    liftIO $ putStrLn $ "list of blocks:\n" ++ show blockList
-    -- liftIO $ putStrLn $ show fi
-    newBlockTest TBD TBD blockList
-    assert (True == True) -- TODO: how to validate this test?
+prop_forkValidates db genBlock =
+    withRocksResource $
+       \rocksIO ->
+        withTempDir $ \dir ->
+        withBlockHeaderDb rocksIO genBlock $ \bhdb ->
+        withPayloadDb $ \pdb -> monadicIO $ do
+            mapRef <- liftIO $ newIORef (HM.empty :: HashMap BlockHeader (HashSet TransactionHash))
+            fi <- genFork db mapRef genBlock
+            let blockList = blocksFromFork fi
+            liftIO $ putStrLn $ "list of blocks:\n" ++ show blockList
+            -- liftIO $ putStrLn $ show fi
+            withPact testVersion Warn pdb bhdb testMemPoolAccess dir $ \reqQIO ->
+                newBlockTest "new-block-0" reqQIO blockList
+            assert (True == True) -- TODO: how to validate this test?
+
+forkValidatesTT :: BlockHeaderDb -> BlockHeader -> TestTree
+forkValidatesTT db genBlock =
+    testProperty "someName" $ prop_forkValidates db genBlock
+
+wrapForkValidatesTT :: BlockHeaderDb -> BlockHeader -> TestTree
+wrapForkValidatesTT db genBlock =
+  withRocksResource $ \rocksIO ->
+     forkValidatesTT db genBlock
+
+-- withRocksResource :: (IO RocksDb -> TestTree) -> TestTree
+-- withResource :: IO a -> (a -> IO ()) -> TestTree -> TestTree
+-- testProperty :: Testable a => TestName -> a -> TestTree
 
 blocksFromFork :: ForkInfo -> [BlockHeader]
 blocksFromFork ForkInfo{..} =
-    fiPreForkHeaders ++ fiLeftHeaders ++ fiRightHeaders
+    fiPreForkHeaders ++ fiLeftForkHeaders ++ fiRightForkHeaders
 
 ----------------------------------------------------------------------------------------------------
 -- Borrowed/modified from PactInProceApi test...
 ----------------------------------------------------------------------------------------------------
+testMemPoolAccess :: MemPoolAccess
+testMemPoolAccess = MemPoolAccess
+    { mpaGetBlock = \validate bh hash _header ->
+        getTestBlock validate bh hash
+    , mpaSetLastHeader = \_ -> return ()
+    , mpaProcessFork = \_ -> return ()
+    }
+  where
+    getTestBlock validate bHeight bHash = do
+        txs <- txsFromHeight bHeight
+        let f = modifyPayloadWithText . set (pMeta . pmCreationTime)
+            g = modifyPayloadWithText . set (pMeta . pmTTL)
+        outtxs' <- goldenTestTransactions txs
+        let outtxs = flip V.map outtxs' $ \tx ->
+                let ttl = TTLSeconds $ ParsedInteger $ 24 * 60 * 60
+                in fmap ((g ttl) . (f (TxCreationTime $ ParsedInteger 1000000))) tx
+        oks <- validate bHeight bHash outtxs
+        when (not $ V.and oks) $ do
+            fail $ mconcat [ "tx failed validation! input list: \n"
+                           , show txs
+                           , "\n\nouttxs: "
+                           , show outtxs
+                           , "\n\noks: "
+                           , show oks ]
+        return outtxs
+
+txsFromHeight :: Int -> IO (Vector PactTransaction)
+txsFromHeight 0 = do
+    d <- adminData
+    moduleStr <- readFile' $ testPactFilesDir ++ "test1.pact"
+    let f = modifyPayloadWithText . set (pMeta . pmCreationTime)
+    let g = modifyPayloadWithText . set (pMeta . pmTTL)
+    return $ V.fromList
+        ( [ PactTransaction { _pactCode = (T.pack moduleStr) , _pactData = d }
+          , PactTransaction { _pactCode = "(create-table test1.accounts)" , _pactData = d }
+          , PactTransaction { _pactCode = "(test1.create-global-accounts)" , _pactData = d }
+          ] V.++ commonTxs d)
+txsFromheight h = do
+    d <- adminData
+    let escAccountStr = "\"Acct" ++ show h ++ "\""
+    return $ V.fromList
+        ( [ PactTransaction { _pactCode = toS ("(test1.createAccount " ++ escAccountStr ++ ")")
+                            , _pactData = d }
+          , PactTransaction { _pactCode = toS ("(test1.transfer \"Acct1\"" ++ escAccountStr ++ " 1.00)")
+                            , _pactData = d }
+          , PactTransaction { _pactCode = "(at 'prev-block-hash (chain-data))" , _pactData = d }
+          , PactTransaction { _pactCode = "(at 'block-time (chain-data))" , _pactData = d }
+          , PactTransaction { _pactCode = "(at 'block-height (chain-data))" , _pactData = d }
+          , PactTransaction { _pactCode = "(at 'gas-limit (chain-data))" , _pactData = d }
+          , PactTransaction { _pactCode = "(at 'gas-price (chain-data))" , _pactData = d }
+          , PactTransaction { _pactCode = "(at 'chain-id (chain-data))" , _pactData = d }
+          , PactTransaction { _pactCode = "(at 'sender (chain-data))" , _pactData = d }
+          ] ++ commonTxs d )
+
+commonTxs :: (Maybe Value) -> [PactTransaction]
+commonTxs d =
+    [ PactTransaction { _pactCode = "(at 'prev-block-hash (chain-data))", _pactData = d }
+    , PactTransaction { _pactCode = "(at 'block-time (chain-data))", _pactData = d }
+    , PactTransaction { _pactCode = "(at 'block-height (chain-data))", _pactData = d }
+    , PactTransaction { _pactCode = "(at 'gas-limit (chain-data))", _pactData = d }
+    , PactTransaction { _pactCode = "(at 'gas-price (chain-data))", _pactData = d }
+    , PactTransaction { _pactCode = "(at 'chain-id (chain-data))", _pactData = d }
+    , PactTransaction { _pactCode = "(at 'sender (chain-data))", _pactData = d }
+    ]
+
 newBlockTest :: String -> IO PactQueue -> [BlockHeader] -> TestTree
-newBlockTest label reqIO = golden label blocks $ do
+newBlockTest label reqIO blocks = golden label $ do
     reqQ <- reqIO
-    let genesisHeader = genesisBlockHeader testVersion cidd
     let blockTime = Time $ secondsToTimeSpan $ Seconds $ succ 1000000
     newBlocksToQueue blocks
-    readFromQueue reqQ
+    readNFromQueue reqQ (length blocks)
   where
     cid = someChainId testVersion
 
