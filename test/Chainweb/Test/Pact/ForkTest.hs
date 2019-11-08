8b851e5992325341acdc48d0aabea63044669955
Mark Nichols <marklnichols@gmail.com>
2019-11-07 19:48:39 -0500

    A few more changes

--- test/Chainweb/Test/Pact/ForkTest.hs
+++ test/Chainweb/Test/Pact/ForkTest.hs
@@ -7,23 +7,25 @@ module Chainweb.Test.Pact.ForkTest
   ( tests
   ) where
 
-import Test.QuickCheck hiding ((.&.))
-import Test.QuickCheck.Monadic
-import Test.Tasty.QuickCheck
-
--- internal modules
-import Chainweb.BlockHeader
-import Chainweb.BlockHeaderDB
-import Chainweb.Mempool.Mempool
-import Chainweb.Test.ForkGen
-import Chainweb.Test.Utils
-
 import Control.Monad.IO.Class
 
 import Data.HashMap.Strict (HashMap)
 import qualified Data.HashMap.Strict as HM
 import Data.HashSet (HashSet)
 import Data.IORef
+
+import Test.QuickCheck hiding ((.&.))
+import Test.QuickCheck.Monadic
+import Test.Tasty
+import Test.Tasty.QuickCheck
+
+-- internal modules
+import Chainweb.BlockHeader
+import Chainweb.BlockHeaderDB
+import Chainweb.Mempool.Mempool
+import Chainweb.Pact.Service.PactQueue
+import Chainweb.Test.ForkGen
+import Chainweb.Test.Utils
 
 tests :: BlockHeaderDb -> BlockHeader -> ScheduledTest
 tests db h0 = testGroupSch "pact-fork-quickcheck-tests"
@@ -38,12 +40,14 @@ prop_forkValidates db genBlock = monadicIO $ do
     mapRef <- liftIO $ newIORef (HM.empty :: HashMap BlockHeader (HashSet TransactionHash))
     fi <- genFork db mapRef genBlock
     let blockList = blocksFromFork fi
-    liftIO $ putStrLn $ show fi
+    liftIO $ putStrLn $ "list of blocks:\n" ++ show blockList
+    -- liftIO $ putStrLn $ show fi
     newBlockTest TBD TBD blockList
-    assert (True == True)
+    assert (True == True) -- TODO: how to validate this test?
 
 blocksFromFork :: ForkInfo -> [BlockHeader]
 blocksFromFork ForkInfo{..} =
+    fiPreForkHeaders ++ fiLeftHeaders ++ fiRightHeaders
 
 ----------------------------------------------------------------------------------------------------
 -- Borrowed/modified from PactInProceApi test...
@@ -58,14 +62,15 @@ newBlockTest label reqIO = golden label blocks $ do
   where
     cid = someChainId testVersion
 
-newBlocksFToQueue :: [BlockHeader] -> PactQueue -> IO ()
-newBlocksToQueue blocks reqQ =
+newBlocksToQueue :: [BlockHeader] -> PactQueue -> IO ()
+newBlocksToQueue blocks reqQ = do
     let blockTime = Time $ secondsToTimeSpan $ Seconds $ succ 1000000
     forM blocks $ \h -> do
         respVar <- newBlock noMiner h (BlockCreationTime blockTime) reqQ
         -- how to validate the response...?
+        return ()
 
 readNFromQueue :: PactQueue -> Int -> _
-readFromQueue reqQ 0 = undefined
-readFromQueue reqQ n = undefined
+readNFromQueue reqQ 0 = undefined
+readNFromQueue reqQ n = undefined
   -- readNFromQueue reqQ (n-1)
