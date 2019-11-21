19869fa2cb710bdaf4c054679cb2196565753db3
Doug Beardsley <mightybyte@gmail.com>
2019-11-21 01:36:16 -0500

    Build docker with Linux

--- project.nix
+++ project.nix
@@ -1,6 +1,7 @@
 {
   pactRef ? "1e5741fa7ff4ebe9ec896efd65f15627ed232f30"
 , pactSha ? "1wg1nnxq59j7xb6m4w5dvxmr76idcpvpc6fmp9anyqrq27i4hc0l"
+, system ? builtins.currentSystem
 }:
 
 let
@@ -9,38 +10,42 @@ pactSrc = builtins.fetchTarball {
   sha256 = pactSha;
 };
 pactProj = "${pactSrc}/project.nix";
+rp = (import pactProj { inherit system; }).rp;
+proj =
+  rp.project ({ pkgs, hackGet, ... }:
+  let
+
+  gitignoreSrc = pkgs.fetchFromGitHub {
+    owner = "hercules-ci";
+    repo = "gitignore";
+    rev = "f9e996052b5af4032fe6150bba4a6fe4f7b9d698";
+    sha256 = "0jrh5ghisaqdd0vldbywags20m2cxpkbbk5jjjmwaw0gr8nhsafv";
+  };
+  inherit (import gitignoreSrc { inherit (pkgs) lib; }) gitignoreSource;
+
+  in {
+      name = "chainweb";
+      overrides = import ./overrides.nix { inherit pactSrc pkgs hackGet; };
+
+      packages = {
+        chainweb = gitignoreSource ./.;
+        #chainweb = gitignoreFilter
+        #  [ ".git" ".gitlab-ci.yml" "CHANGELOG.md" "README.md" "future-work.md" ] ./.;
+      };
+
+      shellToolOverrides = ghc: super: {
+        dnsutils = pkgs.dnsutils;
+        stack = pkgs.stack;
+        cabal-install = pkgs.haskellPackages.cabal-install;
+        ghcid = pkgs.haskellPackages.ghcid;
+        z3 = pkgs.z3;
+      };
+
+      shells = {
+        ghc = ["chainweb"];
+      };
+  });
 
 in
-  (import pactProj {}).rp.project ({ pkgs, hackGet, ... }:
-let
 
-gitignoreSrc = pkgs.fetchFromGitHub {
-  owner = "hercules-ci";
-  repo = "gitignore";
-  rev = "f9e996052b5af4032fe6150bba4a6fe4f7b9d698";
-  sha256 = "0jrh5ghisaqdd0vldbywags20m2cxpkbbk5jjjmwaw0gr8nhsafv";
-};
-inherit (import gitignoreSrc { inherit (pkgs) lib; }) gitignoreSource;
-
-in {
-    name = "chainweb";
-    overrides = import ./overrides.nix { inherit pactSrc pkgs hackGet; };
-
-    packages = {
-      chainweb = gitignoreSource ./.;
-      #chainweb = gitignoreFilter
-      #  [ ".git" ".gitlab-ci.yml" "CHANGELOG.md" "README.md" "future-work.md" ] ./.;
-    };
-
-    shellToolOverrides = ghc: super: {
-      dnsutils = pkgs.dnsutils;
-      stack = pkgs.stack;
-      cabal-install = pkgs.haskellPackages.cabal-install;
-      ghcid = pkgs.haskellPackages.ghcid;
-      z3 = pkgs.z3;
-    };
-
-    shells = {
-      ghc = ["chainweb"];
-    };
-  })
+{ inherit pactRef pactSrc rp proj; }
