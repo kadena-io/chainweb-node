0d88e5ec2b3d6c6881e28582f10e671825d887bd
Lars Kuhtz <lars@kadena.io>
2019-11-27 18:16:48 -0800

    improve cut db member ship queries (#756)
    * improve cut db member ship queries
    
    * add `verifyTransactionProofAt`, `verifyTransactionOutputProofAt` and add documenation to SPV API

--- src/Chainweb/SPV/VerifyProof.hs
+++ src/Chainweb/SPV/VerifyProof.hs
@@ -16,10 +16,12 @@ module Chainweb.SPV.VerifyProof
 -- * Transaction Proofs
   runTransactionProof
 , verifyTransactionProof
+, verifyTransactionProofAt
 
 -- * Transaction Output Proofs
 , runTransactionOutputProof
 , verifyTransactionOutputProof
+, verifyTransactionOutputProofAt
 ) where
 
 import Control.Monad.Catch
@@ -50,12 +52,35 @@ runTransactionProof :: TransactionProof SHA512t_256 -> BlockHash
 runTransactionProof (TransactionProof _ p)
     = BlockHash $ MerkleLogHash $ runMerkleProof p
 
+-- | Verifies the proof against the current state of consensus. The result
+-- confirms that the subject of the proof occurs in the history of the winning
+-- fork of the target chain.
+--
 verifyTransactionProof
     :: CutDb cas
     -> TransactionProof SHA512t_256
     -> IO Transaction
 verifyTransactionProof cutDb proof@(TransactionProof cid p) = do
     unlessM (member cutDb cid h) $ throwM
+        $ SpvExceptionVerificationFailed "target header is not in the chain"
+    proofSubject p
+  where
+    h = runTransactionProof proof
+
+-- | Verifies the proof for the given block hash. The result confirms that the
+-- subject of the proof occurs in the history of the target chain before the
+-- given block hash.
+--
+-- Throws 'TreeDbKeyNotFound' if the given block hash doesn't exist on target
+-- chain.
+--
+verifyTransactionProofAt
+    :: CutDb cas
+    -> TransactionProof SHA512t_256
+    -> BlockHash
+    -> IO Transaction
+verifyTransactionProofAt cutDb proof@(TransactionProof cid p) ctx = do
+    unlessM (memberOfM cutDb cid h ctx) $ throwM
         $ SpvExceptionVerificationFailed "target header is not in the chain"
     proofSubject p
   where
@@ -71,12 +96,35 @@ runTransactionOutputProof :: TransactionOutputProof SHA512t_256 -> BlockHash
 runTransactionOutputProof (TransactionOutputProof _ p)
     = BlockHash $ MerkleLogHash $ runMerkleProof p
 
+-- | Verifies the proof against the current state of consensus. The result
+-- confirms that the subject of the proof occurs in the history of the winning
+-- fork of the target chain.
+--
 verifyTransactionOutputProof
     :: CutDb cas
     -> TransactionOutputProof SHA512t_256
     -> IO TransactionOutput
 verifyTransactionOutputProof cutDb proof@(TransactionOutputProof cid p) = do
     unlessM (member cutDb cid h) $ throwM
+        $ SpvExceptionVerificationFailed "target header is not in the chain"
+    proofSubject p
+  where
+    h = runTransactionOutputProof proof
+
+-- | Verifies the proof for the given block hash. The result confirms that the
+-- subject of the proof occurs in the history of the target chain before the
+-- given block hash.
+--
+-- Throws 'TreeDbKeyNotFound' if the given block hash doesn't exist on target
+-- chain.
+--
+verifyTransactionOutputProofAt
+    :: CutDb cas
+    -> TransactionOutputProof SHA512t_256
+    -> BlockHash
+    -> IO TransactionOutput
+verifyTransactionOutputProofAt cutDb proof@(TransactionOutputProof cid p) ctx = do
+    unlessM (memberOfM cutDb cid h ctx) $ throwM
         $ SpvExceptionVerificationFailed "target header is not in the chain"
     proofSubject p
   where
