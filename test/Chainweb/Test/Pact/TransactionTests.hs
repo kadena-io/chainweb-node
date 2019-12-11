c38385529796f74aad2c9f2285771ded91f5ac2c
Emily Pillmore <emily@kadena.io>
2019-12-11 18:18:49 -0500

    initial tests WIP

--- test/Chainweb/Test/Pact/TransactionTests.hs
+++ test/Chainweb/Test/Pact/TransactionTests.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE QuasiQuotes #-}
 
@@ -18,6 +19,7 @@ import Test.Tasty
 import Test.Tasty.HUnit
 
 import Control.Concurrent (readMVar)
+import Control.Exception (SomeException, try)
 import Control.Lens hiding ((.=))
 import Data.Aeson
 import Data.Foldable (for_, traverse_)
@@ -29,6 +31,7 @@ import Data.Default
 
 import Pact.Repl
 import Pact.Repl.Types
+import Pact.Types.Command
 import Pact.Types.Runtime
 import Pact.Types.Logger
 import Pact.Interpreter
@@ -59,6 +62,10 @@ tests = testGroup "Chainweb.Test.Pact.TransactionTests"
     ]
   , testGroup "Coinbase tests"
     [ testCase "testCoinbase" testCoinbase
+    --, testCase "testCoinbaseGenesis" testCoinbaseGenesis
+    , testCase "testCoinbase791Fix" testCoinbase797Fix
+    , testCase "testCoinbaseNewBlock" testCoinbaseNewBlock
+    , testCase "testCoinbaseEnforceFailure" testCoinbaseEnforceFailure
     ]
   ]
 
@@ -102,6 +109,63 @@ testCoinbase = do
        mc
   where
     miner = noMiner
+    pubData = PublicData def blockHeight' blockTime (toText blockHash')
+    blockHsh@(BlockHash blockHash') = nullBlockHash
+    blockTime = toInt64 [timeMicrosQQ| 2019-12-17T01:00:00.0 |]
+    toInt64 (Time (TimeSpan (Micros m))) = m
+    blockHeight' = 123
+    logger = newLogger neverLog ""
+
+testCoinbase797Fix :: Assertion
+testCoinbase797Fix = do
+    (pdb,mc) <- loadCC
+    void $ applyCoinbase toyVersion logger pdb miner 0.1 pubData blockHsh
+     (EnforceCoinbaseFailure True) (CoinbaseUsePrecompiled False) mc
+  where
+    miner = noMiner
+    pubData = PublicData def blockHeight' blockTime (toText blockHash')
+    blockHsh@(BlockHash blockHash') = nullBlockHash
+    blockTime = toInt64 [timeMicrosQQ| 2019-12-16T01:00:00.0 |]
+    toInt64 (Time (TimeSpan (Micros m))) = m
+    blockHeight' = 123
+    logger = newLogger neverLog ""
+
+testCoinbaseNewBlock :: Assertion
+testCoinbaseNewBlock = do
+    (pdb,mc) <- loadCC
+    r <- try $ applyCoinbase toyVersion logger pdb miner 0.1 pubData blockHsh
+      (EnforceCoinbaseFailure True) (CoinbaseUsePrecompiled False) mc
+    case r of
+      Left (e :: SomeException) -> assertFailure $ "Coinbase tx failed: " <> show e
+      Right (CommandResult _ _ (PactResult (Left e)) _ _ _ _) ->
+        if isInfixOf "exploitable" (sshow e) then
+          return ()
+        else assertFailure (show e)
+      Right cr -> assertFailure $ "Code succeeded and did not fail" <> show cr
+  where
+    miner = Miner
+      (MinerId "NoMiner\" (read-keyset \"miner-keyset\") 1000.0)(module m g (defcap g () true ) (defun k () (enforce false \"exploitable\"))) (m.k) (coin.coinbase \"NoMiner")
+      (MinerKeys $ mkKeySet [] "<")
+    pubData = PublicData def blockHeight' blockTime (toText blockHash')
+    blockHsh@(BlockHash blockHash') = nullBlockHash
+    blockTime = toInt64 [timeMicrosQQ| 2019-12-16T01:00:00.0 |]
+    toInt64 (Time (TimeSpan (Micros m))) = m
+    blockHeight' = 123
+    logger = newLogger neverLog ""
+
+testCoinbaseEnforceFailure :: Assertion
+testCoinbaseEnforceFailure = do
+    (pdb,mc) <- loadCC
+    r <- try $ applyCoinbase toyVersion logger pdb miner 0.1 pubData blockHsh
+      (EnforceCoinbaseFailure True) (CoinbaseUsePrecompiled False) mc
+    case r of
+      Left (e :: SomeException) ->
+        if isInfixOf "Coinbase tx failure" (sshow e) then
+          return ()
+        else assertFailure $ "Coinbase failed for unknown reason: " <> show e
+      Right _ -> assertFailure "Coinbase did not failure on bad miner id"
+  where
+    miner = Miner (MinerId "") (MinerKeys $ mkKeySet [] "<")
     pubData = PublicData def blockHeight' blockTime (toText blockHash')
     blockHsh@(BlockHash blockHash') = nullBlockHash
     blockTime = toInt64 [timeMicrosQQ| 2019-12-17T01:00:00.0 |]
