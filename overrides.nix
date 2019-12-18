c6acc094aef0f0dabd6d7567cd0512be4afbcd68
Colin Woodbury <colin@fosskers.ca>
2019-12-18 14:05:54 -0800

    Up-pin wai/warp/servant

--- overrides.nix
+++ overrides.nix
@@ -125,10 +125,64 @@ let # Working on getting this function upstreamed into nixpkgs, but
         sha256 = "00bps2bmp3ahlfw6wf7ifnni8kn306bbzapqcgsallnpgzx62gp1";
       };
 
+      wai = dontCheck (callHackageDirect {
+        pkg = "wai";
+        ver = "3.2.2.1";
+        sha256 = "0msyixvsk37qsdn3idqxb4sab7bw4v9657nl4xzrwjdkihy411jf";
+      });
+
+      wai-cors = dontCheck (callHackageDirect {
+        pkg = "wai-cors";
+        ver = "0.2.7";
+        sha256 = "10yhjjkzp0ichf9ijiadliafriwh96f194c2g02anvz451capm6i";
+      });
+
+      wai-middleware-throttle = dontCheck (callHackageDirect {
+        pkg = "wai-middleware-throttle";
+        ver = "0.3.0.1";
+        sha256 = "13pz31pl7bk51brc88jp0gffjx80w35kzzrv248w27d7dc8xc63x";
+      });
+
+      wai-extra = dontCheck (callHackageDirect {
+        pkg = "wai-extra";
+        ver = "3.0.28";
+        sha256 = "1k470vbn2c852syj15m9xzfjnaraw6cyn35ajf2b67i01ghkshgw";
+      });
+
+      wai-app-static = doJailbreak (dontCheck (callHackageDirect {
+        pkg = "wai-app-static";
+        ver = "3.1.6.3";
+        sha256 = "00dzhv3cdkmxgid34y7bbrkp9940pcmr2brhl2wal7kp0y999ldp";
+      }));
+
+      warp = dontCheck (callHackageDirect {
+        pkg = "warp";
+        ver = "3.3.5";
+        sha256 = "1a5m90m8kq8ma9fh9qlqdf9bd9wy4dz1bi006d2qpg17jhhj5602";
+      });
+
       warp-tls = callHackageDirect {
         pkg = "warp-tls";
-        ver = "3.2.8";
-        sha256 = "0bf6gnyz9pq57y3hgv1xpfi1cnsda0wrwyd18zmh2220hxmvda71";
+        ver = "3.2.9";
+        sha256 = "14lwqr1yj9yh2adcm2h4q4kb4k3m5w416fzx5fl4f5qkicdqqjjx";
+      };
+
+      time-manager = callHackageDirect {
+        pkg = "time-manager";
+        ver = "0.0.0";
+        sha256 = "0z2fxikx5ax2x5bg8mcjg4y6b6irmf0swrnfprrp2xry6j5ji6hx";
+      };
+
+      http2 = callHackageDirect {
+        pkg = "http2";
+        ver = "2.0.3";
+        sha256 = "14bqmxla0id956y37fpfx9v6crwxphbfxkl8v8annrs8ngfbhbr7";
+      };
+
+      network-byte-order = callHackageDirect {
+        pkg = "network-byte-order";
+        ver = "0.1.2.0";
+        sha256 = "1a2kq8rmx5q3l1a3b3jcklm7hy3c3z0x08jnnwfik22sy5a5v2nr";
       };
 
       strict-tuple = callHackageDirect {
@@ -136,6 +190,30 @@ let # Working on getting this function upstreamed into nixpkgs, but
         ver = "0.1.3";
         sha256 = "1vg0m27phd6yf0pszcy2c2wbqx509fr9gacn34yja521z17cxd8z";
       };
+
+      servant = dontCheck (callHackageDirect {
+        pkg = "servant";
+        ver = "0.16.2";
+        sha256 = "1a83fdcwnlkmyc6fqsrfd26izcgk1jgpdgyqma3l897cnnr62frs";
+      });
+
+      servant-client = dontCheck (callHackageDirect {
+        pkg = "servant-client";
+        ver = "0.16.0.1";
+        sha256 = "1236sldcgvk2zj20cxib9yxrdxz7d1a83jfdnn9mxa272srfq9a9";
+      });
+
+      servant-client-core = dontCheck (callHackageDirect {
+        pkg = "servant-client-core";
+        ver = "0.16";
+        sha256 = "0panxplcjslsvqxvsabn2fy0fhcqmmr0dqj51hk7bk7yyvgwxklf";
+      });
+
+      servant-server = dontCheck (callHackageDirect {
+        pkg = "servant-server";
+        ver = "0.16.2";
+        sha256 = "1klcszpfqy1vn3q1wbqxjghfyddw8wbg4f0ggllqw8qx2f5zp5y1";
+      });
     };
 in
 
