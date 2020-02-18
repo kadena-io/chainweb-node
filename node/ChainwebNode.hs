15d0a04ccde84f98ca865debb6cf042a94df09b0
Stuart Popejoy <slpopejoy@users.noreply.github.com>
2020-02-17 19:18:21 -0500

    Changelog, killswitch to 2020-04-02

--- node/ChainwebNode.hs
+++ node/ChainwebNode.hs
@@ -494,7 +494,7 @@ pkgInfoScopes =
 -- KILLSWITCH for version 1.5
 --
 killSwitchDate :: Maybe String
-killSwitchDate = Just "2020-02-20T00:00:00Z"
+killSwitchDate = Just "2020-04-02T00:00:00Z"
 
 mainInfo :: ProgramInfo ChainwebNodeConfiguration
 mainInfo = programInfoValidate
