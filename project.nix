d651afe38a9fe056c96a5e55d44ac52b682efb03
Alexandre Esteves <alexandre.fmp.esteves@gmail.com>
2020-03-02 23:49:30 +0000

    Use kpkgs

--- project.nix
+++ project.nix
@@ -1,6 +1,7 @@
 { pactRef ? "731a9c07dd7486dc8d5c1b11905ceb34d9bd8af0"
 , pactSha ? "0gq0yngz69jxv4zxazclq3i5bwsics15f6z0ca5s7cmhg2jy5qbr"
 , system ? builtins.currentSystem
+, kpkgs ? import ./dep/kpkgs {}
 }:
 
 let
@@ -8,43 +9,28 @@ pactSrc = builtins.fetchTarball {
   url = "https://github.com/kadena-io/pact/archive/${pactRef}.tar.gz";
   sha256 = pactSha;
 };
-pactProj = "${pactSrc}/project.nix";
-rp = (import pactProj { inherit system; }).rp;
-proj =
-  rp.project ({ pkgs, hackGet, ... }:
-  let
 
-  gitignoreSrc = pkgs.fetchFromGitHub {
-    owner = "hercules-ci";
-    repo = "gitignore";
-    rev = "f9e996052b5af4032fe6150bba4a6fe4f7b9d698";
-    sha256 = "0jrh5ghisaqdd0vldbywags20m2cxpkbbk5jjjmwaw0gr8nhsafv";
-  };
-  inherit (import gitignoreSrc { inherit (pkgs) lib; }) gitignoreSource;
+proj = kpkgs.rp.project ({ pkgs, hackGet, ... }: with pkgs.haskell.lib; {
+    name = "chainweb";
+    overrides = self: super: (import ./overrides.nix { inherit pkgs; } self super) // {
+      pact = dontCheck ( addBuildDepend (self.callCabal2nix "pact" pactSrc {}) pkgs.z3);
+    };
 
-  in {
-      name = "chainweb";
-      overrides = import ./overrides.nix { inherit pactSrc pkgs hackGet; };
+    packages = {
+      chainweb = kpkgs.gitignoreSource ./.;
+    };
 
-      packages = {
-        chainweb = gitignoreSource ./.;
-        #chainweb = gitignoreFilter
-        #  [ ".git" ".gitlab-ci.yml" "CHANGELOG.md" "README.md" "future-work.md" ] ./.;
-      };
+    shellToolOverrides = ghc: super: {
+      dnsutils = pkgs.dnsutils;
+      stack = pkgs.stack;
+      cabal-install = pkgs.haskellPackages.cabal-install;
+      ghcid = pkgs.haskellPackages.ghcid;
+      z3 = pkgs.z3;
+    };
 
-      shellToolOverrides = ghc: super: {
-        dnsutils = pkgs.dnsutils;
-        stack = pkgs.stack;
-        cabal-install = pkgs.haskellPackages.cabal-install;
-        ghcid = pkgs.haskellPackages.ghcid;
-        z3 = pkgs.z3;
-      };
-
-      shells = {
-        ghc = ["chainweb"];
-      };
+    shells = {
+      ghc = ["chainweb"];
+    };
   });
 
-in
-
-{ inherit pactRef pactSrc rp proj; }
+in { inherit proj; }
