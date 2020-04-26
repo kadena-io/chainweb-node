b1d8ea5e967c9e343796eaee8eff1ebed057a764
Stuart Popejoy <slpopejoy@users.noreply.github.com>
2020-04-25 20:08:33 -0400

    refactor to get rid of dupe initPactService'

--- test/Chainweb/Test/Pact/ModuleCacheOnRestart.hs
+++ test/Chainweb/Test/Pact/ModuleCacheOnRestart.hs
@@ -13,12 +13,9 @@ import Control.Lens
 import Control.Monad
 import Control.Monad.IO.Class
 
-import Data.Default
 import qualified Data.HashMap.Strict as HM
 import Data.List (intercalate)
 import Data.Tuple.Strict (T2(..))
-import Data.Word
-import qualified Data.Text as T
 import qualified Data.Text.IO as T
 
 import Test.Tasty.HUnit
@@ -28,23 +25,16 @@ import System.LogLevel
 
 -- pact imports
 
-import Pact.Gas.Table
-import Pact.Types.Logger hiding (Logger)
-import Pact.Types.SPV
 import Pact.Types.Runtime (mdModule)
 import Pact.Types.Term
 
 -- chainweb imports
 
-import Chainweb.BlockCreationTime
 import Chainweb.BlockHeader
 import Chainweb.BlockHeader.Genesis
-import Chainweb.BlockHeaderDB hiding (withBlockHeaderDb)
 import Chainweb.ChainId
 import Chainweb.Logger
 import Chainweb.Miner.Pact
-import Chainweb.Pact.Backend.RelationalCheckpointer
-import Chainweb.Pact.Backend.Types
 import Chainweb.Pact.PactService
 import Chainweb.Pact.Types
 import Chainweb.Payload
@@ -153,63 +143,9 @@ withPact' bdbio iodir r ctest toTestTree =
         dir <- iodir
         sqlEnv <- startSqliteDb testVer testChainId logger (Just dir) Nothing False
         return $ (sqlEnv,) $ \(ps,cacheTest) -> do
-            T2 _ pstate <- initPactService'' testVer testChainId logger bhdb pdb sqlEnv defaultReorgLimit ps
+            T2 _ pstate <- initPactService' testVer testChainId logger
+                           bhdb pdb sqlEnv defaultPactServiceConfig ps
             cacheTest r (_psInitCache pstate)
 
     stopPact (sqlEnv, _) = stopSqliteDb sqlEnv
     logger = genericLogger Quiet T.putStrLn
-
--- We want a special version of initPactService'. The reason we need this
--- version is that initial version of initPactService' calls evalPactServicM,
--- which only returns the value of running the PactServiceM monad. This version
--- of the function needs both the final state and the value.
-initPactService''
-    :: Logger logger
-    => PayloadCasLookup cas
-    => ChainwebVersion
-    -> ChainId
-    -> logger
-    -> BlockHeaderDb
-    -> PayloadDb cas
-    -> SQLiteEnv
-    -> Word64
-    -> PactServiceM cas a
-    -> IO (T2 a PactServiceState)
-initPactService'' ver cid chainwebLogger bhDb pdb sqlenv reorgLimit act = do
-    checkpointEnv <- initRelationalCheckpointer initBlockState sqlenv logger testVer
-    let !rs = readRewards ver
-        !gasModel = tableGasModel defaultGasConfig
-        !t0 = BlockCreationTime $ Time (TimeSpan (Micros 0))
-        !pse = PactServiceEnv
-                { _psMempoolAccess = Nothing
-                , _psCheckpointEnv = checkpointEnv
-                , _psPdb = pdb
-                , _psBlockHeaderDb = bhDb
-                , _psGasModel = gasModel
-                , _psMinerRewards = rs
-                , _psReorgLimit = reorgLimit
-                , _psOnFatalError = defaultOnFatalError (logFunctionText chainwebLogger)
-                , _psVersion = ver
-                , _psValidateHashesOnReplay = True
-                , _psAllowReadsInLocal = False
-                }
-        !pst = PactServiceState Nothing mempty 0 t0 Nothing noSPVSupport
-    runPactServiceM pst pse act
-  where
-    loggers = pactLoggers chainwebLogger
-    logger = newLogger loggers $ LogName ("PactService" <> show cid)
-
-pactLoggers :: Logger logger => logger -> Loggers
-pactLoggers logger = Loggers $ mkLogger (error "ignored") fun def
-  where
-    fun :: LoggerLogFun
-    fun _ (LogName n) cat msg = do
-        let namedLogger = addLabel ("logger", T.pack n) logger
-        logFunctionText namedLogger (pactLogLevel cat) $ T.pack msg
-
-pactLogLevel :: String -> LogLevel
-pactLogLevel "INFO" = Info
-pactLogLevel "ERROR" = Error
-pactLogLevel "DEBUG" = Debug
-pactLogLevel "WARN" = Warn
-pactLogLevel _ = Info
