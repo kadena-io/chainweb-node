fda98c0647c1b84d0b323abf55b02619975e0109
Emily Pillmore <emilypi@cohomolo.gy>
2020-06-17 18:59:12 -0400

    boilerplate extravaganza

--- test/Chainweb/Test/Rosetta.hs
+++ test/Chainweb/Test/Rosetta.hs
@@ -8,9 +8,10 @@ import Test.Tasty
 import Test.Tasty.HUnit
 
 import Chainweb.Graph
+import Chainweb.Rosetta.RestAPI.Client
+import Chainweb.Test.Utils
 import Chainweb.Time (Time(..), Micros(..))
 import Chainweb.Version
-import Chainweb.Test.Utils
 
 import Data.CAS.RocksDB
 
@@ -25,7 +26,23 @@ v = FastTimedCPM petersonChainGraph
 nodes:: Natural
 nodes = 1
 
-type RosettaTest = IO (Time Micros) -> IO ChainwebNetwork -> TestTree
+type RosettaTest = IO Time Micros -> IO ChainwebNetwork -> TestTree
+
+data RosettaTestException
+    = AccountBalanceFailure String
+    | BlockTransactionFailure String
+    | BlockFailure String
+    | ConstructionMetadataFailure String
+    | ConstructionSubmitFailure String
+    | MempoolTransactionFailure String
+    | MempoolFailure String
+    | NetworkListFailure String
+    | NetworkOptionsFailure String
+    | NetworkStatusFailure String
+    deriving Show
+
+instance Exception RosettaTestException
+
 
 nid :: NetworkId
 nid = NetworkId
@@ -49,9 +66,9 @@ tests rdb = testGroupSch "Chainweb.Test.Rosetta" go
   where
     go = return $
       withNodes v "rosettaRemoteTests-" rdb nodes $ \nio ->
-      withTime $ \tio -> tgrp nio tio
+      withTime $ \tio -> tgroup nio tio
 
-    tgrp nio tio
+    tgroup nio tio
       = testGroup "Rosetta API tests"
       $ fmap (\test -> test tio nio) tests_
 
@@ -69,11 +86,11 @@ tests rdb = testGroupSch "Chainweb.Test.Rosetta" go
 
 
 accountBalanceTests :: RosettaTest
-accountBalanceTests _tio _nio = testCaseSteps "Account Balance Lookup" $ \ _step -> do
-    _cenv <- _runClientEnv <$> _nio
+accountBalanceTests _tio _nio = testCaseSteps "Account Balance Lookup" $ \step -> do
+    ccenv <- _runClientEnv <$> _nio
     return ()
   where
-    _req = AccountBalanceReq nid aid Nothing
+    req = AccountBalanceReq nid aid Nothing
 
 blockTransactionTests :: RosettaTest
 blockTransactionTests _tio _nio = undefined
@@ -98,3 +115,186 @@ networkOptionsTests _tio _nio = undefined
 
 networkStatusTests :: RosettaTest
 networkStatusTests _tio _nio = undefined
+
+-- ------------------------------------------------------------------ --
+-- Rosetta api w/ retry
+
+accountBalance
+    :: ClientEnv
+    -> AccountBalanceReq
+    -> IO AccountBalanceResp
+accountBalance cenv req =
+    recovering testRetryPolicy [h] $ \s -> do
+    debug
+      $ "requesting account balance for " <> (take 18 $ show req)
+      <> " [" <> show (view rsIterNumberL s) <> "]"
+
+    runClientM (rosettaAccountBalanceApiClient v cmd) cenv >>= \case
+      Left e -> throwM $ AccountBalanceFailure (show e)
+      Right t -> return t
+  where
+    h _ = Handler $ \case
+      AccountBalanceFailure _ -> return True
+      _ -> return False
+
+blockTransaction
+    :: ClientEnv
+    -> BlockTransactionReq
+    -> IO BlockTransactionResp
+blockTransaction cenv req =
+    recovering testRetryPolicy [h] $ \s -> do
+    debug
+      $ "requesting block transaction for " <> (take 18 $ show req)
+      <> " [" <> show (view rsIterNumberL s) <> "]"
+
+    runClientM (rosettaBlockTransactionApiClient v cmd) cenv >>= \case
+      Left e -> throwM $ BlockTransactionFailure (show e)
+      Right t -> return t
+  where
+    h _ = Handler $ \case
+      BlockTransactionFailure _ -> return True
+      _ -> return False
+
+block
+    :: ClientEnv
+    -> BlockReq
+    -> IO BlockResp
+block cenv req =
+    recovering testRetryPolicy [h] $ \s -> do
+    debug
+      $ "requesting block for " <> (take 18 $ show req)
+      <> " [" <> show (view rsIterNumberL s) <> "]"
+
+    runClientM (rosettaBlockApiClient v cmd) cenv >>= \case
+      Left e -> throwM $ BlockFailure (show e)
+      Right t -> return t
+  where
+    h _ = Handler $ \case
+      BlockFailure _ -> return True
+      _ -> return False
+
+constructionMetadata
+    :: ClientEnv
+    -> ConstructionMetadataReq
+    -> IO ConstructionMetadataResp
+constructionMetadata cenv req =
+    recovering testRetryPolicy [h] $ \s -> do
+    debug
+      $ "requesting construction metadata for " <> (take 18 $ show req)
+      <> " [" <> show (view rsIterNumberL s) <> "]"
+
+    runClientM (rosettaConstructionMetadataApiClient v cmd) cenv >>= \case
+      Left e -> throwM $ ConstructionMetadataFailure (show e)
+      Right t -> return t
+  where
+    h _ = Handler $ \case
+      ConstructionMetadataFailure _ -> return True
+      _ -> return False
+
+constructionSubmit
+    :: ClientEnv
+    -> ConstructionSubmitReq
+    -> IO ConstructionSubmitResp
+constructionSubmit cenv req =
+    recovering testRetryPolicy [h] $ \s -> do
+    debug
+      $ "requesting construction submit for " <> (take 18 $ show req)
+      <> " [" <> show (view rsIterNumberL s) <> "]"
+
+    runClientM (rosettaConstructionSubmitApiClient v cmd) cenv >>= \case
+      Left e -> throwM $ ConstructionSubmitFailure (show e)
+      Right t -> return t
+  where
+    h _ = Handler $ \case
+      ConstructionSubmitFailure _ -> return True
+      _ -> return False
+
+mempoolTransaction
+    :: ClientEnv
+    -> MempoolTransactionReq
+    -> IO MempoolTransactionResp
+mempoolTransaction cenv req =
+    recovering testRetryPolicy [h] $ \s -> do
+    debug
+      $ "requesting mempool transaction for " <> (take 18 $ show req)
+      <> " [" <> show (view rsIterNumberL s) <> "]"
+
+    runClientM (rosettaMempoolTransactionApiClient v cmd) cenv >>= \case
+      Left e -> throwM $ MempoolTransactionFailure (show e)
+      Right t -> return t
+  where
+    h _ = Handler $ \case
+      MempoolTransactionFailure _ -> return True
+      _ -> return False
+
+mempool
+    :: ClientEnv
+    -> MempoolReq
+    -> IO MempoolResp
+mempool cenv req =
+    recovering testRetryPolicy [h] $ \s -> do
+    debug
+      $ "requesting mempool for " <> (take 18 $ show req)
+      <> " [" <> show (view rsIterNumberL s) <> "]"
+
+    runClientM (rosettaMempoolTransctionApiClient v cmd) cenv >>= \case
+      Left e -> throwM $ MempoolFailure (show e)
+      Right t -> return t
+  where
+    h _ = Handler $ \case
+      MempoolFailure _ -> return True
+      _ -> return False
+
+networkList
+    :: ClientEnv
+    -> NetworkListReq
+    -> IO NetworkListResp
+networkList cenv req =
+    recovering testRetryPolicy [h] $ \s -> do
+    debug
+      $ "requesting network list for " <> (take 18 $ show req)
+      <> " [" <> show (view rsIterNumberL s) <> "]"
+
+    runClientM (rosettaNetworkListApiClient v cmd) cenv >>= \case
+      Left e -> throwM $ NetworkListFailure (show e)
+      Right t -> return t
+  where
+    h _ = Handler $ \case
+      NetworkListFailure _ -> return True
+      _ -> return False
+
+networkOptions
+    :: ClientEnv
+    -> NetworkOptionsReq
+    -> IO NetworkOptionsResp
+networkOptions cenv req =
+    recovering testRetryPolicy [h] $ \s -> do
+    debug
+      $ "requesting network options for " <> (take 18 $ show req)
+      <> " [" <> show (view rsIterNumberL s) <> "]"
+
+    runClientM (rosettaNetworkOptionsApiClient v cmd) cenv >>= \case
+      Left e -> throwM $ NetworkOptionsFailure (show e)
+      Right t -> return t
+  where
+    h _ = Handler $ \case
+      NetworkOptionsFailure _ -> return True
+      _ -> return False
+
+networkStatus
+    :: ClientEnv
+    -> NetworkStatusReq
+    -> IO NetworkStatusResp
+networkStatus cenv req =
+    recovering testRetryPolicy [h] $ \s -> do
+    debug
+      $ "requesting network status for " <> (take 18 $ show req)
+      <> " [" <> show (view rsIterNumberL s) <> "]"
+
+    runClientM (rosettaNetworkStatusApiClient v cmd) cenv >>= \case
+      Left e -> throwM $ NetworkStatusFailure (show e)
+      Right t -> return t
+  where
+    h _ = Handler $ \case
+      NetworkStatusFailure _ -> return True
+      _ -> return False
