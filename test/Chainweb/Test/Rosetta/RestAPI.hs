741c334feddd6c6be69e10362696446b91cf3342
Emily Pillmore <emilypi@cohomolo.gy>
2020-06-23 19:02:49 -0400

    making progress. TODO's listed

--- test/Chainweb/Test/Rosetta/RestAPI.hs
+++ test/Chainweb/Test/Rosetta/RestAPI.hs
@@ -3,6 +3,7 @@
 {-# LANGUAGE NumericUnderscores #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE TypeApplications #-}
 module Chainweb.Test.Rosetta.RestAPI
 ( tests
 ) where
@@ -13,9 +14,10 @@ import Control.Lens
 
 import qualified Data.Aeson as A
 import Data.Functor (void)
-import qualified Data.List.NonEmpty as NEL
 import qualified Data.HashMap.Strict as HM
+import Data.IORef
 import Data.Text (Text)
+import Data.Foldable (traverse_)
 
 import GHC.Natural
 
@@ -45,6 +47,7 @@ import Data.CAS.RocksDB
 
 import Rosetta
 
+import System.IO.Unsafe (unsafePerformIO)
 
 -- -------------------------------------------------------------------------- --
 -- Global Settings
@@ -58,16 +61,15 @@ nodes = 1
 cid :: ChainId
 cid = unsafeChainId 0
 
-rosettaVersion :: RosettaNodeVersion
-rosettaVersion = RosettaNodeVersion
-    { _version_rosettaVersion = "1.3.1"
-    , _version_nodeVersion = "1.9"
-    , _version_middlewareVersion = Nothing
-    , _version_metadata = Just $ HM.fromList
-      [ "node-api-version" A..= ("0.0" :: Text)
-      , "chainweb-version" A..= ("fastTimedCPM-peterson" :: Text)
-      ]
-    }
+cids :: [Text]
+cids = chainIds v ^.. folded . to (sshow @Int. chainIdInt)
+
+nonceRef :: IORef Natural
+nonceRef = unsafePerformIO $ newIORef 0
+
+
+type RosettaTest
+    = IO (Time Micros) -> IO ClientEnv -> TestTree
 
 -- -------------------------------------------------------------------------- --
 -- Test Tree
@@ -83,7 +85,6 @@ tests rdb = testGroup "Chainweb.Test.Rosetta.RestAPI" go
       [ accountBalanceTests
       , blockTransactionTests
       , blockTests
-      , constructionMetadataTests
       , constructionSubmitTests
       , mempoolTransactionTests
       , mempoolTests
@@ -101,9 +102,7 @@ accountBalanceTests tio envIo = testCaseSteps "Account Balance Lookup" $ \step -
     checkBalance resp0 100000000.000
 
     step "send 1.0 tokens to sender00 from sender01"
-    batch0 <- transferOne tio
-    rks <- sending cid cenv batch0
-    void $ polling cid cenv rks ExpectPactResult
+    void $ transferOne tio cenv
 
     step "check post-transfer balance"
     resp1 <- accountBalance cenv req
@@ -121,29 +120,37 @@ accountBalanceTests tio envIo = testCaseSteps "Account Balance Lookup" $ \step -
 
 blockTransactionTests :: RosettaTest
 blockTransactionTests tio envIo =
-    testCaseSteps "Block Transaction Tests" $ \step -> return ()
+    testCaseSteps "Block Transaction Tests" $ \step -> do
+      cenv <- envIo
+
+      step "fetch genesis tx id"
+      -- resp <- blockTransaction cenv req
+
+      return ()
+  where
+    req = BlockTransactionReq nid genesisId genesisTxId
 
 blockTests :: RosettaTest
 blockTests tio envIo = testCaseSteps "Block Tests" $ \step -> do
     step "fetch genesis block"
     cenv <- envIo
-    resp0 <- _block_blockId . _blockResp_block <$> block cenv req0
+    resp0 <- _block_blockId . _blockResp_block <$> block cenv (req 0)
     resp0 @=? genesisId
 
-    step "send transaction in at block height 1"
-    batch0 <- transferOne tio
-    rks <- sending cid cenv batch0
-    prs <- polling cid cenv rks ExpectPactResult
+    step "send transaction"
+    prs <- transferOne tio cenv
     cmdMeta <- extractMetadata prs
     bh <- cmdMeta ^?! ix "blockHeight" . to fromAeson
 
-    step "check tx at block height 1 matches sent tx"
-    resp1 <- block cenv $ BlockReq nid $ PartialBlockId (Just bh) Nothing
-    print resp1
+    step "check tx at block height matches sent tx"
+    resp1 <- block cenv (req bh)
 
+    step "validate remediations at block height 1"
+    remResp <- block cenv (req 1)
+    -- check remediation tx id's
     return ()
   where
-    req0 = BlockReq nid $ PartialBlockId (Just 0) Nothing
+    req h = BlockReq nid $ PartialBlockId (Just h) Nothing
 
     fromAeson = aeson assertFailure return . A.fromJSON
 
@@ -151,10 +158,6 @@ blockTests tio envIo = testCaseSteps "Block Tests" $ \step -> do
       case _crMetaData . snd . head . HM.toList $ pr of
         Nothing -> assertFailure "test transfer did not succeed"
         Just (A.Object o) -> return o
-
-constructionMetadataTests :: RosettaTest
-constructionMetadataTests tio envIo =
-    testCaseSteps "Construction Metadata Tests" $ \step -> return ()
 
 constructionSubmitTests :: RosettaTest
 constructionSubmitTests tio envIo =
@@ -169,45 +172,53 @@ mempoolTests tio envIo =
     testCaseSteps "Mempool Tests" $ \step -> return ()
 
 networkListTests :: RosettaTest
-networkListTests tio envIo = testCaseSteps "Network List Tests" $ \step -> do
+networkListTests _ envIo = testCaseSteps "Network List Tests" $ \step -> do
     cenv <- envIo
-
     step "send network list request"
     resp <- networkList cenv req
-    print resp
-    return ()
+    traverse_ checkChainIds (_networkListResp_networkIds resp)
   where
     req = MetadataReq Nothing
 
+    checkChainIds n = do
+      _networkId_blockchain n @=? "kadena"
+      _networkId_network n @=? "fastTimedCPM-peterson"
+      assertBool "chain id of subnetwork is valid" $
+        case _networkId_subNetworkId n of
+          Nothing -> False
+          Just a -> elem (_subNetworkId_network a) cids
+
 networkOptionsTests :: RosettaTest
-networkOptionsTests tio envIo = testCaseSteps "Network Options Tests" $ \step -> do
+networkOptionsTests _ envIo = testCaseSteps "Network Options Tests" $ \step -> do
     cenv <- envIo
 
     step "send network options request"
-    resp <- networkOptions cenv req
+    resp <- networkOptions cenv req0
 
     step "check options response against node version"
     _networkOptionsResp_version resp @=? rosettaVersion
+
+    -- enumerate over exceptions
+
   where
-    req = NetworkReq nid Nothing
+    req0 = NetworkReq nid Nothing
 
 networkStatusTests :: RosettaTest
 networkStatusTests tio envIo = testCaseSteps "Network Status Tests" $ \step -> do
     cenv <- envIo
 
     step "send network status request"
+    transferOne_ tio cenv
     resp <- networkStatus cenv req
 
     step "check status response against genesis"
     genesisId @=? _networkStatusResp_genesisBlockId resp
+    -- check current block (should be updated by tx)
   where
     req = NetworkReq nid Nothing
 
 -- ------------------------------------------------------------------ --
 -- Test Data
-
-type RosettaTest
-    = IO (Time Micros) -> IO ClientEnv -> TestTree
 
 kda :: Currency
 kda = Currency "KDA" 12 Nothing
@@ -235,22 +246,53 @@ aid = AccountId
 genesisId :: BlockId
 genesisId = BlockId 0 "d69wD5SUpshDI6rbmQGugDXTd1-riqr7gfg5ZjvUrqk"
 
+rosettaVersion :: RosettaNodeVersion
+rosettaVersion = RosettaNodeVersion
+    { _version_rosettaVersion = "1.3.1"
+    , _version_nodeVersion = "1.9"
+    , _version_middlewareVersion = Nothing
+    , _version_metadata = Just $ HM.fromList
+      [ "node-api-version" A..= ("0.0" :: Text)
+      , "chainweb-version" A..= ("fastTimedCPM-peterson" :: Text)
+      ]
+    }
+
+genesisTxId :: TransactionId
+genesisTxId = TransactionId "Inlsd2hVbVVBOUtnZjM5d191c2dGRHoycV9RX09YX1lMQmNDMXZBSC1Mc0Ei"
+
+-- write out allowables and check failures against alloweds
+
 -- ------------------------------------------------------------------ --
 -- Test Pact Cmds
 
-transferOne :: IO (Time Micros) -> IO SubmitBatch
-transferOne tio = do
-    t <- toTxCreationTime <$> tio
-    c <- buildTextCmd
-      $ set cbSigners
-        [ mkSigner' sender00
-          [ mkTransferCap "sender00" "sender01" 1.0
-          , mkGasCap
+-- | Transfer one token from sender00 to sender01, polling for responses
+--
+transferOne :: IO (Time Micros) -> ClientEnv -> IO PollResponses
+transferOne tio cenv = do
+    batch0 <- mkTransfer
+    rks <- sending cid cenv batch0
+    prs <- polling cid cenv rks ExpectPactResult
+    return prs
+  where
+    mkTransfer = do
+      t <- toTxCreationTime <$> tio
+      n <- readIORef nonceRef
+      c <- buildTextCmd
+        $ set cbSigners
+          [ mkSigner' sender00
+            [ mkTransferCap "sender00" "sender01" 1.0
+            , mkGasCap
+            ]
           ]
-        ]
-      $ set cbCreationTime t
-      $ set cbNetworkId (Just v)
-      $ mkCmd ("nonce-transfer-" <> sshow t)
-      $ mkExec' "(coin.transfer \"sender00\" \"sender01\" 1.0)"
+        $ set cbCreationTime t
+        $ set cbNetworkId (Just v)
+        $ mkCmd ("nonce-transfer-" <> sshow t <> "-" <> sshow n)
+        $ mkExec' "(coin.transfer \"sender00\" \"sender01\" 1.0)"
 
-    return $ SubmitBatch (pure c)
+      modifyIORef' nonceRef (+1)
+      return $ SubmitBatch (pure c)
+
+-- | Transfer one, ignoring the resulting responses
+--
+transferOne_ :: IO (Time Micros) -> ClientEnv -> IO ()
+transferOne_ tio cenv = void $! transferOne tio cenv
