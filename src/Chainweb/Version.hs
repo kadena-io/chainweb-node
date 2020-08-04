2357a4a1f88c73046daf3dfd1d76d630cfc202f0
Lars Kuhtz <lars@kadena.io>
2020-08-04 10:02:52 -0700

    Fix mainnet coin v2 upgrade for new chains (#1130)
    * fix coinV2Upgrade guard or new mainnet chains
    
    * set vuln797Fix to True for new mainnet chains

--- src/Chainweb/Version.hs
+++ src/Chainweb/Version.hs
@@ -752,7 +752,6 @@ vuln797Fix Mainnet01 cid h
     | cid == unsafeChainId 7 = h >= 121451
     | cid == unsafeChainId 8 = h >= 121452
     | cid == unsafeChainId 9 = h >= 121451
-    | otherwise = error $ "invalid chain id " <> sshow cid
 vuln797Fix _ _ _ = True
 {-# INLINE vuln797Fix #-}
 
@@ -777,7 +776,7 @@ coinV2Upgrade Mainnet01 cid h
     | cid == unsafeChainId 7 = h == 140809
     | cid == unsafeChainId 8 = h == 140808
     | cid == unsafeChainId 9 = h == 140808
-    | otherwise = error $ "invalid chain id " <> sshow cid
+    -- new chains on mainnet start already with v2 deployed in the genesis block
 coinV2Upgrade Testnet04 cid h
     | chainIdInt @Int cid >= 10  && chainIdInt @Int cid < 20 = h == 337000
     | otherwise = h == 1
