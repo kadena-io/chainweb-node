46e8a072d9ece4489f51a93406ecb45883d6b1bd
Lars Kuhtz <lars@kadena.io>
2020-12-08 21:13:45 +0100

    Update src/Chainweb/Pact/PactService/Checkpointer.hs

--- src/Chainweb/Pact/PactService/Checkpointer.hs
+++ src/Chainweb/Pact/PactService/Checkpointer.hs
@@ -642,6 +642,5 @@ blockStreamProgress
     -> S.Stream (S.Of BlockHeader) m r
     -> S.Stream (S.Of BlockHeader) m r
 blockStreamProgress n logFun = progress n $ \i b -> logFun
-    $ "processes blocks: " <> sshow i
+    $ "processed blocks: " <> sshow i
     <> ", current height: " <> sshow (_blockHeight b)
-
