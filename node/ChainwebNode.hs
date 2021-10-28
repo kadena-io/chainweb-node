11759c8742997a0c40ad270dcc39b2f5e35cc3b7
Emmanuel Denloye-Ito <eod8tb@virginia.edu>
2021-10-28 18:34:06 -0400

    Revert "temporarily disable killswitch for testing"
    This reverts commit 4e1021ae34ccfe465ce52cf0324a77fdd04747e9.

--- node/ChainwebNode.hs
+++ node/ChainwebNode.hs
@@ -459,8 +459,7 @@ pkgInfoScopes =
 -- KILLSWITCH for version 2.10
 --
 killSwitchDate :: Maybe String
-killSwitchDate = Nothing
--- killSwitchDate = Just "2021-11-18T00:00:00Z"
+killSwitchDate = Just "2021-11-18T00:00:00Z"
 
 mainInfo :: ProgramInfo ChainwebNodeConfiguration
 mainInfo = programInfoValidate
