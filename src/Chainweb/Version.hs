e7f77e16afbe98994b7f03cf1e6aeedafe2ec9c3
Stuart Popejoy <sirlensalot@users.noreply.github.com>
2022-02-10 13:15:27 -0500

    Review changes

--- src/Chainweb/Version.hs
+++ src/Chainweb/Version.hs
@@ -914,10 +914,11 @@ doCheckTxHash Development = (>= 110)
 doCheckTxHash (FastTimedCPM g) | g == petersonChainGraph = (>= 7)
 doCheckTxHash _ = const False
 
--- | Placeholder name for 2.13 Pact changes
+-- | Omnibus pact changes for 2.13:
+-- | - miner keyset enforce
 chainweb213Pact :: ChainwebVersion -> BlockHeight -> Bool
-chainweb213Pact Mainnet01 = const False -- TODO
-chainweb213Pact Testnet04 = const False -- TODO
+chainweb213Pact Mainnet01 = (>= 2_447_358) -- 2022-02-26 00:00:18
+chainweb213Pact Testnet04 = (>= 1_977_816) -- 2022-02-25 00:00:18
 chainweb213Pact Development = (>= 95)
 chainweb213Pact (FastTimedCPM g) | g == petersonChainGraph = (>= 5)
 chainweb213Pact _ = const False
