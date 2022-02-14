2f0db6c98c1479a6015f99c3bae3945a04dae7e7
Linda Ortega <linda.ortega2013@gmail.com>
2022-02-14 13:33:26 -0500

    change the gas limit back

--- src/Chainweb/Rosetta/Utils.hs
+++ src/Chainweb/Rosetta/Utils.hs
@@ -479,11 +479,11 @@ getSuggestedFee tx someMaxFees someMult = do
     ------------
     -- NOTE: GasLimit should never be greater than default block gas limit.
 
-    -- Derived from a couple of gas unit cost of the following transfer transactions:
+    -- Derived from a couple of gas unit cost of the following transfer transactions + some buffer:
     -- - https://explorer.chainweb.com/testnet/tx/g8dxg1CAM3eZ5S-rk51N27N8-nKEW3Wg_cyk5moqmBg
     -- - https://explorer.chainweb.com/testnet/tx/IGVzaRkTHOSMIiHM7q8bPxrATW5b5SEhoCqE6tPkVFA
     -- - https://explorer.chainweb.com/testnet/tx/cK0B0XOkOlMDR32GloR0GQvjAWAJ9mvNPZwQDalPr6c
-    defGasUnitsTransferCreate = 566
+    defGasUnitsTransferCreate = 600
 
     -- See Chainweb.Chainweb.Configuration for latest min gas
     minGasPrice = Decimal 8 1
