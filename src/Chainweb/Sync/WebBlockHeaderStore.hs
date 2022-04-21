5dd29c66fad81ddd5047b61700549379a4758f5c
Edmund Noble <edmundnoble@gmail.com>
2022-04-21 13:58:21 -0400

    Try grabbing block headers from the header store first

--- src/Chainweb/Sync/WebBlockHeaderStore.hs
+++ src/Chainweb/Sync/WebBlockHeaderStore.hs
@@ -279,19 +279,23 @@ getBlockHeaderInternal headerStore payloadStore candidateHeaderCas candidatePayl
 
         -- query BlockHeader via
         --
+        -- - header store,
         -- - candidates header cache,
         -- - cut origin, or
         -- - task queue of P2P network
         --
         (maybeOrigin', header) <- casLookup candidateHeaderCas k' >>= \case
             Just !x -> return (maybeOrigin, x)
-            Nothing -> pullOrigin k maybeOrigin >>= \case
-                Nothing -> do
-                    t <- queryBlockHeaderTask k
-                    pQueueInsert queue t
-                    (ChainValue _ !x) <- awaitTask t
-                    return (Nothing, x)
-                (Just !x) -> return (maybeOrigin, x)
+            Nothing -> casLookup (_webBlockHeaderStoreCas headerStore) k >>= \case
+                Just (ChainValue _ !x) -> return (maybeOrigin, x)
+                Nothing ->
+                    pullOrigin k maybeOrigin >>= \case
+                        Nothing -> do
+                            t <- queryBlockHeaderTask k
+                            pQueueInsert queue t
+                            (ChainValue _ !x) <- awaitTask t
+                            return (Nothing, x)
+                        (Just !x) -> return (maybeOrigin, x)
 
         -- Check that the chain id is correct. The candidate cas is indexed just
         -- by the block hash. So, if this fails it is most likely a bug in code
