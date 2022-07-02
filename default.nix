ace1c3bd5d9b93e368a4b583dc45bc3d8f2cb6a2
John Wiegley <johnw@newartisans.com>
2022-07-02 00:55:01 -0700

    Whitespace fix

--- default.nix
+++ default.nix
@@ -53,7 +53,7 @@ pkgs.haskell.packages.${compiler}.developPackage {
         pkg = "direct-sqlite";
         ver = "2.3.27";
         sha256 = "0w8wj3210h08qlws40qhidkscgsil3635zk83kdlj929rbd8khip";
-       } {};
+      } {};
 
       pact = appendConfigureFlag super.pact "-f-build-tool";
 
