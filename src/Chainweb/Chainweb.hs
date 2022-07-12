c1630487692e8b78244b359886de1226e446f511
Lars Kuhtz <lars@kadena.io>
2022-07-12 08:44:39 -0700

    Limit block gas limit (#1472)
    * add version property for block gas limit
    
    * Initial block gas limit
    
    * Address feedback
    
    * Update src/Chainweb/Version.hs
    
    Co-authored-by: Lars Kuhtz <lars@kadena.io>
    
    Co-authored-by: Edmund Noble <edmundnoble@gmail.com>
    Co-authored-by: Edmund Noble <edmundnoble@users.noreply.github.com>

--- src/Chainweb/Chainweb.hs
+++ src/Chainweb/Chainweb.hs
@@ -360,6 +360,10 @@ withChainwebInternal conf logger peer serviceSock rocksDb pactDbDir backupDir re
         -- initialize chains concurrently
         (\cid x -> do
             let mcfg = validatingMempoolConfig cid v (_configBlockGasLimit conf) (_configMinGasPrice conf)
+            -- NOTE: the gas limit may be set based on block height in future, so this approach may not be valid.
+            let maxGasLimit = fromIntegral $ maxBlockGasLimit v cid maxBound
+            when (_configBlockGasLimit conf > maxGasLimit) $
+                logg Warn "configured block gas limit is greater than the maximum for this chain; the maximum will be used instead"
             withChainResources
                 v
                 cid
@@ -368,7 +372,7 @@ withChainwebInternal conf logger peer serviceSock rocksDb pactDbDir backupDir re
                 mcfg
                 payloadDb
                 pactDbDir
-                pactConfig
+                (pactConfig maxGasLimit)
                 x
         )
 
@@ -379,7 +383,7 @@ withChainwebInternal conf logger peer serviceSock rocksDb pactDbDir backupDir re
         )
         cidsList
   where
-    pactConfig = PactServiceConfig
+    pactConfig maxGasLimit = PactServiceConfig
       { _pactReorgLimit = _configReorgLimit conf
       , _pactRevalidate = _configValidateHashesOnReplay conf
       , _pactQueueSize = _configPactQueueSize conf
@@ -388,7 +392,7 @@ withChainwebInternal conf logger peer serviceSock rocksDb pactDbDir backupDir re
       , _pactUnlimitedInitialRewind =
           isJust (_cutDbParamsInitialHeightLimit cutConfig) ||
           isJust (_cutDbParamsInitialCutFile cutConfig)
-      , _pactBlockGasLimit = _configBlockGasLimit conf
+      , _pactBlockGasLimit = min (_configBlockGasLimit conf) maxGasLimit
       }
 
     pruningLogger :: T.Text -> logger
