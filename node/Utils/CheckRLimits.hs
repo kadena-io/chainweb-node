723eac754a0f60e9a747807d0a613abe3ff8f5ab
Edmund Noble <edmundnoble@gmail.com>
2022-09-13 11:14:35 -0400

    First pass at checking file descriptor rlimit

---
+++ node/Utils/CheckRLimits.hs
@@ -0,0 +1,36 @@
+{-# language ScopedTypeVariables #-}
+
+module Utils.CheckRLimits(checkRLimits) where
+
+import Control.Monad
+import Data.Char
+import Data.List(stripPrefix)
+import System.Process
+import Text.Read
+
+checkRLimits :: IO ()
+checkRLimits = do
+    pid <- getCurrentPid
+    limits <- readFile $ "/proc/" <> show pid <> "/limits"
+    let
+        -- the format of /proc/<pid>/limits looks like
+        -- Something      its soft limit     its hard limit    the units of the limit
+        -- for example
+        -- Max open files     1024        1024       files
+        -- we only care about the soft limit, which is the real limit for the process.
+        openFileLimit =
+            [ n
+            | l <- lines limits
+            , Just rest <- [stripPrefix "Max open files" l]
+            , Just (n :: Int) <- [readMaybe (takeWhile isDigit $ dropWhile isSpace rest)]
+            ]
+    case openFileLimit of
+        [n] ->
+            when (n < 32768) $
+                error $
+                    "This process is only able to open " <> show n <> " file descriptors at once, "
+                    <> "which is not enough to run chainweb-node.\n" <>
+                    "Set the limit higher than 32768 using the ulimit command or contact an administrator."
+        -- this check is conservative to avoid crashing nodes that don't deserve it
+        _ ->
+            putStrLn "Couldn't find out the open file descriptor limit on your system"
