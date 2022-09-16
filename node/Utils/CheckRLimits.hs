433deb9736d3baaea62832d76e0cf54b231e63ee
Edmund Noble <edmundnoble@gmail.com>
2022-09-16 10:05:22 -0400

    Use stderr to print message and exitFailure after, rather than using error

--- node/Utils/CheckRLimits.hs
+++ node/Utils/CheckRLimits.hs
@@ -5,6 +5,8 @@ module Utils.CheckRLimits(checkRLimits) where
 import Control.Monad
 import Data.Char
 import Data.List(stripPrefix)
+import System.Exit
+import System.IO
 import System.Process
 import Text.Read
 
@@ -26,11 +28,12 @@ checkRLimits = do
             ]
     case openFileLimit of
         [n] ->
-            when (n < 32768) $
-                error $
+            when (n < 32768) $ do
+                hPutStrLn stderr $
                     "This process is only able to open " <> show n <> " file descriptors at once, "
                     <> "which is not enough to run chainweb-node.\n" <>
                     "Set the limit higher than 32768 using the ulimit command or contact an administrator."
+                exitFailure
         -- this check is conservative to avoid crashing nodes that don't deserve it
         _ ->
             putStrLn "Couldn't find out the open file descriptor limit on your system"
