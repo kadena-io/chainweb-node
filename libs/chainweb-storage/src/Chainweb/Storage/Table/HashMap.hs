6339ce42ee458af5ff1ad88f019ce2152d21e611
Edmund Noble <edmundnoble@gmail.com>
2022-09-26 18:43:22 -0400

    add a Table abstraction replacing the Cas abstraction

---
+++ libs/chainweb-storage/src/Chainweb/Storage/Table/HashMap.hs
@@ -0,0 +1,66 @@
+{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE UndecidableInstances #-}
+
+-- |
+-- Module: Chainweb.Storage.Table.HashMap
+-- Copyright: Copyright © 2019 Kadena LLC.
+-- License: MIT
+-- Maintainer: Lars Kuhtz <lars@kadena.io>
+-- Stability: experimental
+-- Description:
+--
+-- A thread-safe in-memory 'Table' implementation based on 'HM.HashMap'.
+--
+module Chainweb.Storage.Table.HashMap
+( HashMapTable
+, emptyTable
+, toList
+, size
+) where
+
+import Control.Concurrent.STM.TVar
+import Control.Monad ((<$!>))
+import Control.Monad.STM
+
+import Data.Hashable
+import qualified Data.HashMap.Strict as HM
+
+-- internal modules
+
+import Chainweb.Storage.Table
+
+-- | An 'IsTable' implementation that is base on 'HM.HashMap'.
+--
+data HashMapTable k v = HashMapTable !(TVar (HM.HashMap k v))
+
+instance Hashable k => ReadableTable (HashMapTable k v) k v where
+    tableLookup k (HashMapTable var) = HM.lookup k <$!> readTVarIO var
+    {-# INLINE tableLookup #-}
+
+instance Hashable k => Table (HashMapTable k v) k v where
+    tableInsert k v table@(HashMapTable var) =
+        atomically $ modifyTVar' var (HM.insert k v)
+    tableDelete k table@(HashMapTable var) =
+        atomically $ modifyTVar' var (HM.delete k)
+    {-# INLINE tableInsert #-}
+    {-# INLINE tableDelete #-}
+
+-- | Create new empty CAS
+--
+emptyTable :: Hashable k => IO (HashMapTable k v)
+emptyTable = HashMapTable <$> newTVarIO mempty
+
+-- | Return all entries of CAS as List
+--
+toList :: HashMapTable k v -> IO [v]
+toList (HashMapTable var) = HM.elems <$!> readTVarIO var
+
+-- | The number of items in the CAS
+--
+size :: HashMapTable k v -> IO Int
+size (HashMapTable var) = HM.size <$!> readTVarIO var
+
