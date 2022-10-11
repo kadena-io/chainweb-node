33d5884c7bf69ef0a5ad0112aad716279b562b30
Stuart Popejoy <sirlensalot@users.noreply.github.com>
2022-10-11 18:28:39 -0400

    refactor

--- src/Chainweb/Pact/Backend/Compaction.hs
+++ src/Chainweb/Pact/Backend/Compaction.hs
@@ -1,10 +1,15 @@
+{-# LANGUAGE DeriveFunctor #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell #-}
 
 -- |
 -- Module: Chainweb.Pact.Backend.Compaction
@@ -15,155 +20,285 @@
 --
 
 module Chainweb.Pact.Backend.Compaction
+  ( mkCompactEnv
+  , runCompactM
+  , CompactM
+  , compact
+  , withDefaultLogger
+  ) where
 
-  where
-
-import Control.Exception
+import Control.Lens
 import Control.Monad
+import Control.Monad.Catch
+import Control.Monad.Reader
+
+import Data.Int
+import Data.Text (Text,replace,isInfixOf)
+import Data.Text.Encoding
 
 import Database.SQLite3.Direct
 
-import Chainweb.BlockHeight
-import Chainweb.Pact.Backend.Utils
-import Chainweb.Pact.Service.Types
-import Chainweb.Pact.Types
-import Chainweb.Utils
+import Prelude hiding (log)
 
-import Pact.Types.Logger
+import Chainweb.BlockHeight
+
+import System.Logger
+
 import Pact.Types.SQLite
 
+data CompactException
+    = CompactExceptionInternal Text
+    | CompactExceptionInvalidBlockHeight
+    | CompactExceptionTableVerificationFailure Utf8
+  deriving Show
+instance Exception CompactException
 
-compact :: Logger -> BlockHeight -> Database -> IO ()
-compact log' blockheight' db = do
+internalError :: MonadThrow m => Text -> m a
+internalError = throwM . CompactExceptionInternal
 
-    txid <- initialize
+data CompactEnv = CompactEnv
+  { _ceDb :: Database
+  , _ceBlockHeight :: BlockHeight
+  , _ceTxId :: Maybe Int64
+  , _ceVersionTables :: [Utf8]
+  , _ceVersionTable :: Maybe Utf8
+  , _ceLogger :: Logger Text
+  }
+makeLenses ''CompactEnv
 
-    vtables <- collectVersionedTables
+withDefaultLogger :: LogLevel -> (Logger Text -> IO a) -> IO a
+withDefaultLogger ll f = withHandleBackend defaultHandleBackendConfig $ \b ->
+    withLogger defaultLoggerConfig b $ \l -> f (set setLoggerLevel ll l)
 
-    withTx $ do
-      mapM_ (computeTableHash txid) vtables
-      computeGlobalHash
+mkCompactEnv :: Logger Text -> Database -> BlockHeight -> CompactEnv
+mkCompactEnv l d b = CompactEnv d b Nothing [] Nothing l
 
-    withTx $ do
-      forM_ vtables $ \t -> do
-        compactTable t
-        verifyTable t
-      dropNewTables
-      compactSystemTables
+newtype CompactM a = CompactM {
+  unCompactM :: ReaderT CompactEnv IO a
+  }
+  deriving (Functor,Applicative,Monad,MonadReader CompactEnv,MonadIO,MonadThrow,MonadCatch)
+
+instance MonadLog Text CompactM where
+
+  localScope f = local (over (ceLogger.setLoggerScope) f)
+
+  logg ll m = do
+    l <- view ceLogger
+    liftIO $ loggerFunIO l ll m
+
+  withLevel l = local (set (ceLogger.setLoggerLevel) l)
+
+  withPolicy p = local (set (ceLogger.setLoggerPolicy) p)
+
+runCompactM :: CompactEnv -> CompactM a -> IO a
+runCompactM e a = runReaderT (unCompactM a) e
+
+
+execM_ :: Text -> CompactM ()
+execM_ q = do
+  q' <- templateStmt q
+  withDb $ \db -> liftIO $ exec_ db q'
+
+execM' :: Text -> [CompactM SType] -> CompactM ()
+execM' stmt ps' = do
+  ps <- sequence ps'
+  stmt' <- templateStmt stmt
+  withDb $ \db -> liftIO $ exec' db stmt' ps
+
+qryM :: Text -> [CompactM SType] -> [RType] -> CompactM [[SType]]
+qryM q ins' outs = do
+  q' <- templateStmt q
+  ins <- sequence ins'
+  withDb $ \db -> liftIO $ qry db q' ins outs
+
+templateStmt :: Text -> CompactM Utf8
+templateStmt s
+    | tblTemplate `isInfixOf` s =
+        vtable' >>= \(Utf8 v) ->
+          return $ Utf8 $ encodeUtf8 $
+            replace tblTemplate ("[" <> decodeUtf8 v <> "]") s
+    | otherwise = pure $ Utf8 $ encodeUtf8 s
+  where
+    tblTemplate = "$VTABLE$"
+
+blockheight :: CompactM SType
+blockheight = SInt . fromIntegral <$> view ceBlockHeight
+
+txid :: CompactM SType
+txid = view ceTxId >>= \case
+  Just t -> pure $ SInt t
+  Nothing -> internalError "txid not initialized!"
+
+vtable :: CompactM SType
+vtable = SText <$> vtable'
+
+vtable' :: CompactM Utf8
+vtable' = view ceVersionTable >>= \case
+  Just t -> pure t
+  Nothing -> internalError "version table not initialized!"
+
+withTx :: CompactM a -> CompactM a
+withTx a = withDb $ \db -> do
+  liftIO $ exec_ db $ "BEGIN TRANSACTION"
+  catch (a >>= \r -> liftIO (exec_ db "COMMIT TRANSACTION") >> return r) $
+      \e@SomeException {} -> do
+        liftIO $ exec_ db "ROLLBACK TRANSACTION"
+        throwM e
+
+withDb :: (Database -> CompactM a) -> CompactM a
+withDb a = view ceDb >>= a
+
+withTables :: CompactM () -> CompactM ()
+withTables a = view ceVersionTables >>= \ts ->
+  forM_ ts $ \t@(Utf8 t') ->
+    local (set ceVersionTable $ Just t) $
+      localScope (("table",decodeUtf8 t'):) $ a
+
+setTables :: [[SType]] -> CompactM () -> CompactM ()
+setTables rs next = do
+  ts <- forM rs $ \r -> case r of
+    [SText n] -> return n
+    _ -> internalError "setTables: expected text"
+  local (set ceVersionTables ts) next
+
+
+createTables :: CompactM ()
+createTables = do
+  logg Info "createTables"
+  execM_
+      " CREATE TABLE IF NOT EXISTS VersionedTableChecksum \
+      \ ( tablename TEXT \
+      \ , blockheight UNSIGNED BIGINT NOT NULL \
+      \ , hash BLOB \
+      \ , UNIQUE (tablename) ); "
+
+  execM_
+      "DELETE FROM VersionedTableChecksum"
+
+  execM_
+      " CREATE TABLE IF NOT EXISTS ActiveVersion \
+      \ ( tablename TEXT NOT NULL \
+      \ , rowkey TEXT NOT NULL \
+      \ , vrowid INTEGER NOT NULL \
+      \ , hash BLOB \
+      \ , UNIQUE (tablename,rowkey) ); "
+
+  execM_
+      "DELETE FROM ActiveVersion"
+
+readTxId :: CompactM a -> CompactM a
+readTxId next = do
+
+  r <- qryM
+       "SELECT endingtxid FROM BlockHistory WHERE blockheight=?"
+       [blockheight]
+       [RInt]
+
+  case r of
+    [] -> throwM CompactExceptionInvalidBlockHeight
+    [[SInt t]] ->
+        local (set ceTxId (Just t)) next
+    _ -> internalError "initialize: expected single-row int"
+
+
+
+compact :: CompactM ()
+compact = do
+
+    createTables
+
+    readTxId $ collectVersionedTables $ do
+
+        withTx $ do
+          withTables computeTableHash
+          computeGlobalHash
+
+        withTx $ do
+          withTables $ do
+            compactTable
+            verifyTable
+          dropNewTables
+          compactSystemTables
 
   where
-    blockheight = SInt $ fromIntegral blockheight'
-    debug = logDebug_ log'
-
-    withTx a = do
-      exec_ db $ "BEGIN TRANSACTION"
-      catch (a >>= \r -> exec_ db "COMMIT TRANSACTION" >> return r) $
-          \e@SomeException {} -> do
-            exec_ db "ROLLBACK TRANSACTION"
-            throw e
 
 
-    initialize = do
-      debug "initialize"
-      exec_ db $
-          "CREATE TABLE IF NOT EXISTS VersionedTableChecksum " <>
-          "( tablename TEXT " <> -- NULL is global checksum
-          ", blockheight UNSIGNED BIGINT NOT NULL " <>
-          ", hash BLOB " <>
-          ", UNIQUE (tablename) );"
-
-      exec_ db $ "DELETE FROM VersionedTableChecksum"
-
-      exec_ db $
-          "CREATE TABLE IF NOT EXISTS ActiveVersion " <>
-          "( tablename TEXT NOT NULL " <>
-          ", rowkey TEXT NOT NULL " <>
-          ", vrowid INTEGER NOT NULL " <>
-          ", hash BLOB " <>
-          ", UNIQUE (tablename,rowkey) );"
-
-      exec_ db $ "DELETE FROM ActiveVersion"
-
-      qry db "SELECT endingtxid FROM BlockHistory WHERE blockheight=?"
-         [blockheight] [RInt] >>= \r ->
-        case r of
-          [] -> internalError "initialize: invalid target block height"
-          [[SInt txid]] -> return txid
-          _ -> internalError "initialize: expected single-row int"
 
 
-    collectVersionedTables = do
-      debug "collectVersionedTables"
-      rs <- qry db
-          ("SELECT DISTINCT tablename FROM VersionedTableMutation " <>
-           "WHERE blockheight <= ? ORDER BY tablename")
+
+    collectVersionedTables next = do
+      logg Info "collectVersionedTables"
+      rs <- qryM
+          " SELECT DISTINCT tablename FROM VersionedTableMutation \
+          \ WHERE blockheight <= ? ORDER BY tablename"
           [blockheight]
           [RText]
-      forM rs $ \r -> case r of
-        [SText n] -> return n
-        _ -> internalError "collectVersionedTables: expected text"
+      setTables rs next
 
-    computeTableHash txid vtable = do
-      debug $ "computeTableHash:insert " ++ show vtable
-      let stmt1 =
-            "INSERT INTO ActiveVersion " <>
-            "SELECT ?1,rowkey,rowid,hash FROM " <> tbl vtable <>
-            " t1 WHERE txid=(select max(txid) from " <> tbl vtable <>
-            "  t2 where t2.rowkey=t1.rowkey and t2.txid<?2)" <>
-            " GROUP BY rowkey"
-      exec' db stmt1 [SText vtable,SInt txid]
-      debug $ "computeTableHash:checksum " ++ show vtable
+    computeTableHash = do
+      logg Info "computeTableHash:insert"
+      execM'
+          " INSERT INTO ActiveVersion \
+          \ SELECT ?1,rowkey,rowid,hash FROM $VTABLE$ t1 \
+          \ WHERE txid=(SELECT MAX(txid) FROM $VTABLE$ t2 \
+          \  WHERE t2.rowkey=t1.rowkey AND t2.txid<?2) \
+          \ GROUP BY rowkey "
+          [vtable,txid]
 
-      let stmt2 =
-            "INSERT INTO VersionedTableChecksum VALUES (?1, ?2, " <>
-            " (SELECT sha3a_256(hash) FROM ActiveVersion " <>
-            "  WHERE tablename=?1 ORDER BY rowkey))"
-      exec' db stmt2
-          [SText vtable,blockheight]
+      logg Info "computeTableHash:checksum"
+      execM'
+          " INSERT INTO VersionedTableChecksum \
+          \ VALUES (?1, ?2, \
+          \  (SELECT sha3a_256(hash) FROM ActiveVersion \
+          \   WHERE tablename=?1 ORDER BY rowkey)) "
+          [vtable,blockheight]
 
     computeGlobalHash = do
-      debug "computeGlobalHash"
-      exec' db
-          ("INSERT INTO VersionedTableChecksum VALUES (NULL,?1," <>
-           "(SELECT sha3a_256(hash) FROM VersionedTableChecksum " <>
-           " WHERE tablename IS NOT NULL ORDER BY tablename))")
+      logg Info "computeGlobalHash"
+      execM'
+          " INSERT INTO VersionedTableChecksum \
+          \ VALUES (NULL, ?1, \
+          \  (SELECT sha3a_256(hash) FROM VersionedTableChecksum \
+          \   WHERE tablename IS NOT NULL ORDER BY tablename)) "
           [blockheight]
 
-    compactTable vtable = do
-      debug $ "compactTable: " ++ show vtable
-      let stmt =
-            "DELETE FROM " <> tbl vtable <>
-            " WHERE rowid NOT IN (SELECT t.rowid FROM " <> tbl vtable <>
-            " t LEFT JOIN ActiveVersion v WHERE t.rowid = v.vrowid AND v.tablename=?1)"
-      exec' db stmt [SText vtable]
+    compactTable = do
+      logg Info "compactTable"
+      execM'
+          " DELETE FROM $VTABLE$ WHERE rowid NOT IN \
+          \ (SELECT t.rowid FROM $VTABLE$ t \
+          \  LEFT JOIN ActiveVersion v \
+          \  WHERE t.rowid = v.vrowid AND v.tablename=?1) "
+          [vtable]
 
-    verifyTable vtable = do
-      debug $ "verifyTable: " ++ show vtable
-      rs <- qry db
-          ("SELECT hash FROM VersionedTableChecksum WHERE tablename=?1 " <>
-           "UNION ALL " <>
-           "SELECT sha3a_256(hash) FROM (SELECT hash from " <> tbl vtable <>
-           " t1 WHERE txid=(select max(txid) from " <> tbl vtable <>
-           " t2 where t2.rowkey=t1.rowkey) GROUP BY rowkey)" )
-          [SText vtable]
+    verifyTable = do
+      logg Info "verifyTable"
+      rs <- qryM
+            " SELECT hash FROM VersionedTableChecksum WHERE tablename=?1 \
+            \ UNION ALL \
+            \ SELECT sha3a_256(hash) FROM (SELECT hash FROM $VTABLE$ t1 \
+            \  WHERE txid=(select max(txid) FROM $VTABLE$ t2 \
+            \   WHERE t2.rowkey=t1.rowkey) GROUP BY rowkey) "
+          [vtable]
           [RBlob]
       case rs of
         [[SBlob prev],[SBlob curr]] | prev == curr -> return ()
-        _ -> internalError $ "Table verification failed! " <> sshow (vtable,rs)
+        _ -> vtable' >>= throwM . CompactExceptionTableVerificationFailure
 
     dropNewTables = do
-      debug "dropNewTables"
-      nts <- qry db "SELECT tablename FROM VersionedTableCreation WHERE createBlockheight > ?1"
-          [blockheight] [RText]
-      forM_ nts $ \nt ->
-        case nt of
-          [SText t] ->
-            exec_ db $ "DROP TABLE " <> tbl t
-          _ -> internalError "dropNewTables: Expected text tablename"
+      logg Info "dropNewTables"
+      nts <- qryM
+          "SELECT tablename FROM VersionedTableCreation WHERE createBlockheight > ?1"
+          [blockheight]
+          [RText]
+
+      setTables nts $ withTables $ do
+        execM_ "DROP TABLE $VTABLE$"
+
     compactSystemTables = do
-      exec' db
-          ("DELETE FROM BlockHistory WHERE blockheight != ?1;" <>
-           "DELETE FROM VersionedTableMutation WHERE blockheight != ?1;" <>
-           "DELETE FROM TransactionIndex WHERE blockheight != ?1;" <>
-           "DELETE FROM VersionedTableCreation WHERE createBlockheight != ?1;")
+      execM'
+          " DELETE FROM BlockHistory WHERE blockheight != ?1; \
+          \ DELETE FROM VersionedTableMutation WHERE blockheight != ?1; \
+          \ DELETE FROM TransactionIndex WHERE blockheight != ?1; \
+          \ DELETE FROM VersionedTableCreation WHERE createBlockheight != ?1; "
           [blockheight]
