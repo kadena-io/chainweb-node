a24478b4ffd394286b8476dd6e1b62a2328eb3c6
Lars Kuhtz <lars@kadena.io>
2022-12-09 21:26:33 -0800

    fix tests

--- test/Chainweb/Test/Pact/DbCacheTest.hs
+++ test/Chainweb/Test/Pact/DbCacheTest.hs
@@ -1,13 +1,18 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE FlexibleContexts #-}
+
 module Chainweb.Test.Pact.DbCacheTest (tests) where
 
 import Chainweb.Pact.Backend.DbCache
 
 import Control.Monad.State.Strict
+
 import Data.Aeson
 import Data.ByteString.Lazy (toStrict)
+
 import Database.SQLite3.Direct
+
+import GHC.Compact
 
 import Test.Tasty
 import Test.Tasty.HUnit
@@ -16,57 +21,73 @@ tests :: TestTree
 tests = testGroup "Chainweb.Test.Pact.DbCacheTest"
     [ testCache ]
 
-testCache :: TestTree
-testCache = testCase "testCache" $
-  void $ (`runStateT` (emptyDbCache 100 :: DbCache [String])) $ do
+defaultChunkSize :: Int
+defaultChunkSize = 2^(15::Int)
 
-    let aValue = ["1234567890"]
+-- | Create entries with a size the corresponds to the default
+-- chunk size of the cache.
+--
+entry :: MonadIO m => Int -> Char -> m ([String], Int)
+entry i c = do
+    let e = [replicate (i * defaultChunkSize `div` 256) c]
+    s <- liftIO $ compactSize =<< compact e
+    return (e, fromIntegral s)
+
+combineEntries :: MonadIO m => Monoid a => a -> a -> m (a, Int)
+combineEntries a b = do
+    let e = a <> b
+    s <- liftIO $ compactSize =<< compact e
+    return (e, fromIntegral s)
+
+testCache :: TestTree
+testCache = testCase "testCache" $ do
+
+  -- Create Items
+  (a, sa) <- entry 1 'a'; -- large
+  (b0, sb0) <- combineEntries a a -- larger
+  (b1, sb1) <- entry 0 'b' -- small
+  (c, sc) <- combineEntries b0 b0 -- even larger
+  (d, sd) <- entry 0 'd' -- small
+
+  -- cache size (enough to hold a + b0 + b1 + c)
+  let cs = sa + sb0 + sb1 + sc + 1
+
+  void $ (`runStateT` (emptyDbCache cs :: DbCache [String])) $ do
 
     -- a: simple insert
-    doCheck "a insert @ 1" "a" aValue 1
+    doCheck "a insert @1 -> [a@1]" "a" a 1
+    assertEqual' "size a" sa cacheSize
+    assertEqual' "count 1" 1 cacheCount
 
-    assertEqual' "size 14" 14 cacheSize
-    assertEqual' "count" 1 cacheCount
+    -- b0: simple insert
+    doCheck "b->v0 insert @2 -> [a@1,b0@2]" "b" b0 2
+    assertEqual' "size a + b0" (sa + sb0) cacheSize
+    assertEqual' "count 2" 2 cacheCount
 
-    let bValue0 = aValue ++ aValue
-        bValue1 = []
-
-    -- b: insert with different values, same key+txid
-    doCheck "b->v0 insert @ 2" "b" bValue0 2
-
-    assertEqual' "size + 27" 41 cacheSize
-    assertEqual' "count" 2 cacheCount
-
-    doCheck "b->v1 insert @ 2" "b" bValue1 2
-
-    assertEqual' "size + 2" 43 cacheSize
-    assertEqual' "count" 3 cacheCount
+    -- b1: insert with different values, same key+txid
+    doCheck "b->v1 insert @2 -> [a@1,b0@2,b1@2]" "b" b1 2
+    assertEqual' "size a + b0 + b1" (sa + sb0 + sb1) cacheSize
+    assertEqual' "count 3" 3 cacheCount
 
     -- c: big insert
-    let cValue = bValue0 ++ bValue0
-
-    doCheck "c insert @ 3" "c" cValue 3
-
-    assertEqual' "size + 53" 96 cacheSize
-    assertEqual' "count" 4 cacheCount
+    doCheck "c insert @3 -> [a@1,b0@2,b1@2,c@3]" "c" c 3
+    assertEqual' "size a + b0 + b1 + c" (sa + sb0 + sb1 + sc) cacheSize
+    assertEqual' "count 4" 4 cacheCount
 
     -- d: small insert to trip limit, evict
-    doCheck "d insert @ 4, evict a@1" "d" ["1234"] 4
-
-    assertEqual' "size + 8 - 14" 90 cacheSize
-    assertEqual' "count + 1 - 1" 4 cacheCount
+    doCheck "d insert @4, evict a@1 -> [b0@2,b1@2,c@3,d@4]" "d" d 4
+    assertEqual' "size b0 + b1 + c + d" (sb0 + sb1 + sc + sd) cacheSize
+    assertEqual' "count 4" 4 cacheCount
 
     -- hit b->v0 to avoid eviction, cache stats unchanged
-    doCheck "b->v0 hit @ 5" "b" bValue0 5
-
-    assertEqual' "size unchanged" 90 cacheSize
+    doCheck "b->v0 hit @5 -> [b1@2,c@3,d@4,b0@5]" "b" b0 5
+    assertEqual' "size unchanged" (sb1 + sc + sd + sb0) cacheSize
     assertEqual' "count unchanged" 4 cacheCount
 
     -- reinsert a to evict b->v1, c
-    doCheck "a reinsert, evict b->v1@2 + c@3" "a" aValue 6
-
-    assertEqual' "size + 14 - 2 - 53" 49 cacheSize
-    assertEqual' "count + 1 - 2" 3 cacheCount
+    doCheck "a reinsert, evict b->v1@2 -> [c@3,d@4,b0@5,a@6]" "a" a 6
+    assertEqual' "size c + d + b0 + a) - a - b" (sc + sd + sb0 + sa) cacheSize
+    assertEqual' "count 4" 4 cacheCount
 
   where
 
