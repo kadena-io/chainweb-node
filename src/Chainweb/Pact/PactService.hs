6f6d547758d03a66dbe317d4d204e96d1b5858e3
Emily Pillmore <emily@kadena.io>
2023-01-11 15:33:54 -0700

    fix indentation

--- src/Chainweb/Pact/PactService.hs
+++ src/Chainweb/Pact/PactService.hs
@@ -656,26 +656,26 @@ execLocal cwtx preflight rdepth = withDiscardedBatch $ do
 
     withCheckpointerRewind rewindHeight rewindHeader "execLocal" $
       \(PactDbEnv' pdbenv) -> do
-          --
-          -- if the ?preflight query parameter is set to True, we run the `applyCmd` workflow
-          -- otherwise, we prefer the old (default) behavior. When no preflight flag is
-          -- specified, we run the old behavior. When it is set to true, we also do metadata
-          -- validations.
-          --
-          r <- if preflight
-            then do
-              void $ assertLocalMetadata cmd ctx
-              T2 cr _mc' <- liftIO $ applyCmd
-                _psVersion logger _psGasLogger pdbenv
-                noMiner chainweb213GasModel ctx spv cmd
-                initialGas mc ApplyLocal
-              return cr
-            else liftIO $ applyLocal
-              logger _psGasLogger pdbenv
-              chainweb213GasModel ctx spv
-              cwtx mc execConfig
+        --
+        -- if the ?preflight query parameter is set to True, we run the `applyCmd` workflow
+        -- otherwise, we prefer the old (default) behavior. When no preflight flag is
+        -- specified, we run the old behavior. When it is set to true, we also do metadata
+        -- validations.
+        --
+        r <- if preflight
+          then do
+            void $ assertLocalMetadata cmd ctx
+            T2 cr _mc' <- liftIO $ applyCmd
+              _psVersion logger _psGasLogger pdbenv
+              noMiner chainweb213GasModel ctx spv cmd
+              initialGas mc ApplyLocal
+            return cr
+          else liftIO $ applyLocal
+            logger _psGasLogger pdbenv
+            chainweb213GasModel ctx spv
+            cwtx mc execConfig
 
-          return $! Discard (toHashCommandResult r)
+        return $! Discard (toHashCommandResult r)
 
 execSyncToBlock
     :: CanReadablePayloadCas tbl
