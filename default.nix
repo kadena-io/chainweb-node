ff66c2b197a9b37f4d2351ae751ce467cecab3d3
John Wiegley <johnw@newartisans.com>
2023-01-11 16:03:02 -0800

    Update Nix reference to rocksdb-haskell

--- default.nix
+++ default.nix
@@ -43,9 +43,9 @@ pkgs.haskell.packages.${compiler}.developPackage {
         (self.callCabal2nix "rocksdb-haskell-kadena" (pkgs.fetchFromGitHub {
           owner = "kadena-io";
           repo = "rocksdb-haskell";
-          rev = "2161777750bf879856251289e551e8dc2cd512e2";
-          sha256 = "09cmjrhkjv6mccaaasb2lz1sjndha1df3wyygi7166axj0srw8ds";
-          # date = "2022-08-10T09:03:56-04:00";
+          rev = "b4bfc0e4cb0fd401f68190d1ba90b56b80d3effc";
+          sha256 = "0v1fmiwcpamzflashdwbsd5s3psczlm2yx588938sd5dcx9bil3x";
+          # date = "2023-01-09T11:38:12-05:00";
         }) {})
         (attrs: {
           preConfigure = (attrs.preConfigure or "") +
