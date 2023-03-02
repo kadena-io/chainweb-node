4e990183d39488616373a8ef06dfd41a213670b8
Lars Kuhtz <lars@kadena.io>
2023-03-01 18:52:59 -0800

    mempool throttle

--- src/Chainweb/Chainweb/Configuration.hs
+++ src/Chainweb/Chainweb/Configuration.hs
@@ -23,6 +23,7 @@ module Chainweb.Chainweb.Configuration
   ThrottlingConfig(..)
 , throttlingRate
 , throttlingPeerRate
+, throttlingMempoolRate
 , defaultThrottlingConfig
 
 -- * Cut Configuration
@@ -121,6 +122,7 @@ data ThrottlingConfig = ThrottlingConfig
         -- check of the client. And we want to keep bad actors out of the
         -- system. There should be no need for a client to call this endpoint on
         -- the same node more often than at most few times peer minute.
+    , _throttlingMempoolRate :: !Double
     }
     deriving stock (Eq, Show)
 
@@ -130,18 +132,21 @@ defaultThrottlingConfig :: ThrottlingConfig
 defaultThrottlingConfig = ThrottlingConfig
     { _throttlingRate = 50 -- per second, in a 100 burst
     , _throttlingPeerRate = 11 -- per second, 1 for each p2p network in a burst
+    , _throttlingMempoolRate = 5 -- one every four seconds per mempool.
     }
 
 instance ToJSON ThrottlingConfig where
     toJSON o = object
         [ "global" .= _throttlingRate o
         , "putPeer" .= _throttlingPeerRate o
+        , "mempool" .= _throttlingMempoolRate o
         ]
 
 instance FromJSON (ThrottlingConfig -> ThrottlingConfig) where
     parseJSON = withObject "ThrottlingConfig" $ \o -> id
         <$< throttlingRate ..: "global" % o
         <*< throttlingPeerRate ..: "putPeer" % o
+        <*< throttlingMempoolRate ..: "mempool" % o
 
 -- -------------------------------------------------------------------------- --
 -- Cut Coniguration
