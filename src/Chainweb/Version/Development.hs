63c09220234560b5b9da34ceddf25c66b35457ea
Edmund Noble <edmundnoble@gmail.com>
2023-04-12 11:34:57 -0400

    Move upgrades to after 20 chain move

--- src/Chainweb/Version/Development.hs
+++ src/Chainweb/Version/Development.hs
@@ -53,17 +53,17 @@ devnet = ChainwebVersion
             OldDAGuard -> AllChains $ ForkAtBlockHeight 1
             PactEvents -> AllChains $ ForkAtBlockHeight 1
             SPVBridge -> AllChains $ ForkAtBlockHeight 1
-            Pact4Coin3 -> AllChains $ ForkAtBlockHeight 2
+            Pact4Coin3 -> AllChains $ ForkAtBlockHeight 14
             EnforceKeysetFormats -> AllChains $ ForkAtBlockHeight 1
             Pact420 -> AllChains $ ForkAtBlockHeight 1
             CheckTxHash -> AllChains $ ForkAtBlockHeight 1
-            Chainweb213Pact -> AllChains $ ForkAtBlockHeight 3
-            Chainweb214Pact -> AllChains $ ForkAtBlockHeight 4
-            Chainweb215Pact -> AllChains $ ForkAtBlockHeight 5
+            Chainweb213Pact -> AllChains $ ForkAtBlockHeight 15
+            Chainweb214Pact -> AllChains $ ForkAtBlockHeight 15
+            Chainweb215Pact -> AllChains $ ForkAtBlockHeight 16
             Pact44NewTrans -> AllChains $ ForkAtBlockHeight 1
-            Chainweb216Pact -> AllChains $ ForkAtBlockHeight 6
-            Chainweb217Pact -> AllChains $ ForkAtBlockHeight 6
-            Chainweb218Pact -> AllChains $ ForkAtBlockHeight 6
+            Chainweb216Pact -> AllChains $ ForkAtBlockHeight 17
+            Chainweb217Pact -> AllChains $ ForkAtBlockHeight 17
+            Chainweb218Pact -> AllChains $ ForkAtBlockHeight 17
 
     , _versionUpgrades = foldr (chainZip HM.union) (AllChains mempty)
         [ forkUpgrades devnet
