1dd99bd6b6ff05e95f3013187e10b2aedaca9dca
Enis Bayramoğlu <enisbayramoglu@gmail.com>
2023-04-25 10:02:29 +0200

    Pin explicit commit of nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Chainweb";
 
   inputs = {
-    nixpkgs.follows = "haskellNix/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs?rev=4d2b37a84fad1091b9de401eb450aae66f1a741e";
     haskellNix.url = "github:input-output-hk/haskell.nix";
     flake-utils.url = "github:numtide/flake-utils";
   };
