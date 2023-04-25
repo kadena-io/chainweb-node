921085d7a42462d79fa61f6c44b33bd7b6170fc6
Enis Bayramoğlu <enisbayramoglu@gmail.com>
2023-04-25 12:15:16 +0200

    Make sure default.nix sees same src as flake.nix

--- default.nix
+++ default.nix
@@ -1,10 +1,11 @@
-let inputs = (import (
+let flakeDefaultNix = (import (
       fetchTarball {
          url = "https://github.com/edolstra/flake-compat/archive/35bb57c0c8d8b62bbfd284272c928ceb64ddbde9.tar.gz";
          sha256 = "1prd9b1xx8c0sfwnyzkspplh30m613j42l1k789s521f4kv4c2z2"; }
      ) {
        src =  ./.;
-     }).defaultNix.inputs;
+     }).defaultNix;
+    inputs = flakeDefaultNix.inputs;
     pkgsDef = import inputs.nixpkgs (import inputs.haskellNix {}).nixpkgsArgs;
 in
 { pkgs ? pkgsDef
@@ -12,7 +13,7 @@ in
 , ...
 }:
 let chainweb-node = pkgs.haskell-nix.project' {
-      src = ./.;
+      src = flakeDefaultNix.outPath;
       compiler-nix-name = compiler;
       projectFileName = "cabal.project";
       shell.tools = {
