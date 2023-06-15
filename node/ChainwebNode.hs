1f15789a1699f558cb38922b39377a99e67674b2
Edmund Noble <edmundnoble@gmail.com>
2023-06-15 19:45:06 -0400

    Add log message re: read-only rocksdb in onlySyncPact mode

--- node/ChainwebNode.hs
+++ node/ChainwebNode.hs
@@ -338,22 +338,29 @@ node conf logger = do
     rocksDbDir <- getRocksDbDir conf
     pactDbDir <- getPactDbDir conf
     dbBackupsDir <- getBackupsDir conf
-    (if _configOnlySyncPact cwConf then withReadOnlyRocksDb else withRocksDb)
-        rocksDbDir modernDefaultOptions $ \rocksDb -> do
-            logFunctionText logger Info $ "opened rocksdb in directory " <> sshow rocksDbDir
-            logFunctionText logger Info $ "backup config: " <> sshow (_configBackup cwConf)
-            withChainweb cwConf logger rocksDb pactDbDir dbBackupsDir (_nodeConfigResetChainDbs conf) $ \case
-                Replayed _ _ -> return ()
-                StartedChainweb cw ->
-                    concurrentlies_
-                        [ runChainweb cw
-                        -- we should probably push 'onReady' deeper here but this should be ok
-                        , runCutMonitor (_chainwebLogger cw) (_cutResCutDb $ _chainwebCutResources cw)
-                        , runQueueMonitor (_chainwebLogger cw) (_cutResCutDb $ _chainwebCutResources cw)
-                        , runRtsMonitor (_chainwebLogger cw)
-                        , runBlockUpdateMonitor (_chainwebLogger cw) (_cutResCutDb $ _chainwebCutResources cw)
-                        , runDatabaseMonitor (_chainwebLogger cw) rocksDbDir pactDbDir
-                        ]
+    withRocksDb' <-
+        if _configOnlySyncPact cwConf
+        then
+            if _cutPruneChainDatabase (_configCuts cwConf) == GcNone
+            then withReadOnlyRocksDb <$ logFunctionText logger Info "Opening RocksDB in read-only mode"
+            else withRocksDb <$ logFunctionText logger Info "Opening RocksDB in read-write mode, if this wasn't intended, ensure that cuts.pruneChainDatabase is set to none"
+        else
+            return withRocksDb
+    withRocksDb' rocksDbDir modernDefaultOptions $ \rocksDb -> do
+        logFunctionText logger Info $ "opened rocksdb in directory " <> sshow rocksDbDir
+        logFunctionText logger Info $ "backup config: " <> sshow (_configBackup cwConf)
+        withChainweb cwConf logger rocksDb pactDbDir dbBackupsDir (_nodeConfigResetChainDbs conf) $ \case
+            Replayed _ _ -> return ()
+            StartedChainweb cw ->
+                concurrentlies_
+                    [ runChainweb cw
+                    -- we should probably push 'onReady' deeper here but this should be ok
+                    , runCutMonitor (_chainwebLogger cw) (_cutResCutDb $ _chainwebCutResources cw)
+                    , runQueueMonitor (_chainwebLogger cw) (_cutResCutDb $ _chainwebCutResources cw)
+                    , runRtsMonitor (_chainwebLogger cw)
+                    , runBlockUpdateMonitor (_chainwebLogger cw) (_cutResCutDb $ _chainwebCutResources cw)
+                    , runDatabaseMonitor (_chainwebLogger cw) rocksDbDir pactDbDir
+                    ]
   where
     cwConf = _nodeConfigChainweb conf
 
