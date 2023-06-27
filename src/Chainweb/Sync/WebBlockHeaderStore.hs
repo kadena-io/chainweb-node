d99a2eba3f515e1ba85f80bf5bf8f8da2f68bb56
Edmund Noble <edmundnoble@gmail.com>
2023-06-27 15:10:34 -0400

    fix build

--- src/Chainweb/Sync/WebBlockHeaderStore.hs
+++ src/Chainweb/Sync/WebBlockHeaderStore.hs
@@ -108,7 +108,7 @@ pullOriginResponseTimeout = HTTP.responseTimeoutMicro 1_000_000
 --
 setResponseTimeout :: HTTP.ResponseTimeout -> ClientEnv -> ClientEnv
 setResponseTimeout t env =  env
-    { makeClientRequest = \u r -> (defaultMakeClientRequest u r)
+    { makeClientRequest = \u r -> return (defaultMakeClientRequest u r)
         { HTTP.responseTimeout = t
         }
     }
