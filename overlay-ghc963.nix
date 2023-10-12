deb98b4ba6a2f15e8c1a2932c74e375b56000a73
chessai <chessai1996@gmail.com>
2023-10-12 12:30:51 -0500

    add ghc981 override

--- overlay-ghc963.nix
+++ overlay-ghc963.nix
@@ -3,11 +3,20 @@ self: super:
 let
   lib = self.lib;
   hlib = self.haskell.lib.compose;
+
+  overrideGHC = version: super.haskell-nix.compiler."${version}".override;
+  debugFlavour = "release+llvm+split_sections+debug_info";
 in
 {
   haskell-nix = lib.recursiveUpdate super.haskell-nix {
-    compiler.ghc963 = super.haskell-nix.compiler.ghc963.override {
-      ghcFlavour = "release+llvm+split_sections+debug_info";
+    compiler = {
+      ghc963 = overrideGHC "ghc963" {
+        ghcFlavour = debugFlavour;
+      };
+
+      ghc981 = overrideGHC "ghc981" {
+        ghcFlavour = debugFlavour;
+      };
     };
   };
 }
