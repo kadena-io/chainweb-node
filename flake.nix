456a3808ad53f35dad2d956c08e8e62e242aba89
Enis Bayramoğlu <enisbayramoglu@gmail.com>
2023-11-03 15:25:11 +0100

    Switch to hs-nix-infra for pinning Haskell toolchain

--- flake.nix
+++ flake.nix
@@ -2,13 +2,12 @@
   description = "Chainweb";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs?rev=4d2b37a84fad1091b9de401eb450aae66f1a741e";
     hackage = {
       url = "github:input-output-hk/hackage.nix";
       flake = false;
     };
-    haskellNix = {
-      url = "github:input-output-hk/haskell.nix";
+    hs-nix-infra = {
+      url = "github:kadena-io/hs-nix-infra";
       inputs.hackage.follows = "hackage";
     };
     flake-utils.url = "github:numtide/flake-utils";
@@ -20,11 +19,12 @@
     trusted-public-keys = "nixcache.chainweb.com:FVN503ABX9F8x8K0ptnc99XEz5SaA4Sks6kNcZn2pBY= iohk.cachix.org-1:DpRUyj7h7V830dp/i6Nti+NEO2/nhblbov/8MW7Rqoo= cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=";
   };
 
-  outputs = { self, nixpkgs, flake-utils, haskellNix, nix-filter, ... }:
+  outputs = { self, hs-nix-infra, flake-utils, nix-filter, ... }:
     flake-utils.lib.eachSystem
       [ "x86_64-linux" "x86_64-darwin"
         "aarch64-linux" "aarch64-darwin" ] (system:
     let
+      inherit (hs-nix-infra) nixpkgs haskellNix;
       pkgs = import nixpkgs {
         inherit system;
         inherit (haskellNix) config;
