835aa7ba006b1f19c02f914b71f9d83a70ca23c7
Evgenii Akentev <i@ak3n.com>
2023-11-15 15:23:37 +0400

    Throw error instead of verified field

--- test/Chainweb/Test/Pact/SPV/Hyperlane.hs
+++ test/Chainweb/Test/Pact/SPV/Hyperlane.hs
@@ -47,13 +47,14 @@ tests = testGroup "hyperlane"
 
   , testCase "encodeHyperlaneMessage" hyperlaneEncodeHyperlaneMessage
   , testCase "recoverValidatorAnnouncement" hyperlaneRecoverValidatorAnnouncement
-  , testCase "verify" hyperlaneVerify
+  , testCase "verifySuccess" hyperlaneVerifySuccess
+  , testCase "verifyFailure" hyperlaneVerifyFailure
   ]
 
 hyperlaneEmptyObject :: Assertion
 hyperlaneEmptyObject = do
   res <- runExceptT $ evalHyperlaneCommand $ mkObject []
-  assertEqual "should fail with missing command name" (Left "Unknown hyperlane command") res
+  assertEqual "Should fail with missing command name" (Left "Unknown hyperlane command") res
 
 hyperlaneEncodeTokenMessageERC20 :: Assertion
 hyperlaneEncodeTokenMessageERC20 = do
@@ -66,7 +67,7 @@ hyperlaneEncodeTokenMessageERC20 = do
     Just t ->
       let
         expectedMessage :: Text = "0x0000000000000000000000000000000000000000000000000000000000000040000000000000000000000000000000000281fa059c9e179daafc1235555555550000000000000000000000000000000000000000000000000000000000000009726563697069656e740000000000000000000000000000000000000000000000"
-      in assertEqual "should get encoded message" expectedMessage t
+      in assertEqual "Should get encoded message" expectedMessage t
 
 hyperlaneDecodeTokenMessageERC20 :: Assertion
 hyperlaneDecodeTokenMessageERC20 = do
@@ -113,7 +114,7 @@ hyperlaneEncodeHyperlaneMessage = do
           [ ("encodedMessage", tStr $ asString expectedMessage)
           , ("messageId", tStr $ asString ("0x42f79cdedbfc03af3296d1b337255cc53f870c8b2bb02107bded2a082bd02323" :: Text))
           ]
-      in assertEqual "should get encoded message" expectedObject o
+      in assertEqual "Should get encoded message" expectedObject o
 
 hyperlaneRecoverValidatorAnnouncement :: Assertion
 hyperlaneRecoverValidatorAnnouncement = do
@@ -130,10 +131,10 @@ hyperlaneRecoverValidatorAnnouncement = do
         expectedObject = mkObject
           [ ("address", tStr $ asString ("0x6c414e7a15088023e28af44ad0e1d593671e4b15" :: Text))
           ]
-      in assertEqual "should get encoded message" expectedObject o
+      in assertEqual "Should get encoded message" expectedObject o
 
-hyperlaneVerify :: Assertion
-hyperlaneVerify = do
+hyperlaneVerifySuccess :: Assertion
+hyperlaneVerifySuccess = do
   let
     obj' = mkObject
         [ ("message", tStr $ asString ("0x01000001450000027200000000000000000000000000000000006b622d746f6b656e2d726f757465720000000100000000000000000000000071c7656ec7ab88b098defb751b7401b5f6d8976f00000000000000000000000000000000000000000000000000000000000000400000000000000000000000000000000000000000000000008ac7230489e80000000000000000000000000000000000000000000000000000000000000000002a30783731433736353645433761623838623039386465664237353142373430314235663664383937364600000000000000000000000000000000000000000000" :: Text))
@@ -160,6 +161,20 @@ hyperlaneVerify = do
               )
             ])
           , ("messageId", tStr $ asString ("0x3b2cc397eb65735078b967c5497a028ee35a0ebe89478a2417b2e9c7e891e0f3" :: Text))
-          , ("verified", tLit $ LBool True)
           ]
-      in assertEqual "should get encoded message" expectedObject o
+      in assertEqual "Should get encoded message" expectedObject o
+
+hyperlaneVerifyFailure :: Assertion
+hyperlaneVerifyFailure = do
+  let
+    obj' = mkObject
+        [ ("message", tStr $ asString ("0x01000001450000027200000000000000000000000000000000006b622d746f6b656e2d726f757465720000000100000000000000000000000071c7656ec7ab88b098defb751b7401b5f6d8976f00000000000000000000000000000000000000000000000000000000000000400000000000000000000000000000000000000000000000008ac7230489e80000000000000000000000000000000000000000000000000000000000000000002a30783731433736353645433761623838623039386465664237353142373430314235663664383937364600000000000000000000000000000000000000000000" :: Text))
+        , ("validators", toTList tTyString def $ map (tStr . asString) [("0x5DD34992E0994E9d3c53c1CCfe5C2e38d907338e" :: Text)])
+        , ("metadata", tStr $ asString ("0x0000000000000000000000002e234dae75c793f67a35089c9d99245e1c58470b00000000000000000000000000000000000000000000000000000000000000ad0000000f0e1c8be19e9e2bd14665599b8e8ed1f3dbca562788e5844975770eb31380b3ae5de03487e89a1d3c42fad8aac486a06e1af6b3478ec0d148c0c8566c404537291b" :: Text))
+        , ("threshold", tLit $ LInteger 1)
+        ]
+  res <- runExceptT $ evalHyperlaneCommand obj'
+
+  case res of
+    Left err -> assertEqual "Verification should fail" "Verification failed" err
+    Right _ -> assertFailure "Should fail"
