d8236df90d3d71fe0d9f99266a461ac00b4e6572
Edmund Noble <edmundnoble@gmail.com>
2024-03-27 18:24:08 -0400

    feature: miner continues blocks periodically
    Makes the mining loop use ContinueBlock to periodically "refresh" blocks,
    ensuring that they use all of the transactions in the mempool if possible.
    
    Test plan will be to put this on a testnet mining node.
    
    Change-Id: I25e1ab240e4522d7e56812ff170d0aa849125abe

--- src/Chainweb/WebPactExecutionService.hs
+++ src/Chainweb/WebPactExecutionService.hs
@@ -79,7 +79,7 @@ data PactExecutionService = PactExecutionService
     , _pactContinueBlock :: !(
         ChainId ->
         BlockInProgress ->
-        IO (Maybe NewBlock)
+        IO (Maybe BlockInProgress)
         )
       -- ^ Request a new block to be formed using mempool
     , _pactLocal :: !(
@@ -149,7 +149,7 @@ _webPactContinueBlock
     :: WebPactExecutionService
     -> ChainId
     -> BlockInProgress
-    -> IO (Maybe NewBlock)
+    -> IO (Maybe BlockInProgress)
 _webPactContinueBlock = _pactContinueBlock . _webPactExecutionService
 {-# INLINE _webPactContinueBlock #-}
 
@@ -199,7 +199,7 @@ mkPactExecutionService q = PactExecutionService
     , _pactNewBlock = \_ m fill -> do
         NewBlockInProgress <$> newBlock m fill q
     , _pactContinueBlock = \_ bip -> do
-        fmap NewBlockInProgress <$> continueBlock bip q
+        continueBlock bip q
     , _pactLocal = \pf sv rd ct ->
         local pf sv rd ct q
     , _pactLookup = \_ cd txs ->
