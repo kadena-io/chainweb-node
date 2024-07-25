d333d5fdfe5504192e0e58a58fd7b4ca2a819e00
chessai <chessai1996@gmail.com>
2024-07-25 14:44:25 -0500

    add a function to convert between pact4 tx and pact5 tx
    Change-Id: I8b93fc5705a2e71917e5aab438c9771f94c84af8

--- test/Chainweb/Test/Pact5/Utils.hs
+++ test/Chainweb/Test/Pact5/Utils.hs
@@ -1,8 +1,20 @@
+{-# language
+    ImportQualifiedPost
+  , TypeApplications
+#-}
+
 module Chainweb.Test.Pact5.Utils
     ( initCheckpointer
+    , pactTxFrom4To5
     )
     where
 
+import Pact.JSON.Encode qualified as J
+import Control.Monad (forM)
+import Data.Text.Encoding qualified as Text
+import Data.Text (Text)
+import Data.Aeson qualified as Aeson
+import Data.ByteString.Short qualified as SBS
 import Chainweb.Logger
 import Chainweb.Pact.Backend.RelationalCheckpointer
 import Chainweb.Pact.Backend.Types
@@ -10,7 +22,24 @@ import Chainweb.Pact.Types
 import Chainweb.Version
 import qualified Data.Text as T
 import System.LogLevel
+import qualified Pact.Core.Command.Types as Pact5
+import qualified Pact.Core.ChainData as Pact5
+import qualified Chainweb.Pact4.Transaction as Pact4
+import qualified Pact.Types.Command as Pact4
+import qualified Chainweb.Pact5.Transaction as Pact5
 
 initCheckpointer :: ChainwebVersion -> ChainId -> SQLiteEnv -> IO (Checkpointer GenericLogger)
 initCheckpointer v cid sql = do
     initRelationalCheckpointer defaultModuleCacheLimit sql DoNotPersistIntraBlockWrites (genericLogger Error (error . T.unpack)) v cid
+
+pactTxFrom4To5 :: Pact4.Command Pact4.PayloadWithText -> Pact5.Command Pact5.PayloadWithText
+pactTxFrom4To5 tx =
+  let
+    e = do
+      let json = J.encode (fmap (Text.decodeUtf8 . SBS.fromShort . Pact4.payloadBytes) tx)
+      cmdWithPayload <- Aeson.eitherDecode @(Pact5.Command Text) json
+      Pact5.parseCommand cmdWithPayload
+  in
+  case e of
+    Left err -> error err
+    Right cmds -> cmds
