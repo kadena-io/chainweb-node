c2bfa8ce3696a13772ca9042ef87a3a9d38e5676
chessai <chessai1996@gmail.com>
2024-09-11 16:08:59 -0500

    migrate polling confirmation depth test. implement sending

--- test/Chainweb/Test/Pact5/RemotePactTest.hs
+++ test/Chainweb/Test/Pact5/RemotePactTest.hs
@@ -25,42 +25,45 @@ module Chainweb.Test.Pact5.RemotePactTest
     ( tests
     ) where
 
-import Control.Monad.IO.Class (liftIO)
-import Control.Exception (Exception)
-import Chainweb.Test.Utils (testRetryPolicy)
-import Pact.JSON.Encode qualified as J
-import Data.HashMap.Strict (HashMap)
-import Data.HashMap.Strict qualified as HashMap
-import Data.Aeson qualified as Aeson
-import Control.Monad.Catch (Handler(..), throwM)
-import Data.List qualified as List
-import Control.Retry
-import Data.List.NonEmpty qualified as NE
-import Data.List.NonEmpty (NonEmpty)
-import Servant.Client
-import Chainweb.Pact.RestAPI.Client
-import Chainweb.Pact.Types
-import Chainweb.ChainId
+--import Pact.Core.Command.RPC
 import "pact" Pact.Types.API qualified as Pact4
 import "pact" Pact.Types.Command qualified as Pact4
 import "pact" Pact.Types.Hash qualified as Pact4
+import Chainweb.ChainId
 import Chainweb.Graph (singletonChainGraph)
 import Chainweb.Logger
 import Chainweb.Mempool.Mempool (TransactionHash(..))
+import Chainweb.Pact.RestAPI.Client
+import Chainweb.Pact.Types
 import Chainweb.Payload.PayloadStore
 import Chainweb.Storage.Table.RocksDB
-import Test.Tasty
-import Control.Lens
-import Test.Tasty.HUnit (assertBool, assertEqual, assertFailure, testCase)
+import Chainweb.Test.Pact5.CmdBuilder
+import Chainweb.Test.TestVersions
 import Chainweb.Test.Utils (ChainwebNetwork(..), NodeDbDirs(..), withNodesAtLatestBehavior, withNodeDbDirs, deadbeef)
+import Chainweb.Test.Utils (testRetryPolicy)
+import Chainweb.Version
+import Control.Exception (Exception)
+import Control.Lens
+import Control.Monad.Catch (Handler(..), throwM)
+import Control.Monad.IO.Class (liftIO)
 import Control.Monad.Trans.Resource (ResourceT, runResourceT)
-import PredicateTransformers as PT
---import Pact.Core.Command.RPC
+import Control.Retry
+import Data.Aeson qualified as Aeson
+import Data.HashMap.Strict (HashMap)
+import Data.HashMap.Strict qualified as HashMap
+import Data.List qualified as List
+import Data.List.NonEmpty (NonEmpty)
+import Data.List.NonEmpty qualified as NE
+import Data.Text (Text)
 import Pact.Core.Command.Types
 import Pact.Core.Hash qualified as Pact5
-import Chainweb.Version
-import Chainweb.Test.TestVersions
-
+import Pact.JSON.Encode qualified as J
+import PredicateTransformers as PT
+import Servant.Client
+import Test.Tasty
+import Chainweb.Utils
+import Pact.Core.Gas.Types
+import Test.Tasty.HUnit (assertBool, assertEqual, assertFailure, testCase)
 {-
 import Chainweb.Test.Cut.TestBlockDb (TestBlockDb (_bdbPayloadDb, _bdbWebBlockHeaderDb), addTestBlockDb, getCutTestBlockDb, getParentTestBlockDb, mkTestBlockDb, setCutTestBlockDb)
 import Chainweb.Test.Pact4.Utils (stdoutDummyLogger, testPactServiceConfig, withBlockHeaderDb)
@@ -172,16 +175,10 @@ mkFixture baseRdb = do
 
 tests :: RocksDb -> TestTree
 tests rdb = testGroup "Pact5 RemotePactTest"
-    [ testCase "foo" (foo rdb)
-    , testCase "pollingBadlistTest" (pollingBadlistTest rdb)
+    [ testCase "pollingBadlistTest" (pollingBadlistTest rdb)
+    , testCase "pollingConfirmationDepthTest" (pollingConfirmationDepthTest rdb)
     ]
 
-foo :: RocksDb -> IO ()
-foo rdb = runResourceT $ do
-    _fixture <- mkFixture rdb
-    return ()
-
---polling
 pollingBadlistTest :: RocksDb -> IO ()
 pollingBadlistTest baseRdb = runResourceT $ do
     fixture <- mkFixture baseRdb
@@ -192,6 +189,31 @@ pollingBadlistTest baseRdb = runResourceT $ do
         x <- polling clientEnv (NE.singleton pactDeadBeef)
         print x
 
+pollingConfirmationDepthTest :: RocksDb -> IO ()
+pollingConfirmationDepthTest baseRdb = runResourceT $ do
+    fixture <- mkFixture baseRdb
+    let clientEnv = fixture ^. fixtureClientEnv
+
+    liftIO $ do
+        cmd1 <- buildTextCmd v (trivialTx 42)
+        cmd2 <- buildTextCmd v (trivialTx 43)
+        rks <- sending clientEnv (cmd1 NE.:| [cmd2])
+
+        pollResponse <- pollingWithDepth clientEnv rks (Just (ConfirmationDepth 10))
+
+        assertEqual "there are two command results" 2 (length (HashMap.keys pollResponse))
+        --afterPolling <- getCurrentBlockHeight v clientEnv cid
+
+{-localTest :: RocksDb -> IO ()
+localTest baseRdb = runResourceT $ do
+    fixture <- mkFixture baseRdb
+    let clientEnv = fixture ^. fixtureClientEnv
+
+    liftIO $ do
+        let rks = NE.fromList [pactDeadBeef]
+        x <- polling clientEnv (NE.singleton pactDeadBeef)
+        print x-}
+
 newtype PollingException = PollingException String
     deriving stock (Show)
     deriving anyclass (Exception)
@@ -199,7 +221,8 @@ newtype PollingException = PollingException String
 polling :: ()
     => ClientEnv
     -> NonEmpty RequestKey
-    -> IO (HashMap RequestKey (CommandResult Aeson.Value Pact5.Hash))
+    -> IO (HashMap Pact4.RequestKey (Pact4.CommandResult Pact4.Hash))
+    -- -->(HashMap RequestKey (CommandResult Aeson.Value Pact5.Hash))
 polling clientEnv rks = do
     pollingWithDepth clientEnv rks Nothing
 
@@ -207,7 +230,8 @@ pollingWithDepth :: ()
     => ClientEnv
     -> NonEmpty RequestKey
     -> Maybe ConfirmationDepth
-    -> IO (HashMap RequestKey (CommandResult Aeson.Value Pact5.Hash))
+    -> IO (HashMap Pact4.RequestKey (Pact4.CommandResult Pact4.Hash))
+    -- -> IO (HashMap RequestKey (CommandResult Aeson.Value Pact5.Hash))
 pollingWithDepth clientEnv rks mConfirmationDepth = do
     recovering testRetryPolicy [retryHandler] $ \iterNumber -> do
         putStrLn $ "pollingWithDepth: iteration " ++ show iterNumber
@@ -218,35 +242,78 @@ pollingWithDepth clientEnv rks mConfirmationDepth = do
             Left e -> do
                 throwM (PollingException (show e))
             Right (Pact4.PollResponses response) -> do
-                return (convertPollResponse response)
-
+                -- TODO: convert response!
+                return response -- (convertPollResponse response)
     where
         retryHandler :: RetryStatus -> Handler IO Bool
         retryHandler _ = Handler $ \case
             PollingException _ -> return True
             _ -> return False
 
-        toPact4RequestKey :: RequestKey -> Pact4.RequestKey
-        toPact4RequestKey = \case
-            RequestKey (Pact5.Hash bytes) -> Pact4.RequestKey (Pact4.Hash bytes)
+newtype SendingException = SendingException String
+    deriving stock (Show)
+    deriving anyclass (Exception)
 
-        toPact5RequestKey :: Pact4.RequestKey -> RequestKey
-        toPact5RequestKey = \case
-            Pact4.RequestKey (Pact4.Hash bytes) -> RequestKey (Pact5.Hash bytes)
+sending :: ()
+    => ClientEnv
+    -> NonEmpty (Command Text)
+    -> IO (NonEmpty RequestKey)
+sending clientEnv cmd = do
+    recovering testRetryPolicy [retryHandler] $ \iterNumber -> do
+        putStrLn $ "sending: iteration " ++ show iterNumber
 
-        toPact5CommandResult :: ()
-            => Pact4.CommandResult Pact4.Hash
-            -> CommandResult Aeson.Value Pact5.Hash
-        toPact5CommandResult cr4 = case Aeson.eitherDecodeStrictText (J.encodeText cr4) of
-            Left err -> error $ "toPact5CommandResult: decode failed: " ++ err
-            Right cr5 -> cr5
+        let batch = Pact4.SubmitBatch (NE.map toPact4Command cmd)
+        send <- runClientM (pactSendApiClient v cid batch) clientEnv
+        case send of
+            Left e -> do
+                throwM (SendingException (show e))
+            Right (Pact4.RequestKeys response) -> do
+                return (NE.map toPact5RequestKey response)
 
-        convertPollResponse :: ()
-            => HashMap Pact4.RequestKey (Pact4.CommandResult Pact4.Hash)
-            -> HashMap RequestKey (CommandResult Aeson.Value Pact5.Hash)
-        convertPollResponse pact4Response = HashMap.fromList
-            $ List.map (\(rk, cr) -> (toPact5RequestKey rk, toPact5CommandResult cr))
-            $ HashMap.toList pact4Response
+    where
+        retryHandler :: RetryStatus -> Handler IO Bool
+        retryHandler _ = Handler $ \case
+            SendingException _ -> return True
+            _ -> return False
+
+toPact4RequestKey :: RequestKey -> Pact4.RequestKey
+toPact4RequestKey = \case
+    RequestKey (Pact5.Hash bytes) -> Pact4.RequestKey (Pact4.Hash bytes)
+
+toPact5RequestKey :: Pact4.RequestKey -> RequestKey
+toPact5RequestKey = \case
+    Pact4.RequestKey (Pact4.Hash bytes) -> RequestKey (Pact5.Hash bytes)
+
+toPact4Command :: Command Text -> Pact4.Command Text
+toPact4Command cmd4 = case Aeson.eitherDecodeStrictText (J.encodeText cmd4) of
+    Left err -> error $ "toPact4Command: decode failed: " ++ err
+    Right cmd5 -> cmd5
+
+toPact5CommandResult :: ()
+    => Pact4.CommandResult Pact4.Hash
+    -> CommandResult Aeson.Value Pact5.Hash
+toPact5CommandResult cr4 = case Aeson.eitherDecodeStrictText (J.encodeText cr4) of
+    Left err -> error $ "toPact5CommandResult: decode failed: " ++ err
+    Right cr5 -> cr5
+
+convertPollResponse :: ()
+    => HashMap Pact4.RequestKey (Pact4.CommandResult Pact4.Hash)
+    -> HashMap RequestKey (CommandResult Aeson.Value Pact5.Hash)
+convertPollResponse pact4Response = HashMap.fromList
+    $ List.map (\(rk, cr) -> (toPact5RequestKey rk, toPact5CommandResult cr))
+    $ HashMap.toList pact4Response
+
+trivialTx :: Word -> CmdBuilder
+trivialTx n = defaultCmd
+    { _cbRPC = mkExec' (sshow n)
+    , _cbSigners =
+        [ mkEd25519Signer' sender00 []
+        ]
+    , _cbSender = "sender00"
+    , _cbChainId = cid
+    , _cbGasPrice = GasPrice 0.1
+    , _cbGasLimit = GasLimit (Gas 1000)
+    }
 
 successfulTx :: Predicatory p => Pred p (CommandResult log err)
 successfulTx = pt _crResult ? match _PactResultOk something
