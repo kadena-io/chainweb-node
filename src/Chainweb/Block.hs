bf4dfa59f84e0265881e624d681756db224aca2f
Edmund Noble <edmundnoble@gmail.com>
2024-12-12 16:35:13 -0500

    Document Chainweb.Block

--- src/Chainweb/Block.hs
+++ src/Chainweb/Block.hs
@@ -1,3 +1,5 @@
+-- | A type for "blocks" including their header and payload and outputs.
+-- This is only for REST APIs; we do not use the outputs otherwise.
 module Chainweb.Block
     (Block(..))
     where
