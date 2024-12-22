30aeaf9c3f957ee7fe2df3aeeb2f2132f2ecf9dc
Edmund Noble <edmundnoble@gmail.com>
2024-12-22 11:34:29 -0500

    Add webAuthnSignatureTest
    Change-Id: Id000000098659d89a781886f64ac0f8e02d8b59d

--- test/unit/Chainweb/Test/Pact5/RemotePactTest.hs
+++ test/unit/Chainweb/Test/Pact5/RemotePactTest.hs
@@ -188,6 +188,7 @@ tests rdb = withResource' (evaluate httpManager >> evaluate cert) $ \_ ->
         , invalidTxsTest rdb
         , testCaseSteps "caplistTest" (caplistTest rdb)
         , testCaseSteps "allocationTest" (allocationTest rdb)
+        , testCaseSteps "webAuthnSignatureTest" (webAuthnSignatureTest rdb)
         ]
 
 pollingInvalidRequestKeyTest :: RocksDb -> Step -> IO ()
@@ -637,6 +638,23 @@ _PEPact5Error = prism' (PEPact5Error . uncurry3 PactErrorCode) $ \case
     PEPact5Error (PactErrorCode {_peCode, _peMsg, _peInfo}) ->
         Just (_peCode, _peMsg, _peInfo)
     _ -> Nothing
+
+-- Test that transactions signed with (mock) WebAuthn keypairs are accepted
+-- by the pact service.
+webAuthnSignatureTest :: RocksDb -> Step -> IO ()
+webAuthnSignatureTest rdb _step = runResourceT $ do
+    let v = pact5InstantCpmTestVersion petersonChainGraph
+    let cid = unsafeChainId 0
+    fixture <- mkFixture v rdb
+    withFixture fixture $ liftIO $ do
+        cmd <- buildTextCmd v
+            $ set cbSigners [mkWebAuthnSigner' sender02WebAuthn [], mkEd25519Signer' sender00 []]
+            $ set cbRPC (mkExec' "(concat [\"chainweb-\" \"node\"])")
+            $ defaultCmd
+        send v cid [cmd]
+        advanceAllChains_
+        poll v cid [cmdToRequestKey cmd] >>=
+            P.propful [P.match _Just successfulTx]
 
 {-
           recvPwos <- runCutWithTx v pacts targetMempoolRef blockDb $ \_n _bHeight _bHash bHeader -> do
