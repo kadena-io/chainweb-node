d009616f2a305e244c71a2d7fb3abf52b84f8ef3
Lars Kuhtz <lars@kadena.io>
2025-01-19 01:21:32 -0800

    wip-8 (42a4d5a06): src/Chainweb/RestAPI/Utils.hs

--- src/Chainweb/RestAPI/Utils.hs
+++ src/Chainweb/RestAPI/Utils.hs
@@ -22,6 +22,7 @@
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE RankNTypes #-}
 
 #ifndef CURRENT_PACKAGE_VERSION
 #define CURRENT_PACKAGE_VERSION "UNKNOWN"
@@ -98,10 +99,13 @@ module Chainweb.RestAPI.Utils
 , SupportedRespBodyContentType
 , SetRespBodyContentType
 
+-- * Debugging Tools
+, Traced(..)
 ) where
 
 import Control.Exception (Exception, throw)
 import Control.Monad.Catch (bracket)
+import Control.Monad.IO.Class
 
 import Data.Aeson
 import qualified Data.ByteString.Lazy as BSL
@@ -124,6 +128,8 @@ import Network.Wai.Handler.Warp (HostPreference)
 import Servant.API hiding (addHeader)
 import Servant.Client
 import Servant.Server
+import Servant.Server.Internal.Delayed
+import Servant.Server.Internal.DelayedIO
 
 -- internal modules
 import Chainweb.ChainId
@@ -452,6 +458,11 @@ instance Semigroup SomeServer where
     SomeServer (Proxy :: Proxy a) a <> SomeServer (Proxy :: Proxy b) b
         = SomeServer (Proxy @(a :<|> b)) (a :<|> b)
 
+    -- SomeServer (Proxy :: Proxy a) (a :: Server a) <> SomeServer (Proxy :: Proxy b) (b :: Server b)
+    --     = SomeServer
+    --         (Proxy @(Traced "left" a :<|> Traced "right" b))
+    --         (a :<|> b)
+
 instance Monoid SomeServer where
     mappend = (<>)
     mempty = SomeServer (Proxy @EmptyAPI) emptyServer
@@ -531,4 +542,31 @@ deallocateSocket (_, sock) = N.close sock
 
 withSocket :: Port -> HostPreference -> ((Port, N.Socket) -> IO a) -> IO a
 withSocket port interface = bracket (allocateSocket port interface) deallocateSocket
+
+-- -------------------------------------------------------------------------- --
+-- Debugging
+
+-- | Quick And Dirty runtime debugging of Servant routing
+--
+-- FIXME: it may be cleaner to use annotations in the style of Servant's
+-- 'Summary' API feature.
+--
+newtype Traced (s :: Symbol) api = Traced api
+
+enabledTraced :: Bool
+enabledTraced = False
+
+instance (KnownSymbol s, HasServer api ctx) => HasServer (Traced s api) ctx where
+    type ServerT (Traced s api) m = ServerT api m
+    hoistServerWithContext _ pctx f s = hoistServerWithContext (Proxy @api) pctx f s
+    route _ ctx dl
+        | enabledTraced = route (Proxy @api) ctx $ addParameterCheck
+            (const <$> dl)
+            (DelayedIO $ liftIO $ putStrLn (symbolVal @s Proxy))
+        | otherwise = route (Proxy @api) ctx dl
+
+instance HasClient m api => HasClient m (Traced s api) where
+    type Client m (Traced s api) = Client m api
+    clientWithRoute pm _ r = clientWithRoute pm (Proxy @api) r
+    hoistClientMonad pm _ f c = hoistClientMonad pm (Proxy @api) f c
 
