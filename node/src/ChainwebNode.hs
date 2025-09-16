fcc899693217a48462c5c5672129772839bdbf35
Edmund Noble <edmundnoble@gmail.com>
2025-09-16 14:14:45 -0400

    Delete some now-unused replay options from cw-node

--- node/src/ChainwebNode.hs
+++ node/src/ChainwebNode.hs
@@ -318,13 +318,7 @@ node conf logger = do
     rocksDbDir <- getRocksDbDir conf
     pactDbDir <- getPactDbDir conf
     dbBackupsDir <- getBackupsDir conf
-    withRocksDb' <-
-        if _configReadOnlyReplay cwConf
-        then
-            withReadOnlyRocksDb <$ logFunctionText logger Info "Opening RocksDB in read-only mode"
-        else
-            return withRocksDb
-    withRocksDb' rocksDbDir modernDefaultOptions $ \rocksDb -> do
+    withRocksDb rocksDbDir modernDefaultOptions $ \rocksDb -> do
         logFunctionText logger Info $ "opened rocksdb in directory " <> sshow rocksDbDir
         logFunctionText logger Debug $ "backup config: " <> sshow (_configBackup cwConf)
         withChainweb cwConf logger rocksDb pactDbDir dbBackupsDir $ \case
@@ -369,7 +363,6 @@ withNodeLogger logCfg chainwebCfg v f = runManaged $ do
     let !txFailureHandler =
             if isJust (_cutInitialCutFile (_configCuts chainwebCfg))
                 || isJust (_cutInitialBlockHeightLimit (_configCuts chainwebCfg))
-                || _configReadOnlyReplay chainwebCfg
             then [dropLogHandler (Proxy :: Proxy PactTxFailureLog)]
             else []
 
