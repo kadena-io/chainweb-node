7ce326c764989f14bc5e9be12e7cc787236145c9
Lars Kuhtz <lars@kadena.io>
2025-10-08 21:18:27 -0700

    reenable some benchmarks and disable broken actions in CI

--- bench/Bench.hs
+++ bench/Bench.hs
@@ -1,6 +1,4 @@
 {-# LANGUAGE ImportQualifiedPost #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE PatternSynonyms #-}
 
 -- |
 -- Module: JSONEncoding
@@ -10,28 +8,29 @@
 -- Stability: experimental
 module Main (main) where
 
-import Chainweb.Pact.Backend.ApplyCmd qualified as ApplyCmd
-import Chainweb.Pact.Backend.Bench qualified as Checkpointer
-import Chainweb.Pact.Backend.ForkingBench qualified as ForkingBench
+-- import Chainweb.Pact.Backend.ApplyCmd qualified as ApplyCmd
+-- import Chainweb.Pact.Backend.Bench qualified as Checkpointer
+-- import Chainweb.Pact.Backend.ForkingBench qualified as ForkingBench
 import Chainweb.MempoolBench qualified as MempoolBench
-import Chainweb.Pact.Backend.PactService qualified as PactService
-import Chainweb.Storage.Table.RocksDB (withTempRocksDb)
-import Chainweb.Version.Development (pattern Development)
-import Chainweb.Version.RecapDevelopment (pattern RecapDevelopment)
-import Chainweb.Version.Registry (registerVersion)
+-- import Chainweb.Pact.Backend.PactService qualified as PactService
+-- import Chainweb.Storage.Table.RocksDB (withTempRocksDb)
+-- import Chainweb.Version.Development (pattern Development)
+-- import Chainweb.Version.RecapDevelopment (pattern RecapDevelopment)
 import Criterion.Main (defaultMain)
 import JSONEncoding qualified
 
 main :: IO ()
-main = withTempRocksDb "benchmarks" $ \rdb -> do
-  registerVersion RecapDevelopment
-  registerVersion Development
-
-  defaultMain
-    [ ApplyCmd.bench rdb
-    , Checkpointer.bench
-    , ForkingBench.bench rdb
-    , JSONEncoding.benchmarks
+main = defaultMain
+    [ JSONEncoding.benchmarks
     , MempoolBench.bench
-    , PactService.bench rdb
     ]
+
+-- main :: IO ()
+-- main = withTempRocksDb "benchmarks" $ \rdb -> defaultMain
+--     [ JSONEncoding.benchmarks
+--     , ApplyCmd.bench rdb
+--     , Checkpointer.bench
+--     , ForkingBench.bench rdb
+--     , MempoolBench.bench
+--     , PactService.bench rdb
+--     ]
