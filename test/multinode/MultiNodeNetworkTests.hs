eff34aa688026bc66a6577940c205385c9a3f94c
Lars Kuhtz <lars@kadena.io>
2025-10-10 23:37:15 -0700

    multi-node-tests: limit number of nodes by number of available capabilities

--- test/multinode/MultiNodeNetworkTests.hs
+++ test/multinode/MultiNodeNetworkTests.hs
@@ -20,8 +20,11 @@ import System.IO.Temp
 import System.LogLevel
 import Test.Tasty
 import Test.Tasty.HUnit
-import qualified Chainweb.Test.MultiNode
+import Chainweb.Test.MultiNode
 import Chainweb.Version (withVersion)
+import System.IO.Unsafe (unsafePerformIO)
+import Control.Concurrent
+import Numeric.Natural
 
 main :: IO ()
 main = defaultMain suite
@@ -29,27 +32,34 @@ main = defaultMain suite
 loglevel :: LogLevel
 loglevel = Warn
 
+nodeCount :: Natural
+nodeCount = fromIntegral $ unsafePerformIO getNumCapabilities
+{-# NOINLINE nodeCount #-}
+
 -- note that because these tests run in parallel they must all use distinct rocksdb and sqlite dirs.
 suite :: TestTree
 suite = independentSequentialTestGroup "MultiNodeNetworkTests"
-    [ testCaseSteps "ConsensusNetwork - TimedConsensus - 10 nodes - 30 seconds" $ \step ->
+    [ testCaseSteps ("ConsensusNetwork - TimedConsensus - " <> show nodeCount <> " nodes - 30 seconds") $ \step ->
         withTempRocksDb "multinode-tests-timedconsensus-petersen-twenty-rocks" $ \rdb ->
         withSystemTempDirectory "multinode-tests-timedconsensus-petersen-twenty-pact" $ \pactDbDir ->
         withVersion (timedConsensusVersion petersenChainGraph twentyChainGraph) $
-            Chainweb.Test.MultiNode.test loglevel 10 30 rdb pactDbDir step
-    , testCaseSteps "ConsensusNetwork - TimedConsensus - 10 nodes - 30 seconds - d4k4 upgrade" $ \step ->
+            Chainweb.Test.MultiNode.test loglevel nodeCount 30 rdb pactDbDir step
+
+    , testCaseSteps ("ConsensusNetwork - TimedConsensus - " <> show nodeCount <> " nodes - 60 seconds - d4k4 upgrade") $ \step ->
         withTempRocksDb "multinode-tests-timedconsensus-twenty-d4k4-rocks" $ \rdb ->
         withSystemTempDirectory "multinode-tests-timedconsensus-twenty-d4k4-pact" $ \pactDbDir ->
         withVersion (timedConsensusVersion twentyChainGraph d4k4ChainGraph) $
-            Chainweb.Test.MultiNode.test loglevel 4 100 rdb pactDbDir step
-    , testCaseSteps "ConsensusNetwork - InstantTimedCPM singleChainGraph - 10 nodes - 30 seconds" $ \step ->
+            Chainweb.Test.MultiNode.test loglevel nodeCount 60 rdb pactDbDir step
+
+    , testCaseSteps ("ConsensusNetwork - InstantTimedCPM singleChainGraph - " <> show nodeCount <> " nodes - 30 seconds") $ \step ->
         withTempRocksDb "multinode-tests-instantcpm-single-rocks" $ \rdb ->
         withSystemTempDirectory "multinode-tests-instantcpm-single-pact" $ \pactDbDir ->
         withVersion (instantCpmTestVersion singletonChainGraph) $
-            Chainweb.Test.MultiNode.test loglevel 10 30 rdb pactDbDir step
-    , testCaseSteps "Replay - InstantTimedCPM - 6 nodes" $ \step ->
+            Chainweb.Test.MultiNode.test loglevel nodeCount 30 rdb pactDbDir step
+
+    , testCaseSteps ("Replay - InstantTimedCPM - " <> show nodeCount <> " nodes") $ \step ->
         withTempRocksDb "replay-test-instantcpm-pair-rocks" $ \rdb ->
         withSystemTempDirectory "replay-test-instantcpm-pair-pact" $ \pactDbDir ->
         withVersion (instantCpmTestVersion pairChainGraph) $
-            Chainweb.Test.MultiNode.replayTest loglevel 6 rdb pactDbDir step
+            Chainweb.Test.MultiNode.replayTest loglevel nodeCount rdb pactDbDir step
     ]
